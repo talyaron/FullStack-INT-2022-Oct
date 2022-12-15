c83d83744096312dfe147298937e2466a2a9d5bc
itai432 <gelberg4322@gmail.com>
2022-12-15 20:32:21 +0200

    class work

---
+++ 02-TypeScript/03-typescript/exercise/Itai-Idan/index.ts
@@ -0,0 +1,19 @@
+var firstName:string = "itai"
+var secondName:string = "idan"
+var firstAge:number = 28;
+var secondAge:number = 18;
+var avrgeAge:number = (firstAge+secondAge)/2;
+
+
+
+console.log(`${firstName} ${firstAge}`);
+console.log(`${secondName} ${secondAge}`);
+console.log(`avrgeAge is ${avrgeAge}`)
+
+
+
+
+console.log(`${firstName} age is ${firstAge} ${secondName} age is ${secondAge} avegeAge is ${avrgeAge}`);
+
+
+
