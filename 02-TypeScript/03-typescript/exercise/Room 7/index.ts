972acd37781ef0b309d11c4729d04f10bbcb61ce
Nikita Rossenko <darkcaty1999@live.ru>
2022-12-15 20:37:11 +0200

    Update index.ts

--- 02-TypeScript/03-typescript/exercise/Room 7/index.ts
+++ 02-TypeScript/03-typescript/exercise/Room 7/index.ts
@@ -3,7 +3,6 @@ var yourName = "Shlomo";
 var myAge = 30;
 var yourAge = 24;
 
-var avg = (myAge + yourAge) /2;
-
+var avg = (myAge + yourAge) / 2;
 console.log(`My name ${myName} and your name is ${yourName} and our avg age is ${avg}`)
 
