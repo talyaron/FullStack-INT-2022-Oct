3110d4e8d81b2c31b51c21d1c6c79b37fe94c418
Noa1806 <noameillet@gmail.com>
2022-12-15 20:27:52 +0200

    group-3 average exercise

---
+++ 02-TypeScript/03-typescript/exercise/group-3/index.ts
@@ -0,0 +1,20 @@
+var firstName:string= "Noa";
+var secondName:string= "Amit";
+var firstAge:number= 21;
+var secondAge:number= 33;
+
+var average:number= (firstAge+secondAge)/2;
+
+console.log(`The average age of ${firstName} and ${secondName} is ${average}`);
+
+if (average < 30){
+    console.log("You are very young :)");
+} else{
+    console.log("You are young :)");
+}
+
+/*let num;
+for (let i = 0; i <= 5; i++) {
+    num += num;
+    console.log(`The number is ${i}`);
+  }*/
