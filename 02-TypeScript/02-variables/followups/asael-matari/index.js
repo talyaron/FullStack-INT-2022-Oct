02af494d22a2cf503e1c8a2367d92d0fa0670e71
Asael-Matari <mtri01234@gmail.com>
2022-12-15 19:29:12 +0200

    follow up

---
+++ 02-TypeScript/02-variables/followups/asael-matari/index.js
@@ -0,0 +1,13 @@
+// var x=2
+// var y=5
+// // console.log(x/y);
+// alert('bring me to number whose sum isequal to 5')
+// x= +prompt()
+// y=+prompt()
+// console.log(IsEqualTo(x,y));
+// function IsEqualTo(x,y){
+// if(x+y!=5)  return alert('not equalt')
+// return alert('good work')
+// }
+
+
