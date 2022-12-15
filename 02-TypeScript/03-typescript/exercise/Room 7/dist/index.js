14102971420542f7293727e4ca731eb6f367cde9
Nikita Rossenko <darkcaty1999@live.ru>
2022-12-15 20:06:26 +0200

    Exercise - TypeScript - Room 7

---
+++ 02-TypeScript/03-typescript/exercise/Room 7/dist/index.js
@@ -0,0 +1,6 @@
+var myName = "Nikita";
+var yourName = "Shlomo";
+var myAge = 30;
+var yourAge = 24;
+var avg = (myAge + yourAge) / 2;
+console.log("My name " + myName + " and your name is " + yourName + " and our avg age is " + avg);
