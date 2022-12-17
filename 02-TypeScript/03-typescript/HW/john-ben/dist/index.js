fdc3e1ca4484722881b31512d5bce7a7a557616e
jon <jonathanbenn@outlook.fr>
2022-12-17 18:41:23 +0200

    ex

---
+++ 02-TypeScript/03-typescript/HW/john-ben/dist/index.js
@@ -0,0 +1,8 @@
+//var num = 10
+//var num_of_power = 2
+//var result = num ** num_of_power 
+//console.log(result)
+// take the input from the user
+var number = prompt('Enter the number: ');
+var result = Math.sqrt(number);
+console.log("The square root of " + number + " is " + result);
