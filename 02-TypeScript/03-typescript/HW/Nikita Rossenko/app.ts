5150f5e4b0042f5351a2ecfbef6faf0faa212209
Nikita Rossenko <darkcaty1999@live.ru>
2022-12-17 17:21:35 +0200

    Typescript - HW

---
+++ 02-TypeScript/03-typescript/HW/Nikita Rossenko/app.ts
@@ -0,0 +1,23 @@
+// Task 1
+
+var employee1:number = 10000;
+var employee2:number = 11000;
+var employee3:number = 12000;
+var employee4:number = 13000;
+var employee5:number = 14000;
+var employee6:number = 15000;
+var employee7:number = 16000;
+var employee8:number = 17000;
+var employee9:number = 18000;
+var employee10:number = 19000;
+
+var avg = (employee1+employee2+employee3+employee4+employee5+employee6+employee7+employee8+employee9+employee10)/10;
+console.log(`Task 1 \nThe average salary of ten developers is ${avg}$`);
+
+// Task 2
+var num1:number = 30;
+console.log(`Task 2 \nThe number ${num1**2} is a power of ${num1}`)
+
+// Task 3
+var num2:number = 30;
+console.log(`Task 3 \nThe number ${Math.sqrt(num2)} is a square root of ${num2}`)
---
+++ 02-TypeScript/03-typescript/HW/Nikita Rossenko/dist/app.js
@@ -0,0 +1,19 @@
+// Task 1
+var employee1 = 10000;
+var employee2 = 11000;
+var employee3 = 12000;
+var employee4 = 13000;
+var employee5 = 14000;
+var employee6 = 15000;
+var employee7 = 16000;
+var employee8 = 17000;
+var employee9 = 18000;
+var employee10 = 19000;
+var avg = (employee1 + employee2 + employee3 + employee4 + employee5 + employee6 + employee7 + employee8 + employee9 + employee10) / 10;
+console.log("Task 1 \nThe average salary of ten developers is " + avg + "$");
+// Task 2
+var num1 = 30;
+console.log("Task 2 \nThe number " + Math.pow(num1, 2) + " is a power of " + num1);
+// Task 3
+var num2 = 30;
+console.log("Task 3 \nThe number " + Math.sqrt(num2) + " is a square root of " + num2);
