f6fbb778db8c9bbed2b84b342da60b946c853b79
UrielB95 <uriel405@gmail.com>
2022-12-18 17:32:21 +0200

    hw 10 emploee ts

---
+++ 02-TypeScript/03-typescript/HW/uriel bargil/index.ts
@@ -0,0 +1,17 @@
+var sellary1:number = 32000;
+var sellary2:number = 38430;
+var sellary3:number = 27568;
+var sellary4:number = 65233;
+var sellary5:number = 15889;
+var sellary6:number = 22368;
+var sellary7:number = 17852;
+var sellary8:number = 21356;
+var sellary9:number = 55698;
+var sellary10:number = 10369;
+
+var avg = (sellary1+sellary2+sellary3+sellary4+sellary5+sellary6+sellary7+sellary8+sellary9+sellary10)/10;
+
+console.log (avg);
+
+
+console.log (`the averege of 10 employee in company is ${avg}`);
