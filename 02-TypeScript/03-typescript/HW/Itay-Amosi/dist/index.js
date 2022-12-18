4a39ff2fee507951db1400ea399309bdd6b1a7e3
itayz1e <itayz1e@gmail.com>
2022-12-18 11:29:14 +0200

    HW-consol

---
+++ 02-TypeScript/03-typescript/HW/Itay-Amosi/dist/index.js
@@ -0,0 +1,29 @@
+// average-task-1
+console.log("task 1");
+var n = 10; //10 salary og developers
+var itay = 25000;
+var moshe = 23000;
+var shaar = 28000;
+var smuel = 15002;
+var shalome = 17000;
+var amit = 20000;
+var amihai = 20500;
+var tal = 33000;
+var gali = 18200;
+var kobi = 35000;
+var avg = (itay + moshe + shaar + smuel + shalome + amit + amihai + tal + gali + kobi) / n;
+console.log(" the average salary of 10 Developers is " + avg + " ");
+console.log(" --- ");
+console.log(" --- ");
+// variable that gets the power
+console.log("task 2");
+var N_power = 30;
+console.log("The number " + Math.pow(N_power, 2) + " is a power of " + N_power);
+console.log(" --- ");
+console.log(" --- ");
+// square root
+console.log("task 3");
+var sqrt_number1 = 25;
+var sqrt_number2 = 100;
+console.log("The number " + Math.sqrt(sqrt_number1) + " is a square root of " + sqrt_number1);
+console.log("The number " + Math.sqrt(sqrt_number2) + " is a square root of " + sqrt_number2);
