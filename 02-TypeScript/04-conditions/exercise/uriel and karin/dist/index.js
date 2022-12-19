ba96a3886024884ccf72923903738df11a82b531
UrielB95 <uriel405@gmail.com>
2022-12-19 20:32:38 +0200

    ex

---
+++ 02-TypeScript/04-conditions/exercise/uriel and karin/dist/index.js
@@ -0,0 +1,13 @@
+// var pass = prompt("what is the password");
+// console.log (pass);
+// if (pass === "open sessami"){
+// console.log ("you got all the gold ")}
+// else  {console.log ("you are done")};
+var ammount = 566;
+console.log(ammount);
+var vat = (ammount) * 0.17;
+console.log(vat);
+console.log("the vat for " + ammount + " is " + vat + " ");
+var vat = (ammount) * 1.17;
+console.log(vat);
+console.log("the ammount " + ammount + " shekels with the vat is " + vat + " ");
