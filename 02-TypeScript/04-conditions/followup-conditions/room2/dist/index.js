fa0bd0cde928bc69ff2c1b29b279d95ff8018ae5
amitzalts <amitzalts@gmail.com>
2022-12-19 18:27:20 +0200

    added follow-up operators

---
+++ 02-TypeScript/04-conditions/followup-conditions/room2/dist/index.js
@@ -0,0 +1,8 @@
+var x = 10;
+console.log(x === 10);
+if ((x !== 10) || (x === 10)) {
+    console.log("Messi (no." + x + ") is the best");
+}
+else {
+    console.log("Mbape is the best");
+}
---
+++ 02-TypeScript/04-conditions/followup-conditions/room2/index.ts
@@ -0,0 +1,9 @@
+var x:number = 10; 
+console.log(x===10);
+
+if((x!==10) || (x===10)){
+    console.log(`Messi (no.${x}) is the best`); 
+} 
+else {
+    console.log("Mbape is the best");
+}
