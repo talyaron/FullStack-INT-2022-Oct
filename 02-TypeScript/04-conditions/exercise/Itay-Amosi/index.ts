dbddd2cf79b9836b8b1a0696c6e7d43a2a2520a3
itayz1e <itayz1e@gmail.com>
2022-12-19 20:14:38 +0200

    test

--- 02-TypeScript/04-conditions/exercise/Itay-Amosi/index.ts
+++ 02-TypeScript/04-conditions/exercise/Itay-Amosi/index.ts
@@ -1,4 +1,17 @@
+// // var password = `opensesami`;
+// // console.log(password);
 
-var x = open sesami;
-var password = prompt (`enter password`);
-console.log (x)
+// var password = prompt(`enter password`)
+// console.log(password);
+
+// if (password === `opensesami`){
+//     alert("you got all the gold in the cave!!!!")
+// }else{
+//     alert("you are done")
+// }
+
+var n:number = null;
+console.log(number * 1.17)
+
+var calulate = prompt
+console.log(` the average salary of 10 Developers is ${} `);
