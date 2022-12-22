8b8a58d9955ad96f2e8c74c6e96b9ab632c054dc
Vladislav Bykanov <vbykanov1989@outlook.com>
2022-12-22 19:15:18 +0100

    first commit

---
+++ 02-TypeScript/07-functions/class-puzzle/vladiPuzzel/app.ts
@@ -0,0 +1,15 @@
+function multi(a, b): number{
+    return a * b;
+}
+
+function addUp(n: number): number{
+    let x: number = 0;
+    for(let i = 1; i <= n; i++){
+        x += n;
+    }
+    return x;
+}
+
+console.log(multi(3, 4));
+
+console.log(addUp(10));
---
+++ 02-TypeScript/07-functions/class-puzzle/vladiPuzzel/dist/app.js
@@ -0,0 +1,12 @@
+function multi(a, b) {
+    return a * b;
+}
+function addUp(n) {
+    var x = 0;
+    for (var i = 1; i <= n; i++) {
+        x += n;
+    }
+    return x;
+}
+console.log(multi(3, 4));
+console.log(addUp(10));
