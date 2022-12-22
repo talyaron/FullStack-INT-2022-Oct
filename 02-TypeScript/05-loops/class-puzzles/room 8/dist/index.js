f23d3bc3fad1599d1db941a72b1c7a4986e338a8
itai432 <gelberg4322@gmail.com>
2022-12-22 20:32:02 +0200

    class work

---
+++ 02-TypeScript/05-loops/class-puzzles/room 8/dist/index.js
@@ -0,0 +1,8 @@
+//2. calculate with a varibal and a loop how much is 1+2+3+...+n
+var sum = 0;
+var j = ;
+for (var i = 1; i <= 15; i++) {
+    sum = sum + i;
+    console.log(sum);
+}
+//3. calculate with loop inside a loop, how much is (1+2+3+...+n) double j
