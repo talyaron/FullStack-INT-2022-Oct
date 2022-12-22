94292770d566a385754c8701b08f9af1b48e9f26
tomer cohen <tomer66331@gmail.com>
2022-12-22 20:31:56 +0200

    room 4

---
+++ 02-TypeScript/07-functions/class-puzzle/room 4/dist/index.js
@@ -0,0 +1,17 @@
+//test1//
+var a = 2;
+var b = 6;
+function x2(a, b) {
+    return a * b;
+}
+console.log(x2(a, b));
+//test 2//
+var n = 10;
+function sum(n) {
+    var result = 0;
+    for (var i = 1; i <= n; i++) {
+        result += i;
+    }
+    return result;
+}
+console.log(sum(n));
