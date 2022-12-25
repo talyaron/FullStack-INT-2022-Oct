a05c1c9aa322676cdcce88f1dd68046cd769bd0e
amitzalts <amitzalts@gmail.com>
2022-12-25 23:03:34 +0200

    formatted reverse HW (functions)

--- 02-TypeScript/07-functions/puzzles-HW/Amit/reverse.ts
+++ 02-TypeScript/07-functions/puzzles-HW/Amit/reverse.ts
@@ -15,18 +15,18 @@ let again: boolean = true;
 while (again === true) {
     const nStr = prompt("Enter a number you would like to reverse:\n (to stop press cancel)");
 
-    if (nStr !== null) { 
+    if (nStr !== null) {
         const userNumber = parseInt(nStr);
 
-        if(!isNaN(userNumber) && (nStr !== "")){
-        console.log(`${userNumber} in reverse is ${reverse(userNumber)}`);
-        alert (`The reversed number of ${userNumber} is ${reverse(userNumber)}`);
-        } else{
-        alert("You didn't enter a number");
+        if (!isNaN(userNumber) && (nStr !== "")) {
+            console.log(`${userNumber} in reverse is ${reverse(userNumber)}`);
+            alert(`The reversed number of ${userNumber} is ${reverse(userNumber)}`);
+        } else {
+            alert("You didn't enter a number");
         }
 
-    } else{
-       alert ("Thank you, come again!");
+    } else {
+        alert("Thank you, come again!");
         again = false;
-    }   
+    }
 } 
