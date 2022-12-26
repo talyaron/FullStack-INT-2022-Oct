b48a250777da9139cb4068dfdcafb161312c4daf
aviva elias <avivas33@gmail.com>
2022-12-26 15:31:05 +0200

    Aviva-Elias-HW Function-level2
    index.html
    index.ts
    undex.js

--- 02-TypeScript/07-functions/example/dist/indexWithFunctions.js
+++ 02-TypeScript/07-functions/example/dist/indexWithFunctions.js
@@ -4,16 +4,23 @@ function priceWithVat(price) {
 }
 for (var i = 0; i < 7; i++) {
     var userName = prompt("What is your name?");
-    console.log(sayThankYou(userName));
-}
-function sayThankYou(name) {
-    if (name) {
-        name = name.toUpperCase();
-        return "Thank you " + name + " for registering";
+    function sayThankYou(name) {
+        if (name) {
+            name = name.toUpperCase();
+            return "Thank you " + name + " for registering";
+        }
+        else {
+            return "";
+        }
     }
-    else {
-        return "";
-    }
+    // userName = prompt("What is your name?");
+    // console.log(sayThankYou(userName));
+    // const userPrice = prompt("what is the price?");
+    // if (userPrice) {
+    //   const _userPrice = parseInt(userPrice);
+    //   const withVat = priceWithVat(_userPrice);
+    //   console.log(`Your price with vat is ${withVat}`);
+    // }
 }
 // userName = prompt("What is your name?");
 // console.log(sayThankYou(userName));
--- 02-TypeScript/07-functions/puzzles-HW/aviva-elias/index.ts
+++ 02-TypeScript/07-functions/puzzles-HW/aviva-elias/index.ts
@@ -1,19 +1,65 @@
-//level1 1
-
+//level 1 1
+//1.1
 const x:number =123
 function negativReturn(x:number):number{
     return (-x)
 }
 console.log(negativReturn(x))
 
+//1.2
 
 function squareNumber(x:number):number{
     return x**2
 }
 console.log(squareNumber(x))
 
+//1.3
 const Celsius:number=36.4
 function Fahrenheit(Celsius:number):number{
     return Celsius*9/5+32
 }
 console.log(Fahrenheit(Celsius))
+
+//level 2
+//2.1
+const y:number=123
+function theLarger(x:number,y:number){
+    if(x>y){
+        return x}
+      else if(x<y){ return y}
+      else{return "Equals"}
+    }
+    console.log(theLarger(x,y))
+
+//2.2
+
+   
+    let averegeMale=174
+    let avaregeFemale=164
+
+function averegeHeightoGender( userheight:number,usergender:string):number{
+if(usergender==="male") 
+    {   
+           return averegeMale-userheight}
+else {
+    
+    return avaregeFemale-userheight}
+    
+}
+  
+
+  let userheight=prompt("what is your height?");
+ 
+  let usergender=prompt( "what is your gender ?");
+  
+  console.log(averegeHeightoGender(userheight,usergender))  
+
+
+//level3
+
+//function reverseNumbers( num:number):number{
+    
+//}
+let num:number=4
+for(i=1;i<num;i++){
+   console.log(i)}
