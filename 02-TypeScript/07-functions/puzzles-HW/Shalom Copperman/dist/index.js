3dd99fec520024739aeb5c4570d2940197118edc
Shalom Copperman <shalomcop@gmail.com>
2022-12-26 12:12:04 +0200

    wip

---
+++ 02-TypeScript/07-functions/puzzles-HW/Shalom Copperman/dist/index.js
@@ -0,0 +1,11 @@
+var firstNumber = 30;
+var secondNumber = 20;
+function maxNumber(number1, number2) {
+    if (number1 > number2) {
+        return number1;
+    }
+    else {
+        return number2;
+    }
+}
+console.log(maxNumber(firstNumber, secondNumber));
