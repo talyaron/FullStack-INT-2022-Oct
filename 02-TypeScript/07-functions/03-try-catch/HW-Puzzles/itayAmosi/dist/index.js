2b031ab6283cda923c3fc5a3915c485f5ae2ee5b
itayz1e <itayz1e@gmail.com>
2022-12-27 21:23:08 +0200

    Itay-Amosi-HW

---
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/itayAmosi/dist/index.js
@@ -0,0 +1,30 @@
+// Question 1
+var threeNumbers = function (x, y, z) {
+    try {
+        if (typeof x !== 'number' ||
+            typeof y !== 'number' ||
+            typeof z !== 'number') {
+            throw new Error("Error is not a number");
+        }
+        return x * y * z;
+    }
+    catch (Error) {
+        console.error(Error);
+        return false;
+    }
+};
+var calculation = true;
+var x = prompt("Enter a Number 1");
+var y = prompt("Enter a Number 2");
+var z = prompt("Enter a Number 3");
+console.log(x * y * z);
+if (x !== null && y !== null && z !== null) {
+    if (threeNumbers(parseInt(x), parseInt(y), parseInt(z))) {
+        alert("The answer is " + x * y * z);
+        calculation = false;
+    }
+    else {
+        alert("Can't do a calculation There must be 3 numbers to calculate");
+    }
+}
+// Question 2
---
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/itayAmosi/index.ts
@@ -0,0 +1,37 @@
+// Question 1
+const threeNumbers: function = (
+  x: number,
+  y: number,
+  z: number,
+): number | false => {
+  try {
+    if (
+      typeof x !== 'number' ||
+      typeof y !== 'number' ||
+      typeof z !== 'number'
+    ) {
+      throw new Error(`Error is not a number`)
+    }
+    return x * y * z
+  } catch (Error) {
+    console.error(Error)
+    return false
+  }
+}
+let calculation = true;
+const x: any = prompt(`Enter a Number 1`)
+const y: any = prompt(`Enter a Number 2`)
+const z: any = prompt(`Enter a Number 3`)
+console.log(x*y*z)
+if (x !== null && y !== null && z !== null) {
+  if (threeNumbers(parseInt(x), parseInt(y), parseInt(z))) {
+    alert(`The answer is ${x * y * z}`)
+    calculation = false;
+  
+   }else {
+    	alert(`Can't do a calculation There must be 3 numbers to calculate`)
+   }
+}
+
+
+// Question 2
