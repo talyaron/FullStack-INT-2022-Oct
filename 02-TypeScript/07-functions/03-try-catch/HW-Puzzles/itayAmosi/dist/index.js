fb8bdc7c48d7dfc4bbb4d8a40018d819d3afb727
itayz1e <itayz1e@gmail.com>
2022-12-27 21:56:14 +0200

    HW

--- 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/itayAmosi/dist/index.js
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/itayAmosi/dist/index.js
@@ -1,14 +1,13 @@
 // Question 1
 var threeNumbers = function (x, y, z) {
     try {
-        if (typeof x !== 'number' ||
-            typeof y !== 'number' ||
-            typeof z !== 'number') {
-            throw new Error("Error is not a number");
+        if (typeof x !== "number" || typeof y !== "number" || typeof z !== "number") {
+            throw new Error("error is not a number");
         }
         return x * y * z;
     }
     catch (Error) {
+        alert("Can't do a calculation, There must be 3 numbers to calculate");
         console.error(Error);
         return false;
     }
@@ -18,13 +17,13 @@ var x = prompt("Enter a Number 1");
 var y = prompt("Enter a Number 2");
 var z = prompt("Enter a Number 3");
 console.log(x * y * z);
-if (x !== null && y !== null && z !== null) {
+if (x !== null && y !== null && z !== null2) {
     if (threeNumbers(parseInt(x), parseInt(y), parseInt(z))) {
         alert("The answer is " + x * y * z);
         calculation = false;
     }
-    else {
-        alert("Can't do a calculation There must be 3 numbers to calculate");
-    }
+}
+else {
+    alert("Can't do a calculation, There must be 3 numbers to calculate");
 }
 // Question 2
