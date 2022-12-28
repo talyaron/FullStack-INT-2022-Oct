9f7d375fa4d4dbb1557f78c90318b13174e71d30
Vladislav Bykanov <vbykanov1989@outlook.com>
2022-12-28 16:48:37 +0100

    WIP

--- 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/VladiBykanov/app.ts
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/VladiBykanov/app.ts
@@ -1,1 +1,48 @@
-const 
+const message = document.querySelector("#message") as HTMLInputElement;
+const encryptionCode = document.querySelector(
+  "#encryptionCode"
+) as HTMLInputElement;
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const code = "JTREKYAVOGDXPSNCUIZLFBMWHQ";
+
+function encrypt(userInput: string): string {
+  let encytedToNum = "";
+
+  for (let i = 0; i < userInput.length; i++) {
+    // check for space
+    if (userInput[i] == " ") {
+      encytedToNum += " ";
+    }
+    // check if letter is capital
+    else if (userInput[i].toUpperCase() == userInput[i]) {
+      encytedToNum += (userInput.charCodeAt(i) - 65).toString();
+    } else {
+      encytedToNum += (userInput.charCodeAt(i) - 97).toString();
+      //   encytedToNum += "-";
+    }
+  }
+  return encytedToNum;
+}
+
+function decrypt(userInput: string): string {
+  let encytedToNum = "";
+
+  for (let i = 0; i < userInput.length; i++) {
+    // check for space
+    if (userInput[i] == " ") {
+      encytedToNum += " ";
+    }
+    // check if letter is capital
+    else if (userInput[i].toUpperCase() == userInput[i]) {
+      encytedToNum += (userInput.charCodeAt(i) - 65).toString();
+    } else {
+      encytedToNum += (userInput.charCodeAt(i) - 97).toString();
+      //   encytedToNum += "-";
+    }
+  }
+  return encytedToNum;
+}
+
+console.log(" ".charCodeAt(0));
+console.log(encrypt("encrypt this message"));
--- 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/VladiBykanov/dist/app.js
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/VladiBykanov/dist/app.js
@@ -1,1 +1,42 @@
-var ;
+var message = document.querySelector("#message");
+var encryptionCode = document.querySelector("#encryptionCode");
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var code = "JTREKYAVOGDXPSNCUIZLFBMWHQ";
+function encrypt(userInput) {
+    var encytedToNum = "";
+    for (var i = 0; i < userInput.length; i++) {
+        // check for space
+        if (userInput[i] == " ") {
+            encytedToNum += " ";
+        }
+        // check if letter is capital
+        else if (userInput[i].toUpperCase() == userInput[i]) {
+            encytedToNum += (userInput.charCodeAt(i) - 65).toString();
+        }
+        else {
+            encytedToNum += (userInput.charCodeAt(i) - 97).toString();
+            //   encytedToNum += "-";
+        }
+    }
+    return encytedToNum;
+}
+function decrypt(userInput) {
+    var encytedToNum = "";
+    for (var i = 0; i < userInput.length; i++) {
+        // check for space
+        if (userInput[i] == " ") {
+            encytedToNum += " ";
+        }
+        // check if letter is capital
+        else if (userInput[i].toUpperCase() == userInput[i]) {
+            encytedToNum += (userInput.charCodeAt(i) - 65).toString();
+        }
+        else {
+            encytedToNum += (userInput.charCodeAt(i) - 97).toString();
+            //   encytedToNum += "-";
+        }
+    }
+    return encytedToNum;
+}
+console.log(" ".charCodeAt(0));
+console.log(encrypt("encrypt this message"));
