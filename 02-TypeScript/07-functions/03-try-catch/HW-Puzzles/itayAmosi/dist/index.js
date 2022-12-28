b62668ff1a99a99346f5539342f875d2e1e71e33
itayz1e <itayz1e@gmail.com>
2022-12-28 19:44:08 +0200

    HW p2

--- 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/itayAmosi/dist/index.js
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/itayAmosi/dist/index.js
@@ -1,29 +1,31 @@
-// Question 1
-var threeNumbers = function (x, y, z) {
-    try {
-        if (typeof x !== "number" || typeof y !== "number" || typeof z !== "number") {
-            throw new Error("error is not a number");
-        }
-        return x * y * z;
-    }
-    catch (Error) {
-        alert("Can't do a calculation, There must be 3 numbers to calculate");
-        console.error(Error);
-        return false;
-    }
-};
-var calculation = true;
-var x = prompt("Enter a Number 1");
-var y = prompt("Enter a Number 2");
-var z = prompt("Enter a Number 3");
-console.log(x * y * z);
-if (x !== null && y !== null && z !== null) {
-    if (threeNumbers(parseInt(x), parseInt(y), parseInt(z))) {
-        alert("The answer is " + x * y * z);
-        calculation = false;
-    }
-}
-else {
-    alert("Can't do a calculation, There must be 3 numbers to calculate");
-}
+// // Question 1
+// const threeNumbers: Function = (x: number,y: number, z: number): number | false => {
+//     try {
+//     if (typeof x !== "number" ||typeof y !== "number" ||typeof z !== "number") {
+//         throw new Error(`error is not a number`);
+//     }return x*y*z;
+//     }catch (Error){
+//         alert(`Can't do a calculation, There must be 3 numbers to calculate`);
+//         console.error(Error);
+//         return false;
+//     }
+//     }
+// let calculation = true;
+//     const x: any =prompt(`Enter a Number 1`);
+//     const y: any =prompt(`Enter a Number 2`);
+//     const z: any =prompt(`Enter a Number 3`);
+//     console.log(x*y*z);
+//     if (x !== null && y !== null && z !== null) {
+//         if (threeNumbers(parseInt(x), parseInt(y), parseInt(z))) {
+//          alert(`The answer is ${x*y*z}`);
+//             calculation = false;
+//         }
+//     }else{
+//         alert(`Can't do a calculation, There must be 3 numbers to calculate`);
+//     }
 // Question 2
+var acod = function (a) {
+    return a = 1;
+};
+var a, number = prompt("Enter a Number");
+console.log(a);
