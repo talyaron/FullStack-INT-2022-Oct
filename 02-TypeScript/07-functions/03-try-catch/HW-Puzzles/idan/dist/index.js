99f6528e96ee40731ea09835602a398616a55c09
idan <117864474+JoeBidenMyBeloved@users.noreply.github.com>
2022-12-29 05:35:35 +0200

    joe

--- 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/idan/dist/index.js
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/idan/dist/index.js
@@ -2,7 +2,9 @@ while (true) {
     var deOrEncrypt = prompt("do you wish to encrypt or decrypt");
     var message = prompt("enter what you wish to encrypt/decrypt");
     if (deOrEncrypt == "decrypt") {
-        alert(unJoeMachine(message));
+        if (message) {
+            alert(unJoeMachine(message));
+        }
     }
     else {
         if (message) {
@@ -11,64 +13,75 @@ while (true) {
     }
 }
 function joeMachine(unencryptedMsg) {
-    //ong no cap i have no idea why it has an err
-    var encryptedMsg = [unencryptedMsg.length];
-    console.log(typeof (encryptedMsg));
-    //i have no idea what kind of error can occur so i dont know what try/catch thing to put also the name joe machine does have meaning and i wont explain why
-    for (var index = 0; index < unencryptedMsg.length; index++) {
-        switch (index % 8) {
-            case 1:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 5);
-                break;
-            case 2:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 10);
-                break;
-            case 3:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 3);
-                break;
-            case 4:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 7);
-                break;
-            case 5:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 5);
-                break;
-            case 6:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 1);
-                break;
-            default:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 3);
-                break;
+    try {
+        var encryptedMsg = [unencryptedMsg.length];
+        console.log(typeof (encryptedMsg));
+        //i have no idea what kind of error can occur so i dont know what try/catch thing to put also the name joe machine does have meaning and i wont explain why
+        for (var index = 0; index < unencryptedMsg.length; index++) {
+            switch (index % 8) {
+                case 1:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 5);
+                    break;
+                case 2:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 10);
+                    break;
+                case 3:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 3);
+                    break;
+                case 4:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 7);
+                    break;
+                case 5:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 5);
+                    break;
+                case 6:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 1);
+                    break;
+                default:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 3);
+                    break;
+            }
         }
+        return encryptedMsg.join('');
     }
-    return encryptedMsg.join('');
+    catch (_a) {
+        console.error("unexpected input");
+        return "an unexpected error has occured please try again";
+    }
 }
 function unJoeMachine(unencryptedMsg) {
-    var encryptedMsg = [unencryptedMsg.length];
-    console.log(typeof (encryptedMsg));
-    for (var index = 0; index < unencryptedMsg.length; index++) {
-        switch (index % 8) {
-            case 1:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 5);
-                break;
-            case 2:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 10);
-                break;
-            case 3:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 3);
-                break;
-            case 4:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 7);
-                break;
-            case 5:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 5);
-                break;
-            case 6:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 1);
-                break;
-            default:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 3);
-                break;
+    try {
+        var encryptedMsg = [unencryptedMsg.length];
+        console.log(typeof (encryptedMsg));
+        for (var index = 0; index < unencryptedMsg.length; index++) {
+            switch (index % 8) {
+                case 1:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 5);
+                    break;
+                case 2:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 10);
+                    break;
+                case 3:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 3);
+                    break;
+                case 4:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 7);
+                    break;
+                case 5:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) + 5);
+                    break;
+                case 6:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 1);
+                    break;
+                default:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index) - 3);
+                    break;
+            }
         }
+        return encryptedMsg.join('');
     }
-    return encryptedMsg.join('');
+    catch (_a) {
+        console.error("unexpected input");
+        return "an unexpected error has occured please try again";
+    }
 }
--- 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/idan/index.ts
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/idan/index.ts
@@ -4,7 +4,9 @@ while(true){
     const deOrEncrypt:string|null= prompt(`do you wish to encrypt or decrypt`);
     const message:string|null = prompt(`enter what you wish to encrypt/decrypt`);
     if(deOrEncrypt == `decrypt`){
-        alert(unJoeMachine(message))
+        if(message){
+            alert(unJoeMachine(message))
+        }
     }
     else{
         if(message){
@@ -14,68 +16,79 @@ while(true){
 }
 
 function joeMachine(unencryptedMsg:string):string{
-    //ong no cap i have no idea why it has an err
-    let encryptedMsg:any = [unencryptedMsg.length];
-    console.log(typeof(encryptedMsg))
-    //i have no idea what kind of error can occur so i dont know what try/catch thing to put also the name joe machine does have meaning and i wont explain why
-    for (let index = 0; index < unencryptedMsg.length; index++) {
-       switch (index%8){
-            case 1: 
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+5);
-                break;
-            case 2:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-10);
-                break;
-            case 3:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-3);
-                break;
-            case 4:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+7);
-                break;
-            case 5:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-5);
-                break;
-            case 6:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+1);
-                break;
-            default:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+3);
-                break; 
-                
-        }     
+    try{
+        let encryptedMsg:any = [unencryptedMsg.length];
+        console.log(typeof(encryptedMsg))
+        //i have no idea what kind of error can occur so i dont know what try/catch thing to put also the name joe machine does have meaning and i wont explain why
+        for (let index = 0; index < unencryptedMsg.length; index++) {
+           switch (index%8){
+                case 1: 
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+5);
+                    break;
+                case 2:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-10);
+                    break;
+                case 3:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-3);
+                    break;
+                case 4:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+7);
+                    break;
+                case 5:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-5);
+                    break;
+                case 6:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+1);
+                    break;
+                default:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+3);
+                    break; 
+                    
+            }     
+        }
+    
+        return encryptedMsg.join('');
     }
-
-    return encryptedMsg.join('');
+    catch{
+        console.error(`unexpected input`)
+        return `an unexpected error has occured please try again`
+    }
 }
 function unJoeMachine(unencryptedMsg:string):string{
-    let encryptedMsg:any = [unencryptedMsg.length];
-    console.log(typeof(encryptedMsg))
-    for (let index = 0; index < unencryptedMsg.length; index++) {
-       switch (index%8){
-            case 1: 
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-5);
-                break;
-            case 2:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+10);
-                break;
-            case 3:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+3);
-                break;
-            case 4:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-7);
-                break;
-            case 5:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+5);
-                break;
-            case 6:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-1);
-                break;
-            default:
-                encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-3);
-                break; 
-                
-        }     
-    }
+    try{
+        let encryptedMsg:any = [unencryptedMsg.length];
+        console.log(typeof(encryptedMsg))
+        for (let index = 0; index < unencryptedMsg.length; index++) {
+           switch (index%8){
+                case 1: 
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-5);
+                    break;
+                case 2:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+10);
+                    break;
+                case 3:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+3);
+                    break;
+                case 4:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-7);
+                    break;
+                case 5:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)+5);
+                    break;
+                case 6:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-1);
+                    break;
+                default:
+                    encryptedMsg[index] = String.fromCharCode(unencryptedMsg.charCodeAt(index)-3);
+                    break; 
 
-    return encryptedMsg.join('');
+            }     
+        }
+
+        return encryptedMsg.join('');
+    }
+    catch{
+        console.error(`unexpected input`)
+        return `an unexpected error has occured please try again`
+    }
 }
