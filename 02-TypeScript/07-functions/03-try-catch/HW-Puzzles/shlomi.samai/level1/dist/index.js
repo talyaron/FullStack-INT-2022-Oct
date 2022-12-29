383da8c1c9786c5302b6ae4f7a30f1d2e305925d
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2022-12-29 18:29:54 +0200

    hw

--- 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/shlomi.samai/level1/dist/index.js
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/shlomi.samai/level1/dist/index.js
@@ -10,16 +10,16 @@ function multiply(x, y, z) {
         return false;
     }
 }
-let inputA = prompt("please enter a number");
-let inputB = prompt("please enter a number");
-let inputC = prompt("please enter a number");
-if ((inputA == null) || (inputB == null) || (inputC == null)) {
+var inputA = prompt("please enter a number");
+var inputB = prompt("please enter a number");
+var inputC = prompt("please enter a number");
+if (inputA == null || inputB == null || inputC == null) {
     console.log("number is null");
 }
 else {
-    let inputANumber = parseInt(inputA);
-    let inputBNumber = parseInt(inputB);
-    let inputCNumber = parseInt(inputC);
-    const functionResult = multiply(inputANumber, inputBNumber, inputCNumber);
+    var inputANumber = parseInt(inputA);
+    var inputBNumber = parseInt(inputB);
+    var inputCNumber = parseInt(inputC);
+    var functionResult = multiply(inputANumber, inputBNumber, inputCNumber);
     console.log(functionResult);
 }
