1e9bce66ec5129025060dbfda74153006d1f5ca5
Shalom Copperman <shalomcop@gmail.com>
2022-12-29 15:24:35 +0200

    HM DONE

---
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/Shalom Copperman/dist/app.js
@@ -0,0 +1,32 @@
+// function gets string of numbers and returns integer. or false.
+var isNumber = function (numberString) {
+    try {
+        if (isNaN(numberString)) {
+            throw new Error("This in not a number!");
+        }
+        return parseInt(numberString);
+    }
+    catch (error) {
+        console.error(error);
+        return false;
+    }
+};
+// function gets a number from the user and retuns it as an integer.
+// If the user entered a letter, the loop will continue.
+var getNumber = function (textNumber) {
+    var rightInput = false;
+    while (!rightInput) {
+        var numberString = prompt("Please enter your " + textNumber + " number.");
+        var intNumber = isNumber(numberString);
+        if (intNumber) {
+            rightInput = true;
+        }
+    }
+    return intNumber;
+};
+// get three numbers and prints the result.
+var firstNumber = getNumber("first");
+var secondNumber = getNumber("second");
+var thirdNumber = getNumber("third");
+var result = firstNumber * secondNumber * thirdNumber;
+document.write(firstNumber + "*" + secondNumber + "*" + thirdNumber + "=" + result);
