e22f8c16500cbf59586cb75b21a8f85605449c72
Vladislav Bykanov <vbykanov1989@outlook.com>
2022-12-29 10:00:27 +0100

    try catch js

--- 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/VladiBykanov/dist/app.js
+++ 02-TypeScript/07-functions/03-try-catch/HW-Puzzles/VladiBykanov/dist/app.js
@@ -6,42 +6,56 @@ var decryptCheckbox = document.querySelector("#decrypt");
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var encryption_Code = "JTREKYAVOGDXPSNCUIZLFBMWHQ";
 function encrypt(userInput) {
-    var encytedToNum = "";
-    for (var i = 0; i < userInput.length; i++) {
-        // check for space
-        if (userInput[i] == " ") {
-            encytedToNum += " ";
+    try {
+        var encytedToNum = "";
+        for (var i = 0; i < userInput.length; i++) {
+            // check for space
+            if (userInput[i] == " ") {
+                encytedToNum += " ";
+            }
+            // check if letter is capital
+            else if (userInput[i].toUpperCase() == userInput[i]) {
+                encytedToNum += (userInput.charCodeAt(i) - 65).toString();
+                encytedToNum += " ";
+            }
+            else {
+                encytedToNum += (userInput.charCodeAt(i) - 97).toString();
+                encytedToNum += " ";
+            }
         }
-        // check if letter is capital
-        else if (userInput[i].toUpperCase() == userInput[i]) {
-            encytedToNum += (userInput.charCodeAt(i) - 65).toString();
-            encytedToNum += " ";
-        }
-        else {
-            encytedToNum += (userInput.charCodeAt(i) - 97).toString();
-            encytedToNum += " ";
-        }
+        return encytedToNum;
     }
-    return encytedToNum;
+    catch (error) {
+        return console.error(error);
+    }
 }
 function decrypt(encryptedMessage) {
-    var decryptBackToString = "";
-    var textArr = encryptedMessage.split(" ");
-    console.log(textArr);
-    for (var i = 0; i < textArr.length; i++) {
-        decryptBackToString += String.fromCharCode(parseInt(textArr[i]) + 65);
+    try {
+        var decryptBackToString = "";
+        var textArr = encryptedMessage.split(" ");
+        console.log(textArr);
+        for (var i = 0; i < textArr.length; i++) {
+            decryptBackToString += String.fromCharCode(parseInt(textArr[i]) + 65);
+        }
+        return decryptBackToString;
     }
-    return decryptBackToString;
+    catch (error) {
+        return console.error(error);
+    }
 }
 // console.log(" ".charCodeAt(0));
 window.addEventListener("keydown", function (e) {
     e.preventDefault;
     if (e.key == "Enter") {
         if (message.value.length > 5 && message.value != "") {
-            if (encryptCheckbox.checked && !decryptCheckbox.checked && !/\d/.test(message.value)) {
+            if (encryptCheckbox.checked &&
+                !decryptCheckbox.checked &&
+                !/\d/.test(message.value)) {
                 output.textContent = encrypt(message.value);
             }
-            else if (!encryptCheckbox.checked && decryptCheckbox.checked && /\d/.test(message.value)) {
+            else if (!encryptCheckbox.checked &&
+                decryptCheckbox.checked &&
+                /\d/.test(message.value)) {
                 output.textContent = decrypt(message.value);
             }
             else {
