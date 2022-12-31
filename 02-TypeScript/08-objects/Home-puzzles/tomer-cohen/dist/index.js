c970969338ffd015a60ce49240990ecb53892a06
tomer cohen <tomer66331@gmail.com>
2022-12-31 20:50:58 +0200

    tomer HW

--- 02-TypeScript/08-objects/Home-puzzles/tomer-cohen/dist/index.js
+++ 02-TypeScript/08-objects/Home-puzzles/tomer-cohen/dist/index.js
@@ -1,12 +1,3 @@
-function DollerToShekels(video) {
-    try {
-        return video.price * 3.53;
-    }
-    catch (error) {
-        console.error(error);
-        return false;
-    }
-}
 var fastAndFurious9 = {
     nameOfMovie: "fastAndFurious9",
     price: 9.99
@@ -19,13 +10,28 @@ var avatar2 = {
     nameOfMovie: "Avatar 2",
     price: 49.99
 };
+function DollerToShekels(video) {
+    try {
+        return 3.53 * video;
+    }
+    catch (error) {
+        console.error(error);
+        return false;
+    }
+}
 var movieAllow = prompt("what movie do you want to rent? we have FastAndFurious9,AvengersEndGame,Avater2 ");
 if (movieAllow == " " || !movieAllow) {
     var movieAllow_1 = alert("please enter a movie");
-}
-if (fastAndFurious9) {
-    var movieAllow_2 = alert("tomer take the movie");
-}
-else {
-    var user = alert(dollar_to_shekel(movieAllow) + "cost");
+    if (fastAndFurious9) {
+        var movieAllow_2 = alert("tomer take the movie");
+    }
+    if (avatar2) {
+        alert("the price for avatar 2 is " + avatar2.price + "in shekels is " + DollerToShekels(avatar2.price));
+    }
+    if (avengersEndaGame) {
+        alert("the price for avengersEndaGame is " + avengersEndaGame.price + "in shekels is " + DollerToShekels(avengersEndaGame.price));
+    }
+    else {
+        alert("why you dont take movie");
+    }
 }
--- 02-TypeScript/08-objects/Home-puzzles/tomer-cohen/index.ts
+++ 02-TypeScript/08-objects/Home-puzzles/tomer-cohen/index.ts
@@ -8,18 +8,11 @@ interface users{
 Name:string;
 userPassword:string;
 }
-function DollerToShekels(video:videos):number | false {
-try {
-    return video.price*3.53;
-} catch (error) {
-    console.error(error);
-    return false;
-}
-}
 const fastAndFurious9:videos={
     nameOfMovie:`fastAndFurious9`,
     price:9.99
 }
+
 const avengersEndaGame:videos={
     nameOfMovie: `avengers EndaGame`,
     price: 24.99
@@ -28,13 +21,26 @@ const avatar2:videos={
     nameOfMovie: `Avatar 2`,
     price: 49.99
 }
+function DollerToShekels(video:number):number | false {
+try {
+    return 3.53*video;
+} catch (error) {
+    console.error(error);
+    return false;
+}
+}
 const movieAllow:string| null= prompt(`what movie do you want to rent? we have FastAndFurious9,AvengersEndGame,Avater2 `)
 if (movieAllow==" "|| !movieAllow){
     const movieAllow = alert ("please enter a movie")
-}
+
     if(fastAndFurious9){
         const movieAllow= alert(`tomer take the movie`)
-    }else{
-        const user=alert(`${dollar_to_shekel(movieAllow)}cost`)
-    }
-
+    }if(avatar2){
+       alert(`the price for avatar 2 is ${avatar2.price}in shekels is ${DollerToShekels(avatar2.price)}`)
+        }
+    if(avengersEndaGame){
+            alert(`the price for avengersEndaGame is ${avengersEndaGame.price}in shekels is ${DollerToShekels(avengersEndaGame.price)}`)
+        }else{
+            alert(`why you dont take movie`)
+}
+}
