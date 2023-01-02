6650296d9a303cd0119e0f3d2eb95d2bfc8e9285
angela-zorov <zorov.angela.01@gmail.com>
2023-01-02 21:12:08 +0200

    done

--- 02-TypeScript/08-objects/Home-puzzles/angela/index.ts
+++ 02-TypeScript/08-objects/Home-puzzles/angela/index.ts
@@ -4,41 +4,42 @@ interface Movie {
     
 }
 
-const train_to_busan:Movie = {
-    name:"train to busan",
-    price:9.99
-}
+const train_to_busan = {
+    name: "train to busan",
+    price: 9.99
+};
+const fight_club = {
+    name: "fight club",
+    price: 14.99
+};
+const I_Am_Legend = {
+    name: "i Am Legend",
+    price: 5.50
+};
 
-const fight_club:Movie ={
-    name:"fight club",
-    price:14.99
-}
-const I_Am_Legend:Movie={
-    name:"i Am Legend",
-    price:5.50
-}
-
-const renter=['john','amelia','luke','dan','lily','luna'];
-
-function dollar_to_shekel (video:Movie){
+const renter = ['john', 'amelia', 'luke', 'dan', 'lily', 'luna'];
+function dollar_to_shekel(userInput) {
     try {
-        switch(dollar_to_shekel){
-            case "train to busan": return video.price *3.53;
-            case "fight club ":return video.price * 3.53;
-            case "i am legend": return video.price *3.53;
-            default: return "you choose an unavailable movie"
+        switch (userInput) {
+            case "train to busan": return train_to_busan.price * 3.53;
+            case "fight club": return fight_club.price * 3.53;
+            case "i am legend": return I_Am_Legend.price * 3.53;
+            
         }
-    } catch (error) {
-       console.error("error") 
+    }
+    catch (error) {
+        console.error("error");
     }
 }
 const randomName = Math.floor(Math.random() * renter.length);
-// console.log(randomname, renter[random]);
-
-const wellcome=alert("wellcome to blockbuster")
-const userInput= prompt("currnt available movies are: train to busan,fight club,i am legend")
-if (userInput==" "|| !userInput){
-    const userInput = alert ("please enter a movie")
-} else {
-    const user = alert(`${dollar_to_shekel(userInput)}} cost ,also ${renter[randomName]} rented it`)
+let wellcome = alert("wellcome to blockbuster");
+let userInput = prompt("current available movies are: train to busan,fight club,i am legend");
+if (userInput == " " || !userInput) {
+    let userInput = alert("please enter a movie");
 }
+
+else {
+    const dts = dollar_to_shekel(userInput);
+    const user = alert(`${userInput} costs ${dts}, also ${renter[randomName]} rented it `)
+}
+
