036c1412c586da34302ed6e2cdb32ef107338831
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-01-02 21:07:05 +0100

    WIP

--- 02-TypeScript/08-objects/Home-puzzles/VladiBykanov/app.ts
+++ 02-TypeScript/08-objects/Home-puzzles/VladiBykanov/app.ts
@@ -1,61 +1,81 @@
 // initiating a variable that will hold all the users
-const netflixUsers: {}[] = [];
+const netflixUsers: NetflixUser[] = [];
+const movieList = {};
 const userBtn = document.querySelectorAll(".chooseUser");
 const movieBtn = document.querySelectorAll(".chooseMovie");
+
 // user template
 interface NetflixUser {
   userName: string;
   videoList: {};
-  whoWatchedThisMovie: Function;
-  getName: Function;
+  checkIfMovieWasWatched: Function;
+  getUserName: Function;
   addMovieToList: Function;
 }
 
 // function that will crate a user based on our interface template
-function AddNetflixUser(userName: string, videoList: {}) {
+function addUser(userName: string, videoList: {}) {
   this.userName = userName;
   this.videoList = videoList;
   // Return a list of peope that watched the movie chosen
-  this.whoWatchedThisMovie = (movie: string) => {
-    return this.videoList[movie];
-  };
+  this.checkIfMovieWasWatched = (movie: string): boolean => this.videoList[movie];
   // return users name
-  this.getName = () => {
+  this.getUserName = () => {
     return this.userName;
   };
   // add a movie to users watch list
   this.addMovieToList = (movie: string) => {
-    this.videoList[movie] = [];
+    this.videoList[movie] = false;
   };
   //adding user to list of users on platform
   netflixUsers.push(this);
 }
 
-const userOne: NetflixUser = new AddNetflixUser("Vladislav Bykanov", {
-  matrix: ["who watched matrix?"],
-  avatar: ["who watched Avatar"],
-  "the godfather": ["who watched The godfather?"],
+const userOne: NetflixUser = new addUser("Vladislav Bykanov", {
+  matrix: false,
+  avatar: false,
+  "the godfather": true,
 });
 
-console.log(userOne.whoWatchedThisMovie("matrix"));
-console.log(userOne.getName());
+const userTwo: NetflixUser = new addUser("John Doe", {
+  matrix: false,
+  avatar: false,
+  "the godfather": true,
+});
+
+// console.log(userOne.checkIfMovieWasWatched("matrix"));
+// console.log(userOne.getUserName());
 userOne.addMovieToList("Titanic");
-console.log(userOne);
+console.log(netflixUsers);
 console.log(userOne.videoList["avatar"]);
-addMovieAsViewedToPerson(userOne.videoList['Titanic'], 'vladi');
 console.log(userOne.videoList["Titanic"]);
 
-// console.log(account.whoWatchedThisMovie("The Matrix"));
-// account.addNewMovie = 'movie';
-// console.log(account);
-// console.log(account.userName);
-// console.log(netflixUsers[1].userName);
-// netflixUsers.forEach(user => console.log(user.userName))
+// console.log(movieList);
+
+const keys = Object.keys(netflixUsers[1].videoList);
+const values = Object.values(netflixUsers[1].videoList)[0];
+// console.log(keys);
+// console.log(values);
+
+// console.log(movieList);
 
 // adding the movie as viewed for the chosen user
-function addMovieAsViewedToPerson(movie: string[], user: string) {
-  return movie.push(user);
+function markMovieAsViewedForPerson(movie: string, user: string) {
+  return movieList[movie].push(user);
 }
+
+// console.log each movie that was watched and who watched it
+netflixUsers.forEach((user) => {
+  Object.entries(user.videoList).forEach(([key, value]) => {
+    if (value == true) {
+      if (!movieList[key]) {
+        movieList[key] = [];
+      }
+      movieList[key].push(user.userName);
+      // console.log(key, user.userName);
+    }
+  });
+});
 
 // const account = {
 //   userName: "Vladi",
@@ -63,10 +83,10 @@ function addMovieAsViewedToPerson(movie: string[], user: string) {
 //     "The Matrix": ["Vladi", "John", "Jerry"],
 //     "Lord of the rings": ["Vladi", "Sara", "Michel"],
 //   },
-//   whoWatchedThisMovie(movie: string): string[] {
+//   checkIfMovieWasWatched(movie: string): string[] {
 //     return this.videoList[movie];
 //   },
-//   get getName() {
+//   get getUserName() {
 //     return this.userName;
 //   },
 //   set addNewMovie(movie: string) {
@@ -74,9 +94,7 @@ function addMovieAsViewedToPerson(movie: string[], user: string) {
 //   },
 // };
 
-
-
 let word = "hello";
 let upperWord = word.toLowerCase().charAt(0).toUpperCase() + word.slice(1);
 
-console.log(upperWord);
+// console.log(upperWord);
--- 02-TypeScript/08-objects/Home-puzzles/VladiBykanov/dist/app.js
+++ 02-TypeScript/08-objects/Home-puzzles/VladiBykanov/dist/app.js
@@ -1,59 +1,75 @@
 // initiating a variable that will hold all the users
 var netflixUsers = [];
+var movieList = {};
 var userBtn = document.querySelectorAll(".chooseUser");
 var movieBtn = document.querySelectorAll(".chooseMovie");
 // function that will crate a user based on our interface template
-function AddNetflixUser(userName, videoList) {
+function addUser(userName, videoList) {
     var _this = this;
     this.userName = userName;
     this.videoList = videoList;
     // Return a list of peope that watched the movie chosen
-    this.whoWatchedThisMovie = function (movie) {
-        return _this.videoList[movie];
-    };
+    this.checkIfMovieWasWatched = function (movie) { return _this.videoList[movie]; };
     // return users name
-    this.getName = function () {
+    this.getUserName = function () {
         return _this.userName;
     };
     // add a movie to users watch list
     this.addMovieToList = function (movie) {
-        _this.videoList[movie] = [];
+        _this.videoList[movie] = false;
     };
     //adding user to list of users on platform
     netflixUsers.push(this);
 }
-var userOne = new AddNetflixUser("Vladislav Bykanov", {
-    matrix: ["who watched matrix?"],
-    avatar: ["who watched Avatar"],
-    "the godfather": ["who watched The godfather?"]
+var userOne = new addUser("Vladislav Bykanov", {
+    matrix: false,
+    avatar: false,
+    "the godfather": true
 });
-console.log(userOne.whoWatchedThisMovie("matrix"));
-console.log(userOne.getName());
+var userTwo = new addUser("John Doe", {
+    matrix: false,
+    avatar: false,
+    "the godfather": true
+});
+// console.log(userOne.checkIfMovieWasWatched("matrix"));
+// console.log(userOne.getUserName());
 userOne.addMovieToList("Titanic");
-console.log(userOne);
+console.log(netflixUsers);
 console.log(userOne.videoList["avatar"]);
-addMovieAsViewedToPerson(userOne.videoList['Titanic'], 'vladi');
 console.log(userOne.videoList["Titanic"]);
-// console.log(account.whoWatchedThisMovie("The Matrix"));
-// account.addNewMovie = 'movie';
-// console.log(account);
-// console.log(account.userName);
-// console.log(netflixUsers[1].userName);
-// netflixUsers.forEach(user => console.log(user.userName))
+// console.log(movieList);
+var keys = Object.keys(netflixUsers[1].videoList);
+var values = Object.values(netflixUsers[1].videoList)[0];
+// console.log(keys);
+// console.log(values);
+// console.log(movieList);
 // adding the movie as viewed for the chosen user
-function addMovieAsViewedToPerson(movie, user) {
-    return movie.push(user);
+function markMovieAsViewedForPerson(movie, user) {
+    return movieList[movie].push(user);
 }
+// console.log each movie that was watched and who watched it
+netflixUsers.forEach(function (user) {
+    Object.entries(user.videoList).forEach(function (_a) {
+        var key = _a[0], value = _a[1];
+        if (value == true) {
+            if (!movieList[key]) {
+                movieList[key] = [];
+            }
+            movieList[key].push(user.userName);
+            // console.log(key, user.userName);
+        }
+    });
+});
 // const account = {
 //   userName: "Vladi",
 //   videoList: {
 //     "The Matrix": ["Vladi", "John", "Jerry"],
 //     "Lord of the rings": ["Vladi", "Sara", "Michel"],
 //   },
-//   whoWatchedThisMovie(movie: string): string[] {
+//   checkIfMovieWasWatched(movie: string): string[] {
 //     return this.videoList[movie];
 //   },
-//   get getName() {
+//   get getUserName() {
 //     return this.userName;
 //   },
 //   set addNewMovie(movie: string) {
@@ -62,4 +78,4 @@ function addMovieAsViewedToPerson(movie, user) {
 // };
 var word = "hello";
 var upperWord = word.toLowerCase().charAt(0).toUpperCase() + word.slice(1);
-console.log(upperWord);
+// console.log(upperWord);
