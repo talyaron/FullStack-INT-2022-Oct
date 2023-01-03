c694ca3bc2e6765a78a4eaef25c72ae9ef7127dd
Tanya1786 <tanya1786@gmail.com>
2023-01-03 16:23:19 +0200

    HW

---
+++ 02-TypeScript/07-functions/puzzles-HW/Tanya Holczer/dist/index.js
@@ -0,0 +1,34 @@
+// Level 1:
+var number = prompt("Enter a number");
+console.log("The invert value of ", number, "is: ", number * (-1));
+var number2 = prompt("Enter a second number");
+console.log("The square root of ", number2, "is: ", Math.sqrt(number2));
+var celsius = prompt("Enter degrees in Celsius");
+console.log("Degrees in Fahrenheit: ", celsius * 1.8 + 32);
+//Level 2:
+var integer1 = prompt("Enter first integer");
+var integer2 = prompt("Enter second integer");
+if (integer1 > integer2) {
+    console.log(integer1, "is bigger than ", integer2);
+}
+else {
+    console.log(integer2, "is bigger than ", integer1);
+}
+var gender = prompt("Are you a male or a female?");
+var height = prompt("Enter your height in cm");
+if (gender === "male") {
+    if (height > 174) {
+        console.log("your height is ", height - 174, " cm above average");
+    }
+    else {
+        console.log("Your height is ", 174 - height, " cm below average");
+    }
+}
+else {
+    if (height >= 160) {
+        console.log("your height is ", height - 160, " cm above average");
+    }
+    else {
+        console.log("Your height is ", 160 - height, " cm below average");
+    }
+}
