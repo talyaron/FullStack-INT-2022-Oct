7ce8a6919a253ddf8f1828809d342631a83f728c
itayz1e <itayz1e@gmail.com>
2023-01-04 22:32:39 +0200

    HW-Final

--- 02-TypeScript/08-objects/Home-puzzles/itayAmosi/index.ts
+++ 02-TypeScript/08-objects/Home-puzzles/itayAmosi/index.ts
@@ -52,17 +52,24 @@ const matan: Users = {
   Address: 'Moses Nussbaum, 23, Tel Aviv',
   phone: 2232325,
 }
-
-const Welcome = prompt(`Welcome to BLOCKBUSTER
+const Welcome =prompt(`Welcome to BLOCKBUSTER
 Enter a username and click OK`)
-if(Welcome == " " || !Welcome){
-  alert(`It is not possible to register without a username
+const enter: Function = () => {
+  try {
+
+    if (Welcome == ' ' || !Welcome) {
+      alert(`It is not possible to register without a username
   Please refresh the site and enter a username again`)
+enter()
+    } else {
+      alert(`Hi,${Welcome},
+      thanks for signing up!`)
+      return Welcome
+    }
+  } catch (error) {}
 }
-
-
+enter()
 console.log(`A username has been entered: ${Welcome}`)
-const Thanks = alert(`Hi ${Welcome}, thanks for signing up`)
 const dollarToShekel: Function = (dTS: number): number | false => {
   try {
     return dTS * 3.35
@@ -72,77 +79,82 @@ const dollarToShekel: Function = (dTS: number): number | false => {
     return false
   }
 }
-const myButton1:any = document.getElementById('Black_Adam');
-const clickedRent = myButton1.click;
-myButton1.addEventListener(`click`, function (e) {+
-  alert(`this movie rent by ${matan.name}
+const myButton1: any = document.getElementById('Black_Adam')
+const clickedRent = myButton1.click
+myButton1.addEventListener(`click`, function (e) {
+  ;+alert(`this movie rent by ${matan.name}
   is ${matan.age} years old
   and is lives in ${matan.Address}
-  is phone number ${matan.phone}`);
-});
-const myButton2:any = document.getElementById('Spirited');
-const clickedNoRent = myButton2.click;
+  is phone number ${matan.phone}`)
+})
+const myButton2: any = document.getElementById('Spirited')
+const clickedNoRent = myButton2.click
 myButton2.addEventListener(`click`, function (e) {
   alert(
     `this move is no rent by nobody!
     You can rent the movie for ${Spirited.parice} Dollars,
-    (Enter OK to Continue to Rent)`)
-    if(myButton2){
-      return alert(`Thanks ${Welcome} You rented the movie for ${Spirited.parice},
+    (Enter OK to Continue to Rent)`,
+  )
+  if (myButton2) {
+    return alert(`Thanks ${Welcome} You rented the movie for ${Spirited.parice},
       by the way the price in shekels is ${dollarToShekel(Spirited.parice)}.`)
-    }
-});
-const myButton3:any = document.getElementById('Emancipation');
-const clickedRent2 = myButton3.click;
-myButton3.addEventListener(`click`, function (e) {+
-  alert(`this movie rent by ${tomer.name}
+  }
+})
+const myButton3: any = document.getElementById('Emancipation')
+const clickedRent2 = myButton3.click
+myButton3.addEventListener(`click`, function (e) {
+  ;+alert(`this movie rent by ${tomer.name}
   is ${tomer.age} years old
   and is lives in ${tomer.Address}
-  is phone number ${tomer.phone}`);
-});
-const comingSoon:any = document.getElementById('Nope');
-const notArrived = comingSoon.click;
+  is phone number ${tomer.phone}`)
+})
+const comingSoon: any = document.getElementById('Nope')
+const notArrived = comingSoon.click
 comingSoon.addEventListener(`click`, function (e) {
-  alert(`this movie Not Arrived`);
-});
-const checkMoney1:any = document.getElementById('checkMoney1');
-const clicked1 = checkMoney1.click;
+  alert(`this movie Not Arrived`)
+})
+const comingSoon2: any = document.getElementById('White_Noise')
+const notArrived2 = comingSoon2.click
+comingSoon2.addEventListener(`click`, function (e) {
+  alert(`this movie Not Arrived`)
+})
+const comingSoon3: any = document.getElementById('Strange_World')
+const notArrived3 = comingSoon3.click
+comingSoon3.addEventListener(`click`, function (e) {
+  alert(`this movie Not Arrived`)
+})
+const checkMoney1: any = document.getElementById('checkMoney1')
+const clicked1 = checkMoney1.click
 checkMoney1.addEventListener(`click`, function (e) {
   alert(`44.99 dollars,
   Click OK for the rental and the price in shekels `)
   try {
-    if (myButton1){
+    if (myButton1) {
       return alert(`This movie is already rented by ${matan.name},
        by the way its price is ${dollarToShekel(Black_Adam.parice)} Shekels`)
     }
-  } catch (error) {
-    
-  }
-});
-const checkMoney2:any = document.getElementById('checkMoney2');
-const clicked2 = checkMoney2.click;
+  } catch (error) {}
+})
+const checkMoney2: any = document.getElementById('checkMoney2')
+const clicked2 = checkMoney2.click
 checkMoney2.addEventListener(`click`, function (e) {
   alert(`${Spirited.parice} Dollars,
   Click OK for the rental and the price in shekels `)
   try {
-    if (myButton1){
+    if (myButton1) {
       return alert(`${dollarToShekel(Spirited.parice)} in shekel`)
     }
-  } catch (error) {
-    
-  }
-});
-const checkMoney3:any = document.getElementById('checkMoney3');
-const clicked3 = checkMoney3.click;
+  } catch (error) {}
+})
+const checkMoney3: any = document.getElementById('checkMoney3')
+const clicked3 = checkMoney3.click
 checkMoney3.addEventListener(`click`, function (e) {
   alert(`${Emancipation.parice} Dollars,
   Click OK for the rental and the price in shekels `)
   try {
-    if (myButton3){
+    if (myButton3) {
       return alert(`This movie is already rented by ${tomer.name},
        by the way its price is ${dollarToShekel(Emancipation.parice)} Shekels`)
     }
-  } catch (error) {
-    
-  }
-});
+  } catch (error) {}
+})
