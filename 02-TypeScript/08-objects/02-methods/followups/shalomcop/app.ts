e63379492c51a5c41e247233aaa7cc700943f044
Shalom Copperman <shalomcop@gmail.com>
2023-01-05 20:02:20 +0200

    wip

--- 02-TypeScript/08-objects/02-methods/followups/shalomcop/app.ts
+++ 02-TypeScript/08-objects/02-methods/followups/shalomcop/app.ts
@@ -1,17 +1,17 @@
 interface Doit {
-    name: string;
-    cardType: string;
-    cardNo: number;
-    cardDate: string;
-    cardIncrypt: Function;
+  name: string;
+  cardType: string;
+  cardNo: number;
+  cardDate: string;
+  cardIncrypt: Function;
 }
 
-const Go:Doit = {
-    name: "shalom",
-    cardType: "mastercard",
-    cardNo: 231,
-    cardDate: "12-07-1985",
-    cardIncrypt : function () {
-        return this.cardNo*12;
-    }
-}
+const Go: Doit = {
+  name: "shalom",
+  cardType: "mastercard",
+  cardNo: 231,
+  cardDate: "12-07-1985",
+  cardIncrypt: function () {
+    return this.cardNo * 12;
+  },
+};
