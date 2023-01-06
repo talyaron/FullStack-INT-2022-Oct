9708a1ac12659855ded61f7627440715a55cfc33
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-01-06 13:02:42 +0100

    finish level 4

--- 02-TypeScript/09-classes/Home-puzzles/VladiBykanovHW/level4/app.ts
+++ 02-TypeScript/09-classes/Home-puzzles/VladiBykanovHW/level4/app.ts
@@ -1,34 +1,65 @@
 class Dates {
-    date: {};
+  date: Date;
+  hour: number;
+  minute: number;
+  seconds: number;
+  year: number;
+  month: number;
+  day: number;
 
-    constructor(date: {}){
-        this.date = date;
-    }
+  constructor(date: Date) {
+    this.date = date;
+    this.hour = date.getHours();
+    this.minute = date.getMinutes();
+    this.seconds = date.getSeconds();
+    this.year = date.getFullYear();
+    this.month = date.getMonth() + 1;
+    this.day = date.getDay() + 1;
+  }
 
-    dateFormat(){
-        const formatedDate = JSON.stringify(this.date).replace(/[A-Z"]/g, ' ').trim().split(' ');
-        // console.log(formatedDate)
-        return `Date Entered: ${formatedDate[0]} at ${formatedDate[1]}`
-    }
+  dateFormat() {
+    const dateToString = JSON.stringify(this.date)
+      .replace(/[A-Z"]/g, " ")
+      .trim()
+      .split(" ");
+    const properDateFormat = `${this.day}/${this.month}/${this.year}`
+    return `Date Entered: ${properDateFormat} at ${dateToString[1]}`;
+  }
 
-    timePassed(){
-        console.log(JSON.stringify(this.date).slice(1, 11).split(''));
-        // .toJSON().slice(11, 23)
-    }
+  timePassed() {
+    const theTimeOfUserInput = this.date.getTime();
+    const currentTime = new Date().getTime();
+    const timePassed = (currentTime - theTimeOfUserInput) / 1000;
+    return `${timePassed} seconds past from that date`
+  }
 }
 
 const aDate = new Date();
 const newDate = new Dates(aDate);
 
-console.log(JSON.stringify(newDate).slice(0,10));
-console.log(aDate);
+console.log(newDate);
+console.log(newDate.hour);
+console.log(newDate.minute);
+console.log(newDate.seconds);
+console.log(newDate.year);
+console.log(newDate.month);
+console.log(newDate.day);
 console.log(newDate.timePassed());
-// console.log(newDate.dateFormat());
+console.log(newDate.dateFormat());
 
-// console.log(JSON.stringify(aDate));
+const userInput = document.querySelector("#userInput") as HTMLInputElement;
+const output = document.querySelector(".output") as HTMLDivElement;
 
-// console.log(JSON.stringify(aDate).split('"').join('').split('T'));
+window.addEventListener("keydown", (e) => {
+  if (e.key == "Enter" && userInput.value) {
+    const currentTime = new Date().getTime() / (1000 * 60);
+    const timeEntered = Date.parse(userInput.value) / (1000 * 60);
+    output.textContent = numberWithCommas(
+      (currentTime - timeEntered).toFixed()
+    );
+  }
+});
 
-
-
-// const newDate = new Dates(aDate);
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
--- 02-TypeScript/09-classes/Home-puzzles/VladiBykanovHW/level4/dist/app.js
+++ 02-TypeScript/09-classes/Home-puzzles/VladiBykanovHW/level4/dist/app.js
@@ -1,24 +1,49 @@
 var Dates = /** @class */ (function () {
     function Dates(date) {
         this.date = date;
+        this.hour = date.getHours();
+        this.minute = date.getMinutes();
+        this.seconds = date.getSeconds();
+        this.year = date.getFullYear();
+        this.month = date.getMonth() + 1;
+        this.day = date.getDay() + 1;
     }
     Dates.prototype.dateFormat = function () {
-        var formatedDate = JSON.stringify(this.date).replace(/[A-Z"]/g, ' ').trim().split(' ');
-        // console.log(formatedDate)
-        return "Date Entered: " + formatedDate[0] + " at " + formatedDate[1];
+        var dateToString = JSON.stringify(this.date)
+            .replace(/[A-Z"]/g, " ")
+            .trim()
+            .split(" ");
+        var properDateFormat = this.day + "/" + this.month + "/" + this.year;
+        return "Date Entered: " + properDateFormat + " at " + dateToString[1];
     };
     Dates.prototype.timePassed = function () {
-        console.log(JSON.stringify(this.date).slice(1, 11).split(''));
-        // .toJSON().slice(11, 23)
+        var theTimeOfUserInput = this.date.getTime();
+        var currentTime = new Date().getTime();
+        var timePassed = (currentTime - theTimeOfUserInput) / 1000;
+        return timePassed + " seconds past from that date";
     };
     return Dates;
 }());
 var aDate = new Date();
 var newDate = new Dates(aDate);
-console.log(JSON.stringify(newDate).slice(0, 10));
-console.log(aDate);
+console.log(newDate);
+console.log(newDate.hour);
+console.log(newDate.minute);
+console.log(newDate.seconds);
+console.log(newDate.year);
+console.log(newDate.month);
+console.log(newDate.day);
 console.log(newDate.timePassed());
-// console.log(newDate.dateFormat());
-// console.log(JSON.stringify(aDate));
-// console.log(JSON.stringify(aDate).split('"').join('').split('T'));
-// const newDate = new Dates(aDate);
+console.log(newDate.dateFormat());
+var userInput = document.querySelector("#userInput");
+var output = document.querySelector(".output");
+window.addEventListener("keydown", function (e) {
+    if (e.key == "Enter" && userInput.value) {
+        var currentTime = new Date().getTime() / (1000 * 60);
+        var timeEntered = Date.parse(userInput.value) / (1000 * 60);
+        output.textContent = numberWithCommas((currentTime - timeEntered).toFixed());
+    }
+});
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
