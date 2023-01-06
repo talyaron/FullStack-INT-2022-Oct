777bc2edec5b6bdc39bb3e1fef09b873b968c675
Noa1806 <noameillet@gmail.com>
2023-01-06 16:32:24 +0200

    WIP

---
+++ 02-TypeScript/09-classes/Home-puzzles/noa-meillet/index.ts
@@ -0,0 +1,44 @@
+/*
+1. create a class of celebs (influencers) with properties: name, genre, account in TikTok, account in Instagram, and the number of followers.
+
+2. based on question number one, create a method to set the number of followers and another method to get the number of followers.
+   Create a few celebs-objects, and set the number of followers. Write to the console each celeb TikTok account.
+
+3. create a function that gets all the celebs and returns the celeb with the most followers. Even better, return a list of celebs, ordered by their number of followers.
+
+4. Create a class for dates. the instnce(object) gets a date and one method returns the date in the format: "dd/mm/yyyy, hh:mm:ss" for instance, gets a date (new Date ())returns "12/1/2023 17:56:12". And another method return in the following format "2 seconds past from that date"
+*/ 
+
+class Celebs{
+    name: string;
+    genre: string;
+    accountInTiktok: string;
+    accountInInstagram: string;
+    numberOfFollower: number;
+
+    constructor(name: string, genre: string, accountInTiktok: string, accountInInstagram: string, numberOfFollower: number,){
+        name= this.name ;
+        genre= this.genre ;
+        accountInTiktok= this.accountInTiktok ;
+        accountInInstagram= this.accountInInstagram ;
+        numberOfFollower= this.numberOfFollower ;
+    }
+
+    setNumOfFollowers(){
+        let numberOfFollower:string | null = null;
+        while(!numberOfFollower || !isNaN(numberOfFollower)){
+            prompt(`enter the number of follower of this celeb:`)
+        } 
+        this.numberOfFollower= parseInt(numberOfFollower);
+        console.log(this.numberOfFollower);
+        return this.numberOfFollower;
+    }
+
+    getNumOfFollowers(){
+        return this.numberOfFollower;
+    }
+}
+
+const shlomiShabat = new Celebs("hlomi Shabat","singer","shlomi123","shlomi123",200);
+shlomiShabat.setNumOfFollowers();
+
