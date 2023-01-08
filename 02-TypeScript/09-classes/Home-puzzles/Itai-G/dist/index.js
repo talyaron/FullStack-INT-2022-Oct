b9db9a9ec7c689f0efb5e673330e34227d96cb20
itai432 <gelberg4322@gmail.com>
2023-01-08 23:14:11 +0200

    HW

---
+++ 02-TypeScript/09-classes/Home-puzzles/Itai-G/dist/index.js
@@ -0,0 +1,22 @@
+// task 1
+var Celebs = /** @class */ (function () {
+    function Celebs(name, age, instagram, tiktok, followersIsntagram, followersTiktok, followers) {
+        this.name = name;
+        this.age = age;
+        this.instagram = instagram;
+        this.tiktok = tiktok;
+        this.followersIsntagram = followersIsntagram;
+        this.followersTiktok = followersTiktok;
+        this.followers = followers;
+    }
+    Celebs.prototype.setFollowers = function (followers) {
+        this.followers = followers;
+    };
+    Celebs.prototype.getFollowers = function () {
+        return this.followers;
+    };
+    return Celebs;
+}());
+var yodaLevi = new Celebs("Yoda Levi", 43, true, false, 150000, 0);
+var fiftiCent = new Celebs("50cent", 47, true, true, 1500000, 3000000);
+console.dir(yodaLevi);
