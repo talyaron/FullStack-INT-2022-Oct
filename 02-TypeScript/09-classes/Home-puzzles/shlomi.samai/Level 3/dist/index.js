b6f371bfbb905b939c43baa9a4d5c765f8cd875e
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-01-09 14:38:07 +0200

    level 3 done

--- 02-TypeScript/09-classes/Home-puzzles/shlomi.samai/Level 3/dist/index.js
+++ 02-TypeScript/09-classes/Home-puzzles/shlomi.samai/Level 3/dist/index.js
@@ -16,7 +16,7 @@ class Celeb {
     }
 }
 const brad = new Celeb("Brad", "Action", 123, 456, 789);
-const james = new Celeb("james", "Drama", 741, 5852, 963);
+const james = new Celeb("james", "Drama", 741, 5852, 2000000);
 const bill = new Celeb("bill", "PF", 321654, 456789, 789);
 const joan = new Celeb("joan", "horror", 962156, 468, 8742);
 brad.getFollowers();
@@ -24,12 +24,23 @@ brad.serFollowers(1000000);
 const celebsArray = [brad, james, bill, joan];
 function sortArray(array) {
     let i = 0;
+    let j = 0;
     let newArray = [];
     for (i = 0; i <= array.length - 1; i++) {
         newArray[i] = array[i].numberOfFollowers;
     }
     const sortedArray = newArray.sort(function (a, b) { return b - a; });
     console.log(sortedArray);
-    return 7;
+    let sortedClasses = [];
+    for (i = 0; i <= sortedArray.length - 1; i++) {
+        // console.log(i)
+        for (j = 0; j <= sortedArray.length - 1; j++) {
+            //console.log(j)
+            if (sortedArray[i] === array[j].numberOfFollowers) {
+                sortedClasses[i] = array[j];
+            }
+        }
+    }
+    return sortedClasses;
 }
 console.log(sortArray(celebsArray));
