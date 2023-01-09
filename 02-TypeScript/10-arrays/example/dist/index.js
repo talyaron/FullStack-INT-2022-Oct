032377683117a6f2d81fab3bea8d25bb6ba76edb
Tal Yaron <tal.yaron@gmail.com>
2023-01-09 18:58:43 +0200

    array methods

--- 02-TypeScript/10-arrays/example/dist/index.js
+++ 02-TypeScript/10-arrays/example/dist/index.js
@@ -1,13 +1,31 @@
 var Person = /** @class */ (function () {
-    function Person(name) {
+    function Person(name, age, gender) {
         this.name = name;
+        this.age = age;
+        this.gender = gender;
     }
     return Person;
 }());
-var noam = new Person("noam");
-var ruth = new Person("Ruth");
-var dan = new Person("Dan");
+var noam = new Person("noam", 33, "male");
+var ruth = new Person("Ruth", 27, "female");
+var dan = new Person("Dan", 41, "male");
 /// we use lists to hold togther many elements
 var persons = [noam, ruth, dan];
 console.log(persons);
 console.log(persons[1]);
+console.log(persons.length);
+//sort
+persons.sort(function (a, b) { return a.age - b.age; });
+console.log(persons);
+//filter
+var personsAbove30 = persons.filter(function (person) { return person.age > 30; });
+console.log(personsAbove30);
+//callback
+function logPerson(person) {
+    console.log(person.name, person.age);
+}
+persons.forEach(logPerson);
+persons.forEach(function (person) {
+    console.log(person.name, person.age);
+});
+console.log(persons.filter(function (person) { return person.gender === "female"; }));
