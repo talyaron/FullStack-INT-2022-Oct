27502370389e37b178483c31e9a5a8b14c96b8b3
Tal Yaron <tal.yaron@gmail.com>
2023-01-09 17:29:33 +0200

    Auto stash before merge of "main" and "origin/main"

--- 02-TypeScript/09-classes/Home-puzzles/tomer-cohen/dist/index.js
+++ 02-TypeScript/09-classes/Home-puzzles/tomer-cohen/dist/index.js
@@ -1,20 +1,40 @@
-var Celebs = /** @class */ (function () {
-    function Celebs(name, genre, nameTikTok, nameInstagram, numOfFollowers) {
-        name = this.name;
-        genre = this.genre;
-        nameTikTok = this.nameTikTok;
-        nameInstagram = this.nameInstegram;
-        numOfFollowers = this.numOfFollowers;
+var Celeb = /** @class */ (function () {
+    function Celeb(name, genre, tiktok, insta, numberFollowers) {
+        this.name = name;
+        this.genre = genre;
+        this.tiktok = tiktok;
+        this.insta = insta;
+        this.numberFollowers = numberFollowers;
     }
-    Celebs.prototype.setFollowers = function (followers) {
-        this.numOfFollowers = followers;
+    Celeb.prototype.setNumFollowers = function (number) {
+        return (this.numberFollowers = number);
     };
-    Celebs.prototype.getFollowers = function () {
-        return this.numOfFollowers;
+    Celeb.prototype.getNumFollowers = function () {
+        return this.numberFollowers;
     };
-    return Celebs;
+    return Celeb;
 }());
-var kendalljenner = new Celebs("kendall jenner", "model", "kendalljenner", "kendalljenner", 269);
-var tomer = new Celebs("tomer", "tomer", "tpmer", "tomer", 421);
-console.dir(kendalljenner);
+var kendalljenner = new Celeb("kendall jenner", "model", "kendalljenner", "kendalljenner", 269000000);
+var tomer = new Celeb("Tomer", "Male", "TomerTiktok", "tomerInsta", 1000000);
+var messi = new Celeb("messi", "male", "leomessi", "leomessi", 460000000);
+var r9 = new Celeb("ronaldo", "male", "r9", "r9", 11000000);
 console.log(tomer);
+console.dir(kendalljenner);
+console.dir(messi);
+console.dir(r9);
+var sortby = [tomer, messi, kendalljenner, r9];
+var lol = [tomer, kendalljenner, r9];
+console.log(sortby[0].numberFollowers);
+function sortfollowers(arry) {
+    var mostfollowers = 0;
+    var celebrty;
+    for (var i = 0; i < arry.length; i++) {
+        if (arry[i].numberFollowers > mostfollowers) {
+            mostfollowers = arry[i].numberFollowers;
+            celebrty = arry[i].name;
+        }
+    }
+    return celebrty;
+}
+console.log(sortfollowers(sortby));
+console.log(sortfollowers(lol));
--- 02-TypeScript/09-classes/Home-puzzles/tomer-cohen/index.ts
+++ 02-TypeScript/09-classes/Home-puzzles/tomer-cohen/index.ts
@@ -1,40 +1,65 @@
-class Celebs {
+class Celeb {
   name: string;
   genre: string;
-  nameTikTok: string;
-  nameInstegram: string;
-  numOfFollowers: number;
+  tiktok: string;
+  insta: string;
+  numberFollowers: number;
   constructor(
     name: string,
     genre: string,
-    nameTikTok: string,
-    nameInstagram: string,
-    numOfFollowers: number
+    tiktok: string,
+    insta: string,
+    numberFollowers: number
   ) {
-    name = this.name;
-    genre = this.genre;
-    nameTikTok = this.nameTikTok;
-    nameInstagram = this.nameInstegram;
-    numOfFollowers = this.numOfFollowers;
+    this.name = name;
+    this.genre = genre;
+    this.tiktok = tiktok;
+    this.insta = insta;
+    this.numberFollowers = numberFollowers;
   }
-  setFollowers(followers: number) {
-    this.numOfFollowers = followers;
+
+  setNumFollowers(number: number) {
+    return (this.numberFollowers = number);
   }
-  
-  getFollowers(): number  {
-    return this.numOfFollowers;
+
+  getNumFollowers() {
+    return this.numberFollowers;
   }
 }
 
-const kendalljenner = new Celebs(
+const kendalljenner = new Celeb(
   `kendall jenner`,
   `model`,
   `kendalljenner`,
   `kendalljenner`,
-  269
+  269000000
 );
-const tomer= new Celebs(`tomer`,`tomer`,`tpmer`,`tomer`,421)
-console.dir(kendalljenner)
-console.log(tomer)
 
+const tomer = new Celeb("Tomer", "Male", "TomerTiktok", "tomerInsta", 1000000);
+const messi = new Celeb(`messi`, `male`, `leomessi`, `leomessi`, 460000000);
+const r9 = new Celeb(`ronaldo`, `male`, `r9`, `r9`, 11000000);
 
+console.log(tomer);
+console.dir(kendalljenner);
+console.dir(messi);
+console.dir(r9);
+
+const sortby = [tomer, messi, kendalljenner, r9];
+const lol = [tomer, kendalljenner, r9];
+
+console.log(sortby[0].numberFollowers);
+
+function sortfollowers(arry) {
+  let mostfollowers = 0;
+  let celebrty;
+  for (let i = 0; i < arry.length; i++) {
+    if (arry[i].numberFollowers > mostfollowers) {
+      mostfollowers = arry[i].numberFollowers;
+      celebrty = arry[i].name;
+    }
+  }
+  return celebrty;
+}
+
+console.log(sortfollowers(sortby));
+console.log(sortfollowers(lol));
