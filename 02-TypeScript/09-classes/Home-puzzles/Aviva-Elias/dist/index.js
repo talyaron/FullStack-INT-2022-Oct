b78eedb0349fe6c176fab383127113e41b0de356
aviva elias <avivas33@gmail.com>
2023-01-09 16:46:07 +0200

    Aviva-Elias: H.W-Classes

---
+++ 02-TypeScript/09-classes/Home-puzzles/Aviva-Elias/dist/index.js
@@ -0,0 +1,50 @@
+var Celeb = /** @class */ (function () {
+    function Celeb(name, genre, instagramAccount, tiktokAccount, follower) {
+        this.name = name;
+        this.genre = genre;
+        this.instagramAccount = instagramAccount;
+        this.tiktokAccount = tiktokAccount;
+        this.follower = follower;
+    }
+    Celeb.prototype.setFollower = function (number) {
+        return this.follower = number;
+    };
+    Celeb.prototype.getFollwer = function () {
+        return this.follower;
+    };
+    Celeb.prototype.whoHasAccount = function () {
+        if (this.tiktokAccount !== "") {
+            return this.tiktokAccount;
+        }
+        else {
+            return "unknown";
+        }
+    };
+    return Celeb;
+}());
+var messi = new Celeb("messi", "sport", "messi_num1", "messi_still-num1", 1000000);
+var bibi = new Celeb("Benjamin Netanyahu ", "politics", "bibi_Still_rule", "bibi_123", 200000);
+var rachel = new Celeb("rachel imenu", "holiness");
+17000000;
+;
+console.log(messi.name + " have " + messi.getFollwer() + " followers");
+var newFollower = messi.setFollower(3000000);
+console.log(messi.name + " update is followers and now they are " + newFollower + " ");
+console.log("Tiktok acount of " + messi.name + " is: " + messi.whoHasAccount());
+console.log("Tiktok acount of " + rachel.name + " is: " + rachel.whoHasAccount());
+var celebsArray = [messi, bibi, rachel];
+function clebHightesFollowers() {
+    for (var i = 0; i < celebsArray.length; i++) {
+        if (celebsArray[0].follower > (celebsArray[1].follower) && celebsArray[0].follower > (celebsArray[2].follower)) {
+            return celebsArray[0].name;
+        }
+        else if (celebsArray[1].follower > (celebsArray[2].follower) && (celebsArray[1].follower > celebsArray[0].follower)) {
+            return celebsArray[1].name;
+        }
+        else {
+            return celebsArray[2].name;
+        }
+    }
+}
+var result = clebHightesFollowers();
+console.log(result + " have The hightes num of followers :" + celebsArray[2].follower);
---
+++ 02-TypeScript/09-classes/Home-puzzles/Aviva-Elias/index.ts
@@ -0,0 +1,74 @@
+class Celeb {
+    name: string;
+    genre: string;
+    instagramAccount?: string;
+    tiktokAccount?: string;
+    follower: number;
+
+
+    constructor(name: string, genre: string, instagramAccount: string, tiktokAccount: string, follower: number) {
+        this.name = name;
+        this.genre = genre;
+        this.instagramAccount = instagramAccount;
+        this.tiktokAccount = tiktokAccount;
+        this.follower = follower
+
+    }
+    setFollower(number:number):number{
+return this.follower=number
+    }
+
+    getFollwer(){
+        return this.follower
+    }
+   
+    whoHasAccount(){
+    
+        if(this.tiktokAccount!==""){
+            return this.tiktokAccount
+        }
+        else{
+            return "unknown"
+        }
+        
+        } 
+    
+
+}
+
+
+
+
+    const messi = new Celeb("messi", "sport", "messi_num1", "messi_still-num1", 1000000);
+    const bibi = new Celeb("Benjamin Netanyahu ", "politics", "bibi_Still_rule", "bibi_123", 200000);
+    const rachel = new Celeb("rachel imenu", "holiness", , 
+    
+    , 17000000);
+
+
+console.log( `${messi.name} have ${messi.getFollwer()} followers`)
+const newFollower= messi.setFollower(3000000)
+console.log(`${messi.name} update is followers and now they are ${newFollower} `)
+
+
+console.log(`Tiktok acount of ${messi.name} is: ${messi.whoHasAccount()}`)
+console.log(`Tiktok acount of ${rachel.name} is: ${rachel.whoHasAccount()}`)
+  
+const celebsArray:Celeb[]=[messi,bibi,rachel];
+
+function  clebHightesFollowers(){
+    for(let i=0; i< celebsArray.length;i++){
+    
+        if (celebsArray[0].follower>(celebsArray[1].follower)&&celebsArray[0].follower>(celebsArray[2].follower)){
+            return celebsArray[0].name
+        }
+        else if(celebsArray[1].follower>(celebsArray[2].follower)&&(celebsArray[1].follower>celebsArray[0].follower)){
+            return celebsArray[1].name
+        }
+        else{return celebsArray[2].name}
+    }
+    
+    }
+
+const result=clebHightesFollowers()
+console.log(`${result} have The hightes num of followers :${celebsArray[2].follower}`)
