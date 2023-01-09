592f4398e7cf63ec51c96ff30fbc102a7ef71855
Ayala p <ap47046@gmail.com>
2023-01-09 18:28:59 +0200

    arrays

---
+++ 02-TypeScript/10-arrays/followups/Ayala/index.ts
@@ -0,0 +1,13 @@
+
+
+class Person{
+    constructor(private name:string){}
+}
+
+const david = new Person("david");
+const uri = new Person("uri");
+
+const persons :Person[] =[david,uri]
+console.log(persons)
+console.log(persons[-2])
+
