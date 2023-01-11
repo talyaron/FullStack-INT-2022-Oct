4d124b3eefd93508caf96111fe2aafdc475b8dc1
karin-Levi <karintehrany@gmail.com>
2023-01-11 21:13:29 +0200

    update

---
+++ 02-TypeScript/05-loops/followup/karin-levi/index.ts
@@ -0,0 +1,13 @@
+// for (var i =15; i >=0; i--){
+//     console.log(i);
+// }
+
+
+
+var shouldIContinue: boolean = true;
+
+while (shouldIContinue) {
+    shouldIContinue = confirm("Should I Continue?");
+    console.log(shouldIContinue);
+
+}
