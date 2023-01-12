909b50a2bc46ab86a381c87a464777a02e6ab6e9
Tal Yaron <tal.yaron@gmail.com>
2023-01-12 18:33:05 +0200

    document

--- 02-TypeScript/10-arrays/home-puzzles/john-ben/dist/index.js
+++ 02-TypeScript/10-arrays/home-puzzles/john-ben/dist/index.js
@@ -1,10 +1,3 @@
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
 var Cars = /** @class */ (function () {
     function Cars(name, price, model) {
         this.name = name;
@@ -17,18 +10,38 @@ var tesla_model_s = new Cars("Tesla", 170000, "3");
 var tesla_model_3 = new Cars("Tesla", 370000, "s");
 var tesla_model_x = new Cars("Tesla", 270000, "x");
 var tesla_model_y = new Cars("Tesla", 470000, "y");
-var items = [tesla_model_s, tesla_model_3, tesla_model_x, tesla_model_y];
-function cheapest_price() {
-    var result = items[0].price;
-    for (var i = 0; i < items.length; i++) {
-        if (items[i].price < result) {
-            result = items[i].price;
+var myCars = [
+    tesla_model_s,
+    tesla_model_3,
+    tesla_model_x,
+    tesla_model_y,
+];
+var companyCars = [];
+function cheapest_price(items) {
+    try {
+        if (!Array.isArray(items))
+            throw new Error("Items is not an array");
+        if (items.length === 0)
+            throw new Error("array is empty");
+        var minPrice = items[0].price;
+        var minIndex = 0;
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].price < minPrice) {
+                minPrice = items[i].price;
+                minIndex = i;
+            }
         }
+        return items[minIndex];
     }
-    console.log("Smallest number is: " + result);
+    catch (error) {
+        console.error(error);
+        return false;
+    }
 }
-cheapest_price();
-function rateCars() {
-    return __spreadArrays(items).sort(function (a, b) { return a.price - b.price; });
-}
-console.log(rateCars());
+var cheapestCar = cheapest_price(myCars);
+console.log(cheapestCar);
+console.log('hi');
+// function rateCars() {
+//   return [...items].sort((a, b) => a.price - b.price);
+// }
+// console.log(rateCars());
