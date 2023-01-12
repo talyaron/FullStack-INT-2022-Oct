c73a8fac16f2ab41ff2015e8c34b3651b9c54afd
Kseniya Timoshchuk <kseni12121@gmail.com>
2023-01-12 18:20:39 +0200

    wip

--- 02-TypeScript/10-arrays/class-puzzle/room-5/dist/index.js
+++ 02-TypeScript/10-arrays/class-puzzle/room-5/dist/index.js
@@ -1,89 +1,88 @@
+var proffession;
+(function (proffession) {
+    proffession["FULL_STACK"] = "FULL_STACK";
+    proffession["BACK_END"] = "Back-End";
+    proffession["FRONT_END"] = "FRONT_END";
+})(proffession || (proffession = {}));
 var developers = [
     {
         name: "Gili",
         age: 24,
         salary: 34000,
-        profession: "Full-Stack"
+        profession: "FULL_STACK"
     },
     {
         name: "Shahar",
         age: 23,
         salary: 40000,
-        profession: "Full-Stack"
+        profession: "FULL_STACK"
     },
     {
         name: "Tal",
         age: 30,
         salary: 44000,
-        profession: "Front-End"
+        profession: "FRONT_END"
     },
     {
         name: "Bill",
         age: 80,
         salary: 100000,
-        profession: "Back-End"
+        profession: "BACK_END"
     },
     {
         name: "Amit",
         age: 22,
         salary: 20000,
-        profession: "Front-End"
+        profession: "FRONT_END"
     },
     {
         name: "Katia",
         age: 35,
         salary: 29000,
-        profession: "Back-End"
+        profession: "BACK_END"
     },
     {
         name: "Omer",
         age: 28,
         salary: 36000,
-        profession: "Full-Stack"
+        profession: "FULL_STACK"
     },
     {
         name: "Muhammad",
         age: 25,
         salary: 11000,
-        profession: "Full-Stack"
+        profession: "FULL_STACK"
     },
     {
         name: "Ovad",
         age: 37,
         salary: 39000,
-        profession: "Back-End"
+        profession: "BACK_END"
     },
     {
         name: "Alex",
         age: 20,
         salary: 12000,
-        profession: "Front-End"
+        profession: "FRONT_END"
     },
 ];
 var avgSalaryPerProffesion = function (developers) {
-    var avgFullStackSalary = 0;
-    var avgFrontEndSalary = 0;
-    var avgBackEndSalary = 0;
-    var fullStackIndex = 0;
-    var frontEndIndex = 0;
-    var backEndIndex = 0;
+    var salaryMap = new Map();
     developers.forEach(function (element) {
-        if (element.profession === "Full-Stack") {
-            avgFullStackSalary += element.salary;
-            fullStackIndex++;
+        if (salaryMap.has(element.profession)) {
+            var value = salaryMap.get(element.profession);
+            value.amount++;
+            value.sum += element.salary;
+            value.avg = value.sum / value.amount;
         }
-        else if (element.profession === "Front-End") {
-            avgFrontEndSalary += element.salary;
-            frontEndIndex++;
-        }
-        else if (element.profession === "Back-End") {
-            avgBackEndSalary += element.salary;
-            backEndIndex++;
+        else {
+            salaryMap.set(element.profession, {
+                amount: 1,
+                sum: element.salary,
+                avg: element.salary
+            });
         }
     });
-    avgFullStackSalary = avgFullStackSalary / fullStackIndex;
-    avgFrontEndSalary = avgFrontEndSalary / frontEndIndex;
-    avgBackEndSalary = avgBackEndSalary / backEndIndex;
-    return console.log("the avarge salary of a Full-Stack developer is " + avgFullStackSalary + ", the avarge salary of a Front-End developer is " + avgFrontEndSalary + " and the avarge salary of a Back-End developer is " + avgBackEndSalary);
+    console.log(salaryMap);
 };
 avgSalaryPerProffesion(developers);
