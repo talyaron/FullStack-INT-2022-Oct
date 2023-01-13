e03bf1d8882a67e5f0344cfe0d70275210124fd7
Noa1806 <noameillet@gmail.com>
2023-01-13 15:55:26 +0200

    homework- shop with DOM and function

--- 02-TypeScript/11-DOM Manipulations/class-puzzle/room-2/dist/index.js
+++ 02-TypeScript/11-DOM Manipulations/class-puzzle/room-2/dist/index.js
@@ -4,6 +4,6 @@ var userName = prompt('Enter your name:');
 var adress = prompt('Enter your adress:');
 var email = prompt('Enter your email:');
 var image = prompt('Enter the image url:');
-if (userName && adress && image && email) {
+if (userName && adress && image && email && card) {
     card.innerHTML = "<h3 class=\"details\" id=\"userName\">" + userName + "</h3>\n    <h5 class=\"details\" id=\"adress\">" + adress + "</h5>\n    <h6 class=\"details\" id=\"email\">" + email + "</h6>\n    <img id=\"image\" src='" + image + "' alt=\"image\"/>";
 }
