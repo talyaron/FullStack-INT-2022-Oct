60b85a003c1910f9089e8fb8f1ba9c15507da25f
Shai-li Harniv <shaili6298@gmail.com>
2023-01-13 21:43:23 +0200

    fi

---
+++ 02-TypeScript/11-DOM Manipulations/followups/shai-li/app.ts
@@ -0,0 +1,22 @@
+"use strict";
+
+console.dir(document);
+
+document.body.style.backgroundColor = "wheat";
+console.log(navigator.onLine);
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  console.log(color);
+  return color;
+}
+
+const root: HTMLDivElement | null = document.querySelector("#shmulik");
+if (root) {
+  console.dir(root);
+  root.style.backgroundColor = getRandomColor();
+}
