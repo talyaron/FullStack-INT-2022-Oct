03ce3a63d17c44da49c250cd18064005c3a0fb6e
tomer6633 <tomer66331@gmail.com>
2023-01-14 21:33:57 +0200

    tomer& itay HW

--- 02-TypeScript/11-DOM Manipulations/home puzzles/itayAmosi/task2/dist/index.js
+++ 02-TypeScript/11-DOM Manipulations/home puzzles/itayAmosi/task2/dist/index.js
@@ -1,19 +1,42 @@
-function addImage() {
-    try {
-        var picture = document.querySelector('#picture');
-        console.dir(picture);
-        if (picture)
-            for (var i = 0; i < 5; i++) {
-                var urlImage = prompt("Enter image please");
-                var img = document.createElement('img');
-                img.src = urlImage;
-                document.body.appendChild(img);
-            }
-    }
-    catch (error) {
-        console.error(error);
-        return false;
-    }
-}
-addImage();
-console.log(addImage);
+//task1
+// function addImage() {
+//   try {
+//     const picture: HTMLHeadingElement | null = document.querySelector(
+//       '#picture',
+//     )
+//     console.dir(picture)
+//     if (picture)
+//       for (let i = 0; i < 5; i++) {
+//         let urlImage: any = prompt(`Enter image please`)
+//         var img = document.createElement('img')
+//         img.src = urlImage
+//         document.body.appendChild(img)
+//       }
+//   } catch (error) {
+//     console.error(error)
+//     return false
+//   }
+// }
+// addImage()
+// console.log(addImage)
+//task2
+// function addImage() {
+//   try {
+//     const picture: HTMLHeadingElement | null = document.querySelector(
+//       '#picture',
+//     )
+//     console.dir(picture)
+//     if (picture)
+//       for (let i = 0; i < 5; i++) {
+//         let urlImage: any = prompt(`Enter image please`)
+//         var img = document.createElement('img')
+//         img.src = urlImage
+//         document.body.appendChild(img)
+//       }
+//   } catch (error) {
+//     console.error(error)
+//     return false
+//   }
+// }
+// addImage()
+// console.log(addImage)
