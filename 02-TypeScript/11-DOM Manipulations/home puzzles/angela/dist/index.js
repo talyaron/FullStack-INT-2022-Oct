f6508071cc4fa0f383e547a158469a4870b5125d
angela-zorov <zorov.angela.01@gmail.com>
2023-01-15 18:28:12 +0200

    wip

--- 02-TypeScript/11-DOM Manipulations/home puzzles/angela/dist/index.js
+++ 02-TypeScript/11-DOM Manipulations/home puzzles/angela/dist/index.js
@@ -7,3 +7,16 @@ function closePopup() {
 }
 openPopup();
 closePopup();
+var books = /** @class */ (function () {
+    function books(bookTitle, price, category, auther, iamge) {
+        this.bookTitle = bookTitle;
+        this.price = price;
+        this.category = category;
+        this.auther = auther;
+        this.iamge = iamge;
+    }
+    return books;
+}());
+var items = [];
+var theBlackDahlia = new items("The Black Dahlia", 50.78, "crime", "James Ellroy", "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/0995/9780099537861.jpg");
+var you = new items();
--- 02-TypeScript/11-DOM Manipulations/home puzzles/angela/index.ts
+++ 02-TypeScript/11-DOM Manipulations/home puzzles/angela/index.ts
@@ -10,3 +10,27 @@ function closePopup(){
 }
 openPopup();
 closePopup();
+
+class books{
+    public constructor(
+        public bookTitle:string,
+        public price:number,
+        public category:string,
+        public auther:string,
+        public iamge:string
+        ){}
+ }
+
+ let items:books[]=[];
+
+ const theBlackDahlia = new items(
+    "The Black Dahlia",
+    50.78,
+    "crime",
+    "James Ellroy",
+    "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/0995/9780099537861.jpg"
+
+ )
+ const you = new items(
+    
+ )
