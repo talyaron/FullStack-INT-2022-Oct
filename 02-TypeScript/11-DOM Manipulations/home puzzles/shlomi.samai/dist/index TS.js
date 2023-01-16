8bae1c2d4cd023321939aaebe73ee5119a894863
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-01-16 14:59:52 +0200

    hw

--- 02-TypeScript/11-DOM Manipulations/home puzzles/shlomi.samai/dist/index TS.js
+++ 02-TypeScript/11-DOM Manipulations/home puzzles/shlomi.samai/dist/index TS.js
@@ -1,7 +1,14 @@
+"use strict";
 console.dir(document);
-var imgInput = prompt("what is your URL?");
-var test = document.querySelector("#myFirstDOM");
+const imgInput = prompt("what is your URL?");
+const test = document.querySelector("#myFirstDOM");
 if (imgInput !== null && test) {
-    console.dir(test);
-    test.innerHTML = "<div class=\"myFirstDOM\">\n  <img src='" + imgInput + "'/>\n  </div>";
+    //console.dir(test);
+    test.innerHTML = `<div class="myFirstDOM">
+  <img src='${imgInput}'/>
+  </div>`;
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `<div class="myFirstDOM">
+  <img src='${imgInput}'/>
+  </div>`;
 }
