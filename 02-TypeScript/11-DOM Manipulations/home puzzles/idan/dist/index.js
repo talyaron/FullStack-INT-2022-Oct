4e8befbf2abbd27d33f8b35ea3fc98442cd48538
idan <117864474+JoeBidenMyBeloved@users.noreply.github.com>
2023-01-16 05:54:38 +0200

    hp lovecraft had a cool cat
    im writing all of my stuff on the basis it will never be seen however if someone does see this then if you judge me i think that is wrong
    schizophrenic  ramblings i love china

---
+++ 02-TypeScript/11-DOM Manipulations/home puzzles/idan/dist/index.js
@@ -0,0 +1,34 @@
+//i did not do the thing with the sorting algorithm so i guess ill settle with 2
+var images = document.querySelector("#imgs");
+var numberOfProducts = 5;
+var imgURL = [];
+while (imgURL.length < numberOfProducts) {
+    var userInput = prompt("please paste the url of an image that describes");
+    if (isValidUrl(userInput) && userInput) {
+        //i added "&& userInput" because i dont like the red line there but i am fine with the other red line on the other place because of the try catch
+        imgURL.push(userInput);
+    }
+    else {
+        alert("not a url");
+    }
+}
+if (images) {
+    for (var index = 0; index < numberOfProducts; index++) {
+        images.innerHTML += "<div class=\"container\"><img src = \"" + imgURL[index] + "\"></div>";
+    }
+}
+//amazed this worked^
+//i like copying from the internet also good enough for me
+function isValidUrl(string) {
+    var url;
+    try {
+        url = new URL(string);
+    }
+    catch (_) {
+        return false;
+    }
+    return true;
+}
+//was the alt way i wanted to do it keeping to myself
+//// i do not know why i decided to save the images in an array and not just placed them on screen however i am now commited and i will keep working with what i did even though i dont like it
+////i.e for length is the numberOfImgs and each time check if its a url and then attempt to paste it on screen ill just do it
