b00cc473845e519260d07bf4d293125c3d8e5125
jon <jonathanbenn@outlook.fr>
2023-01-16 21:24:12 +0200

    final

---
+++ 02-TypeScript/11-DOM Manipulations/home puzzles/john-ben/index.ts
@@ -0,0 +1,28 @@
+
+const image: HTMLDivElement |null  = document.querySelector("#image");
+
+function addPhoto() {
+    try {
+
+        let userPhoto: string | null = prompt("Enter your photo please");
+        for (let i = 0; i < 5; i++) {
+
+
+            if (userPhoto == null  || image == null) {
+                alert('you did not enter a Photo address')
+                return new Error('you did not enter a Photo address');
+                
+            }
+
+            else {
+                image.innerHTML += `<img src='${userPhoto}'/>`
+            }
+
+        }
+    } catch (error) {
+        console.error(error)
+        return false
+    }
+}
+addPhoto()
+
