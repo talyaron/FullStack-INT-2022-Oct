028f04e6c5a037ddb99edc0ed890016c34629593
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-01-18 18:07:58 +0200

    HW

---
+++ 02-TypeScript/12-MVC/Home-Puzzles/shlomi.samai/dist/model.js
@@ -0,0 +1,9 @@
+class Product {
+    constructor(name, department, year, pictureURL) {
+        this.name = name;
+        this.department = department;
+        this.year = year;
+        this.pictureURL = pictureURL;
+    }
+}
+const products = [];
---
+++ 02-TypeScript/12-MVC/Home-Puzzles/shlomi.samai/dist/view.js
@@ -0,0 +1,1 @@
+const listSelector = document.querySelector("#list");
