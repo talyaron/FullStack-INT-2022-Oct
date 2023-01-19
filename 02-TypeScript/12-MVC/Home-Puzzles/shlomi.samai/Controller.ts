fc468f038fc2f8d7c560db4311024338b6c137ad
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-01-19 11:35:56 +0200

    hw

--- 02-TypeScript/12-MVC/Home-Puzzles/shlomi.samai/Controller.ts
+++ 02-TypeScript/12-MVC/Home-Puzzles/shlomi.samai/Controller.ts
@@ -1,5 +1,8 @@
+
 function getProduct(){
+    let i=0;  
     try {
+     for (i=0;i<=1;i++) {   
         const nameInput=prompt("what is the name of the item?")
         const departmentInput=prompt("what is the name of the department?")
         const yearInput=prompt("what is the name of the item?")
@@ -8,6 +11,7 @@ function getProduct(){
         const shirt = new Product(nameInput, departmentInput, yearInput, pictureURLInput);
         products.push(shirt)
         console.log(products)
+        }
         return products
 
     } catch (error) {
@@ -18,17 +22,21 @@ function getProduct(){
 
 console.log(products)
 getProduct()
+let j=0;
+let page="";
 
-const inputURL:string=products[0].pictureURL
+for (j=0;j<=products.length-1;j++){
+    page+=`<div class="list"> 
+            <h3>${product[j].name}</h3>
+            <h3>${product[j].department}</h3>
+            <h3>${product[j].year}</h3>
+            <img src='${products[j].pictureURL}'/> 
+            </div>`
+    }
 const html:HTMLDivElement|null=document.querySelector("#list")
 if (html!==null){
 
-  html.innerHTML= `<div class="list"> 
-  <img src='${inputURL}'/>
-  
-  </div>`
-
-}
+  html.innerHTML= page;
 console.log(html)
 
-
+}
