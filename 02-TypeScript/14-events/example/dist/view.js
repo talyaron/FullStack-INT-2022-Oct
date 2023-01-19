6798542bf14261c11300bb84d16c660655e294e9
Tal Yaron <tal.yaron@gmail.com>
2023-01-19 18:54:55 +0200

    events

---
+++ 02-TypeScript/14-events/example/dist/view.js
@@ -0,0 +1,10 @@
+var myBox = document.querySelector("#myBox");
+if (myBox) {
+    myBox.addEventListener("click", function (event) {
+        console.log(event);
+    });
+}
+// const boxes: any = document.querySelectorAll(".box, .box2");
+// console.dir(boxes);
+// boxes.forEach((box) => (box.style.backgroundColor = "pink"));
+// console.dir([]);
