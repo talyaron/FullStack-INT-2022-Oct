c36aeb3174300d985869d32c2ecd1765848aa4f1
Tal Yaron <tal.yaron@gmail.com>
2023-01-19 19:35:11 +0200

    cont

--- 02-TypeScript/14-events/example/dist/view.js
+++ 02-TypeScript/14-events/example/dist/view.js
@@ -3,8 +3,21 @@ if (myBox) {
     myBox.addEventListener("click", function (event) {
         console.log(event);
     });
+    myBox.addEventListener("mousemove", function (event) {
+        myBox.innerText = "(" + event.x + "," + event.y + ")";
+    });
 }
-// const boxes: any = document.querySelectorAll(".box, .box2");
+var boxes = document.querySelectorAll(".box, .box2");
 // console.dir(boxes);
-// boxes.forEach((box) => (box.style.backgroundColor = "pink"));
+boxes.forEach(function (box) {
+    box.addEventListener("mousemove", function (event) {
+        box.innerText = "(" + event.x + "," + event.y + ")";
+    });
+    box.addEventListener("mouseenter", function (event) {
+        box.style.backgroundColor = 'red';
+    });
+    box.addEventListener("mouseout", function (event) {
+        box.style.backgroundColor = 'teal';
+    });
+});
 // console.dir([]);
