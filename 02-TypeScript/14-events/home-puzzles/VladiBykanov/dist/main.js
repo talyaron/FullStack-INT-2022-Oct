0b00a05cd3e0b071d99c3bda88812093250613b3
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-01-21 12:53:38 +0100

    change on pressing box to Xmark or checkMark

--- 02-TypeScript/14-events/home-puzzles/VladiBykanov/dist/main.js
+++ 02-TypeScript/14-events/home-puzzles/VladiBykanov/dist/main.js
@@ -1,3 +1,18 @@
-var boxes = document.querySelectorAll('.box');
+var boxes = document.querySelectorAll(".box");
 var playerOneTurn = true;
 var playerTwoTurn = false;
+boxes.forEach(function (box) { return box.addEventListener("click", togglePress); });
+function togglePress() {
+    var i = document.createElement("i");
+    if (playerOneTurn) {
+        i.classList.add("fa-solid");
+        i.classList.add("fa-xmark");
+    }
+    else {
+        i.classList.add("fa-solid");
+        i.classList.add("fa-check");
+    }
+    this.append(i);
+    playerOneTurn = playerTwoTurn;
+    playerTwoTurn = !playerOneTurn;
+}
--- 02-TypeScript/14-events/home-puzzles/VladiBykanov/main.ts
+++ 02-TypeScript/14-events/home-puzzles/VladiBykanov/main.ts
@@ -1,4 +1,19 @@
-const boxes = document.querySelectorAll('.box') as NodeListOf<HTMLDivElement>;
+const boxes = document.querySelectorAll(".box") as NodeListOf<HTMLDivElement>;
 let playerOneTurn = true;
 let playerTwoTurn = false;
 
+boxes.forEach((box) => box.addEventListener("click", togglePress));
+
+function togglePress() {
+  const i = document.createElement("i") as HTMLIFrameElement;
+  if (playerOneTurn) {
+    i.classList.add("fa-solid");
+    i.classList.add("fa-xmark");
+  } else {
+    i.classList.add("fa-solid");
+    i.classList.add("fa-check");
+  }
+  this.append(i);
+  playerOneTurn = playerTwoTurn;
+  playerTwoTurn = !playerOneTurn;
+}
