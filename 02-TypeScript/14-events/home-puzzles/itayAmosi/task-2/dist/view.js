ffb95d137e9b9a13781f65d5fbd965a8f16856ef
tomer cohen <tomer66331@gmail.com>
2023-01-22 01:05:40 +0200

    tomer backgroundimg

--- 02-TypeScript/14-events/home-puzzles/itayAmosi/task-2/dist/view.js
+++ 02-TypeScript/14-events/home-puzzles/itayAmosi/task-2/dist/view.js
@@ -8,6 +8,7 @@ var boxs = document.querySelectorAll(".box");
 var img = document.querySelector("img");
 boxs.forEach(function (box) {
     box.addEventListener("click", function (event) {
-        box.style.img = "https://picsum.photos/200/301";
+        box.style.backgroundImage = "url(https://img.lovepik.com/free-png/20211105/lovepik-chess-png-image_400298812_wh1200.png)";
+        box.style.backgroundSize = "100px 100px";
     });
 });
--- 02-TypeScript/14-events/home-puzzles/itayAmosi/task-2/view.ts
+++ 02-TypeScript/14-events/home-puzzles/itayAmosi/task-2/view.ts
@@ -10,7 +10,9 @@ const boxs: any = document.querySelectorAll(".box");
 const img = document.querySelector("img"); 
 
 boxs.forEach((box) => {
-  box.addEventListener("click", (event) => {
-    box.style.img = "https://picsum.photos/200/301";
+  box.addEventListener("click",(event) => {
+    box.style.backgroundImage= `url(https://img.lovepik.com/free-png/20211105/lovepik-chess-png-image_400298812_wh1200.png)`;
+    box.style.backgroundSize= `100px 100px`;
+
   });
 });
