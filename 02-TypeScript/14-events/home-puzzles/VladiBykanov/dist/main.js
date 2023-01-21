fbebd9fc7c52cddd92362beab46172cbbd5956e2
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-01-21 13:09:42 +0100

    new game button

--- 02-TypeScript/14-events/home-puzzles/VladiBykanov/dist/main.js
+++ 02-TypeScript/14-events/home-puzzles/VladiBykanov/dist/main.js
@@ -1,8 +1,11 @@
 var boxes = document.querySelectorAll(".box");
+var newGameBtn = document.querySelector(".newGameBtn");
 var playerOneTurn = true;
 var playerTwoTurn = false;
 boxes.forEach(function (box) { return box.addEventListener("click", togglePress); });
 function togglePress() {
+    if (this.classList.contains("marked"))
+        return;
     var i = document.createElement("i");
     if (playerOneTurn) {
         i.classList.add("fa-solid");
@@ -12,7 +15,14 @@ function togglePress() {
         i.classList.add("fa-solid");
         i.classList.add("fa-check");
     }
+    this.classList.add("marked");
     this.append(i);
     playerOneTurn = playerTwoTurn;
     playerTwoTurn = !playerOneTurn;
 }
+newGameBtn.addEventListener("click", function () {
+    boxes.forEach(function (box) {
+        box.replaceChildren();
+        box.classList.remove("marked");
+    });
+});
--- 02-TypeScript/14-events/home-puzzles/VladiBykanov/main.ts
+++ 02-TypeScript/14-events/home-puzzles/VladiBykanov/main.ts
@@ -1,10 +1,12 @@
 const boxes = document.querySelectorAll(".box") as NodeListOf<HTMLDivElement>;
+const newGameBtn = document.querySelector(".newGameBtn") as HTMLButtonElement;
 let playerOneTurn = true;
 let playerTwoTurn = false;
 
 boxes.forEach((box) => box.addEventListener("click", togglePress));
 
 function togglePress() {
+  if (this.classList.contains("marked")) return;
   const i = document.createElement("i") as HTMLIFrameElement;
   if (playerOneTurn) {
     i.classList.add("fa-solid");
@@ -13,7 +15,15 @@ function togglePress() {
     i.classList.add("fa-solid");
     i.classList.add("fa-check");
   }
+  this.classList.add("marked");
   this.append(i);
   playerOneTurn = playerTwoTurn;
   playerTwoTurn = !playerOneTurn;
 }
+
+newGameBtn.addEventListener("click", () => {
+  boxes.forEach((box) => {
+    box.replaceChildren();
+    box.classList.remove("marked");
+  });
+});
