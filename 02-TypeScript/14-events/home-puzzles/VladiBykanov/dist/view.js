9250ee32c91a3a1473110378a9cf77cf68dddcb6
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-01-22 22:04:58 +0100

    moving code

--- 02-TypeScript/14-events/home-puzzles/VladiBykanov/dist/view.js
+++ 02-TypeScript/14-events/home-puzzles/VladiBykanov/dist/view.js
@@ -5,16 +5,6 @@ var playerXScoreSpan = document.querySelector(".playerXScore");
 var playerOScoreSpan = document.querySelector(".playerOScore");
 var playerX = "X";
 var playerO = "O";
-var winnigCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
 var playerOWinMessage = document.querySelector(".playerOWin");
 var playerXWinMessage = document.querySelector(".playerXWin");
 var drawMessage = document.querySelector(".draw");
