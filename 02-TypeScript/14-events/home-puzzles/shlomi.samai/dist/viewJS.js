b27e319e1648c6ec6786d127fd0bc15c7846b2cd
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-01-23 16:22:02 +0200

    final

--- 02-TypeScript/14-events/home-puzzles/shlomi.samai/dist/viewJS.js
+++ 02-TypeScript/14-events/home-puzzles/shlomi.samai/dist/viewJS.js
@@ -1,19 +1,12 @@
-
-
-const Boxes = document.querySelectorAll('.box4')
-
-
+const Boxes = document.querySelectorAll('.box4');
 Boxes.forEach((box4) => {
     if (Boxes) {
-        box4.addEventListener("click", myEvent2)
+        box4.addEventListener("click", myEvent2);
     }
-
-
     function myEvent2() {
-
         if (Boxes) {
             box4.innerHTML = `<div class='box5'>
-   </div>`
+   </div>`;
         }
     }
-})
+});
