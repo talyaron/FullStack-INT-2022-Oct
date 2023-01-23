43d06d639fe02a8a381245b11419c4d2337b0f6c
karin-Levi <karintehrany@gmail.com>
2023-01-23 14:18:28 +0200

    MVC model

---
+++ 02-TypeScript/12-MVC/Home-Puzzles/Karin-levi/dist/model.js
@@ -0,0 +1,11 @@
+var Call = /** @class */ (function () {
+    function Call(name, date, direction, type, contactPicture) {
+        this.name = name;
+        this.date = date;
+        this.direction = direction;
+        this.type = type;
+        this.contactPicture = contactPicture;
+    }
+    return Call;
+}());
+var calls = [];
---
+++ 02-TypeScript/12-MVC/Home-Puzzles/Karin-levi/model.ts
@@ -0,0 +1,11 @@
+class Call {
+    constructor(
+        public name: string,
+        public date: string,
+        public direction: string,
+        public type: string,
+        public contactPicture: string
+    ) { }
+}
+
+const calls: Call[] = [];
