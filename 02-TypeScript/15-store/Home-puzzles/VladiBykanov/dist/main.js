572bb4f73be74215a583c49475cde5c01e199dfd
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-01-24 14:05:05 +0100

    form is working

--- 02-TypeScript/15-store/Home-puzzles/VladiBykanov/dist/main.js
+++ 02-TypeScript/15-store/Home-puzzles/VladiBykanov/dist/main.js
@@ -1,39 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var User = /** @class */ (function () {
     function User(gender, firstName, lastName, password, userName, dateOfBirth, phoneNumber, location, profileImage) {
         this.gender = gender;
@@ -76,60 +40,55 @@ var User = /** @class */ (function () {
     return User;
 }());
 var userList = [];
-function fetchTenUsers() {
-    return __awaiter(this, void 0, void 0, function () {
-        var fetchRadomUser, error_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 2, , 3]);
-                    return [4 /*yield*/, fetch("https://randomuser.me/api/?results=10").then(function (data) {
-                            data.json().then(function (randomUser) {
-                                console.log(randomUser.results);
-                                randomUser.results.forEach(function (user) {
-                                    // const gender = user.gender;
-                                    // const firstName = user.name.first;
-                                    // const lastName = user.name.last;
-                                    // const password = user.login.password;
-                                    // const userName = user.login.username;
-                                    // const dateOfBirth = user.dob.date.slice(0, 10);
-                                    // const phoneNumber = user.cell;
-                                    // const location = user.location.country;
-                                    // const profileImage = user.picture.large;
-                                    var randomUser = new User(user.gender, user.name.first, user.name.last, user.login.password, user.login.username, user.dob.date.slice(0, 10), user.cell, user.location.country, user.picture.large);
-                                });
-                            });
-                        })];
-                case 1:
-                    fetchRadomUser = _a.sent();
-                    return [3 /*break*/, 3];
-                case 2:
-                    error_1 = _a.sent();
-                    console.log(error_1);
-                    return [3 /*break*/, 3];
-                case 3: return [2 /*return*/];
-            }
+var fetchRadomUser = function () {
+    return fetch("https://randomuser.me/api/?results=8").then(function (data) {
+        data.json().then(function (randomUser) {
+            randomUser.results.forEach(function (user) {
+                // const gender = user.gender;
+                // const firstName = user.name.first;
+                // const lastName = user.name.last;
+                // const password = user.login.password;
+                // const userName = user.login.username;
+                // const dateOfBirth = user.dob.date.slice(0, 10);
+                // const phoneNumber = user.cell;
+                // const location = user.location.country;
+                // const profileImage = user.picture.large;
+                var randomUser = new User(user.gender, user.name.first, user.name.last, user.login.password, user.login.username, user.dob.date.slice(0, 10), user.cell, user.location.country, user.picture.large);
+                userList.push(randomUser);
+            });
         });
     });
-}
-fetchTenUsers();
+};
+fetchRadomUser();
+setTimeout(function () {
+    renderUsers(userList);
+}, 300);
 var wrapper = document.querySelector(".cardsWrapper");
 function renderUsers(arr) {
+    wrapper.replaceChildren();
     arr.forEach(function (user) {
         var userCard = document.createElement("div");
         userCard.classList.add("cardsWrapper__userCard");
-        userCard.innerHTML = "<img src=\"" + user.getProfileImg() + "\"/>";
+        userCard.innerHTML = "<img src=\"" + user.getProfileImg() + "\"/>\n    <ul>\n    <li>Gender: " + user.getGender() + "</li>\n    <li>First Name: " + user.getFirstName() + "</li>\n    <li>Last Name: " + user.getLastName() + "</li>\n    <li>Password: " + user.getPassword() + "</li>\n    <li>UserName: " + user.getUserName() + "</li>\n    <li>Date Of Birth: " + user.getDOB() + "</li>\n    <li>Phone Number: " + user.getPhoneNum() + "</li>\n    <li>Location: " + user.getLocation() + "</li>\n    </ul>";
         wrapper.appendChild(userCard);
-        // const img = new Image();
-        // const ulEle = document.createElement("ul") as HTMLUListElement;
-        // const li = document.createElement("li") as HTMLLIElement;
-        // const span = document.createElement("span") as HTMLSpanElement;
-        // li.textContent = user[0];
     });
 }
-// console.log("End of TS");
-var form = document.querySelector('.formSection');
-form.addEventListener('submit', function (e) {
-    e.preventDefault;
-    console.log(e);
-});
+var form = document.querySelector(".addUserForm");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var gender = e.target.elements.gender.value;
+    var firstName = e.target.elements.firstName.value;
+    var lastName = e.target.elements.lastName.value;
+    var password = e.target.elements.password.value;
+    var userName = e.target.elements.userName.value;
+    var dob = e.target.elements.dob.value;
+    var phone = e.target.elements.phone.value;
+    var location = e.target.elements.location.value;
+    var image = "./img/Profile.jpg";
+    console.log(image);
+    var newUser = new User(gender, firstName, lastName, password, userName, dob, phone, location, image);
+    userList.unshift(newUser);
+    renderUsers(userList);
+}
--- 02-TypeScript/15-store/Home-puzzles/VladiBykanov/main.ts
+++ 02-TypeScript/15-store/Home-puzzles/VladiBykanov/main.ts
@@ -5,8 +5,8 @@ class User {
     private lastName: string,
     private password: string,
     private userName: string,
-    private dateOfBirth: Date,
-    private phoneNumber: number,
+    private dateOfBirth: string,
+    private phoneNumber: string,
     private location: string,
     private profileImage: string
   ) {}
@@ -40,66 +40,91 @@ class User {
 }
 
 const userList: User[] = [];
-async function fetchTenUsers() {
-  try {
-    const fetchRadomUser = await fetch(
-      "https://randomuser.me/api/?results=10"
-    ).then((data) => {
-      data.json().then((randomUser) => {
-          console.log(randomUser.results);
-        randomUser.results.forEach((user) => {
-          // const gender = user.gender;
-          // const firstName = user.name.first;
-          // const lastName = user.name.last;
-          // const password = user.login.password;
-          // const userName = user.login.username;
-          // const dateOfBirth = user.dob.date.slice(0, 10);
-          // const phoneNumber = user.cell;
-          // const location = user.location.country;
-          // const profileImage = user.picture.large;
-          const randomUser = new User(
-            user.gender,
-            user.name.first,
-            user.name.last,
-            user.login.password,
-            user.login.username,
-            user.dob.date.slice(0, 10),
-            user.cell,
-            user.location.country,
-            user.picture.large
-          );
-        });
+const fetchRadomUser = () =>
+  fetch("https://randomuser.me/api/?results=8").then((data) => {
+    data.json().then((randomUser) => {
+      randomUser.results.forEach((user) => {
+        // const gender = user.gender;
+        // const firstName = user.name.first;
+        // const lastName = user.name.last;
+        // const password = user.login.password;
+        // const userName = user.login.username;
+        // const dateOfBirth = user.dob.date.slice(0, 10);
+        // const phoneNumber = user.cell;
+        // const location = user.location.country;
+        // const profileImage = user.picture.large;
+        const randomUser = new User(
+          user.gender,
+          user.name.first,
+          user.name.last,
+          user.login.password,
+          user.login.username,
+          user.dob.date.slice(0, 10),
+          user.cell,
+          user.location.country,
+          user.picture.large
+        );
+        userList.push(randomUser);
       });
     });
-  } catch (error) {
-    console.log(error);
-  }
-}
-fetchTenUsers()
+  });
+
+fetchRadomUser();
+setTimeout(() => {
+  renderUsers(userList);
+}, 300);
 
 const wrapper = document.querySelector(".cardsWrapper") as HTMLDivElement;
 
 function renderUsers(arr: User[]) {
+  wrapper.replaceChildren();
   arr.forEach((user) => {
     const userCard = document.createElement("div") as HTMLDivElement;
     userCard.classList.add("cardsWrapper__userCard");
-    userCard.innerHTML = `<img src="${user.getProfileImg()}"/>`;
+    userCard.innerHTML = `<img src="${user.getProfileImg()}"/>
+    <ul>
+    <li>Gender: ${user.getGender()}</li>
+    <li>First Name: ${user.getFirstName()}</li>
+    <li>Last Name: ${user.getLastName()}</li>
+    <li>Password: ${user.getPassword()}</li>
+    <li>UserName: ${user.getUserName()}</li>
+    <li>Date Of Birth: ${user.getDOB()}</li>
+    <li>Phone Number: ${user.getPhoneNum()}</li>
+    <li>Location: ${user.getLocation()}</li>
+    </ul>`;
     wrapper.appendChild(userCard);
-    // const img = new Image();
-    // const ulEle = document.createElement("ul") as HTMLUListElement;
-    // const li = document.createElement("li") as HTMLLIElement;
-    // const span = document.createElement("span") as HTMLSpanElement;
-    // li.textContent = user[0];
   });
 }
 
-// console.log("End of TS");
-const form = document.querySelector('.formSection') as HTMLFormElement;
+const form = document.querySelector(".addUserForm") as HTMLFormElement;
 
-form.addEventListener('submit', handleSubmit)
+form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(e:Event) {
-    e.preventDefault();
+function handleSubmit(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  const gender = e.target.elements.gender.value;
+  const firstName = e.target.elements.firstName.value;
+  const lastName = e.target.elements.lastName.value;
+  const password = e.target.elements.password.value;
+  const userName = e.target.elements.userName.value;
+  const dob = e.target.elements.dob.value;
+  const phone = e.target.elements.phone.value;
+  const location = e.target.elements.location.value;
+  const image = "./img/Profile.jpg";
+  console.log(image);
+  const newUser = new User(
+    gender,
+    firstName,
+    lastName,
+    password,
+    userName,
+    dob,
+    phone,
+    location,
+    image
+  );
+  userList.unshift(newUser);
 
-    console.log('msg');
+  renderUsers(userList);
 }
