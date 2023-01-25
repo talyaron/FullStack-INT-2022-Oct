e9c29f62a669e7913cf7c1a3a68363fb364d9e34
itai432 <gelberg4322@gmail.com>
2023-01-25 23:52:33 +0200

    hw

--- 02-TypeScript/15-store/Home-puzzles/Itai-G/control.ts
+++ 02-TypeScript/15-store/Home-puzzles/Itai-G/control.ts
@@ -9,14 +9,39 @@ function handleUser(event) {
     const email = event.target.elements.email.value;
     const theOfBirth = event.target.elements.theOfBirth.value;
     const country = event.target.elements.country.value;
-    // const profileImage = imgSrc;
+    const profileImage = event.target.elements.profileImage.value;
 
     userList.push(
-      new User(gender, username, password, email, theOfBirth, country)
+      new User(gender, username, password, email, theOfBirth, country, profileImage)
     );
     console.log(userList);
+    renderCard(userList)
     event.target.reset();
   } catch (error) {
     console.error(error);
   }
 }
+
+
+function renderCard(users:User[]):void {
+  try {
+  const divElement = document.querySelector(`.card`) as HTMLElement;
+      users.forEach((user) => {
+      const userCard = document.createElement("div") as HTMLElement;
+      userCard.classList.add("newCard");
+      userCard.innerHTML =` 
+      <ul>
+      <li>Gender: ${user.gender}</li>
+      <li>Username: ${user.username}</li>
+      <li>Password: ${user.password}</li>
+      <li>Email: ${user.email}</li>
+      <li>Date Of Birth: ${user.theOfBirth}</li>
+      <li>country: ${user.country}</li>
+      </ul> <img class="userImg" src="${user.profileImage}"/>`;
+      divElement.appendChild(userCard);
+  });
+  } catch (error) {
+  console.error(error);
+  }
+  }
+  
