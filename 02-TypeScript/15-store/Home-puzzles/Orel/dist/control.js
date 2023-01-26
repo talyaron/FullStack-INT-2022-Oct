06202ac5cd2b74d0e54a02ce48f8b2abcd17cf10
Orekarako <orekarako@gmail.com>
2023-01-26 17:35:11 +0200

    asdas
    da

---
+++ 02-TypeScript/15-store/Home-puzzles/Orel/dist/control.js
@@ -0,0 +1,19 @@
+"use strict";
+function handlerAddUser(ev) {
+    try {
+        ev.preventDefault();
+        const { fullName, address, email, phone, password } = ev.target.elements;
+        users.push(new User(fullName.value, address.value, email.value, phone.value, password.value));
+        ev.target.reset();
+        renderUsers();
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+const inputElement = document.getElementById("file");
+inputElement.addEventListener("change", handleFiles, false);
+function handleFiles() {
+    const fileList = this.files; /* now you can work with the file list */
+    console.log(fileList);
+}
