fa58e39ad844b9ff504443ed2323ccd00e6ea505
itayz1e <itayz1e@gmail.com>
2023-01-28 00:30:53 +0200

    HW-task 2 balloons

---
+++ 02-TypeScript/16-CRUD/home-puzzles/itayAmosi/task 2/dist/control.js
@@ -0,0 +1,49 @@
+var popUpBalloons = function () {
+    var density = 7;
+    var balloons = [];
+    var color = ["yellow", "green", "blue", "gold"];
+    var stringElement = document.createElement("div");
+    stringElement.classList.add("string");
+    for (var i = 0; i < density; i++) {
+        var element = document.createElement("div");
+        element.classList.add("balloon");
+        element.classList.add(randomColor());
+        element.append(stringElement.cloneNode());
+        document.body.append(element);
+        setTimeout(function () {
+            releaseBalloon(balloons);
+        }, (i * 200) * randomColor(500, 1000));
+    }
+    function randomColor() {
+        return color[random(0, color.length - 1)];
+    }
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    function releaseBalloon(balloon) {
+        var delay = random(100, 1000);
+        var x = random(-99, -30);
+        var y = random(-99, -30);
+        var order = [{ outside: 0, transform: "rotateZ(45deg) translate(0,0)" }];
+        if (random(0, 1) === 0) {
+            balloon.style.left = -1 * x + "vw";
+            order.push({ outside: x / -200, transform: "rotateZ(45deg) translate(" + y + "vw,0)" });
+            order.push({ outside: (x + y) / -200, transform: "rotateZ(45deg) translate(" + x + "vw, " + y + "vh)" });
+            order.push({ outside: (-100 + y) / -200, transform: "rotateZ(45deg) translate(-100vw, " + y + "vw)" });
+        }
+        else {
+            order.push({ outside: y / -200, transform: "rotateZ(45deg) translate(" + x + "vh)" });
+            order.push({ outside: (x + y) / -200, transform: "rotateZ(45deg) translate(" + x + "vw, " + y + "vh)" });
+            order.push({ outside: (-100 + x) / -200, transform: "rotateZ(45deg) translate(" + x + "vw), -100vh" });
+        }
+        order.push({ outside: 1, transform: "rotateZ(45deg) translate(-100vh, -100vh)" });
+        var balloonAnimation = balloon.animate(order, { duration: 15000, delay: delay });
+        balloonAnimation.onfinish = function () {
+            {
+                releaseBalloon(balloons);
+            }
+            ;
+        };
+    }
+    ;
+};
