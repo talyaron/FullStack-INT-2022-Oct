8ab2a0a9d4e66ce4c848c7bd591289f338f19e51
itayz1e <itayz1e@gmail.com>
2023-01-30 00:34:09 +0200

    HW- tasck 2 - balloons

--- 02-TypeScript/16-CRUD/home-puzzles/itayAmosi/task 2/dist/helpers.js
+++ 02-TypeScript/16-CRUD/home-puzzles/itayAmosi/task 2/dist/helpers.js
@@ -1,3 +1,9 @@
 function getRandomColor() {
-    return "#" + (((1 << 24) * Math.random()) | 0).toString(16).padStart(6, "0");
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
+var randomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+var getViewportWidth = function () {
+    return parseInt(getComputedStyle(document.body, null).width, 10);
+};
