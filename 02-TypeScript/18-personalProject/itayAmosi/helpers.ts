996698bdadb82ca0601d6be4b9e5b4815494219e
itayz1e <itayz1e@gmail.com>
2023-01-31 22:59:25 +0200

    personal Project progress

---
+++ 02-TypeScript/18-personalProject/itayAmosi/helpers.ts
@@ -0,0 +1,12 @@
+// function uid() {
+//   return Date.now().toString(36) + Math.random().toString(36).substr(2);
+// }
+
+
+// let shots = 0
+
+// // click event for shots
+// document.getElementById('#target')!.addEventListener('click', () => {
+//     shots ++
+// 	document.querySelector('score')!.textContent = shots.toString()
+// })
