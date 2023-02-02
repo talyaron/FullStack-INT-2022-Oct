5e931d953f2be143f0c9c32f400334e7d72ed31d
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-02-02 14:59:48 +0200

    i

--- 02-TypeScript/18-personalProject/Itsik-B/App-N&N/dist/index.js
+++ 02-TypeScript/18-personalProject/Itsik-B/App-N&N/dist/index.js
@@ -1,10 +1,12 @@
 var btn_Log = document.querySelector(".btn_Login");
 var form = document.querySelector("form");
-btn_Log.addEventListener("click", runform);
+form.addEventListener("submit", runform);
 function runform(e) {
     var inp_Name = document.querySelector(".inpn");
     var inp_pass = document.querySelector(".inp_pass");
     e.preventDefault();
+    inp_Name.required;
+    inp_pass.required;
     localStorage.setItem(inp_Name.value, inp_pass.value);
     window.location.href = "app.html";
     startApp();
