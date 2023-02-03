9217674ec78e218132ad786bdc0400a07f2ac849
Vladislav Bykanov <vbykanov1989@outlook.com>
2023-02-03 18:04:48 +0100

    box shadow

--- 02-TypeScript/18-personalProject/VladiBykanov/src/dist/startPage.js
+++ 02-TypeScript/18-personalProject/VladiBykanov/src/dist/startPage.js
@@ -1,7 +1,14 @@
 var userChoice;
+var imgElements = document.querySelectorAll('img');
+console.log(imgElements);
 window.addEventListener("click", function (e) {
     var target = e.target;
+    imgElements.forEach(function (img) {
+        img.style.boxShadow = '0 0 0 black';
+    });
+    console.log(target.className);
     if (target.nodeName == "IMG") {
+        target.style.boxShadow = '0 0 50px black';
         userChoice = target.className;
         localStorage.setItem("userChoice", userChoice);
     }
--- 02-TypeScript/18-personalProject/VladiBykanov/src/startPage.ts
+++ 02-TypeScript/18-personalProject/VladiBykanov/src/startPage.ts
@@ -1,8 +1,15 @@
 let userChoice: string;
-
+const imgElements = document.querySelectorAll('img') as NodeListOf<HTMLElement>
+console.log(imgElements)
 window.addEventListener("click", (e) => {
   const target = e.target as HTMLElement;
+  imgElements.forEach(img => {
+    img.style.boxShadow = '0 0 0 black'
+  })
+  console.log(target.className);
+
   if (target.nodeName == "IMG") {
+    target.style.boxShadow = '0 0 50px black'
     userChoice = target.className;
     localStorage.setItem("userChoice", userChoice);
   }
@@ -10,5 +17,4 @@ window.addEventListener("click", (e) => {
   if (target.innerHTML === "START") {
     window.location.href = "index.html";
   }
-
 });
