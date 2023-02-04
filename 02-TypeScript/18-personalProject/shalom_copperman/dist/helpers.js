eec1f2fe3c13fc14127b007880b6260da102df45
Shalom Copperman <shalomcop@gmail.com>
2023-02-04 23:36:29 +0200

    wip

--- 02-TypeScript/18-personalProject/shalom_copperman/dist/helpers.js
+++ 02-TypeScript/18-personalProject/shalom_copperman/dist/helpers.js
@@ -1,3 +1,23 @@
-function uid() {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+function uid(length) {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz".split("");
+    if (!length) {
+        length = Math.floor(Math.random() * chars.length);
+    }
+    var str = "";
+    for (var i = 0; i < length; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+function dropdown(arr, selectId) {
+    var select = document.getElementById(selectId);
+    for (var i = 0; i < arr.length; i++) {
+        var option = document.createElement("option");
+        var txt = document.createTextNode(arr[i]);
+        option.appendChild(txt);
+        option.setAttribute("value", arr[i]);
+        if (select) {
+            select.insertBefore(option, select.lastChild);
+        }
+    }
 }
