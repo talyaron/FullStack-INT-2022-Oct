cd3376b67720059377857d2f8006f0390d499d12
Raanan M <yraanan1@gmail.com>
2023-02-06 15:01:11 +0200

    update

--- 02-TypeScript/18-personalProject/Raanan/dist/index.js
+++ 02-TypeScript/18-personalProject/Raanan/dist/index.js
@@ -1,19 +1,40 @@
-var displayMovies = function (movies) {
-    var movieList = document.querySelector("#movieList");
-    movieList.innerHTML = "";
-    movies.forEach(function (movie) {
-        var movieItem = document.createElement("li");
-        movieItem.innerHTML = "\n        <h3>" + movie.title + "</h3>\n        <p>" + movie.description + "</p>\n        <a href=\"#\">Read Review</a>\n        <button class=\"editMovie\">Edit</button>\n      ";
-        movieItem.querySelector(".editMovie").addEventListener("click", function () {
-            var newTitle = prompt("Enter the new title:");
-            var newDescription = prompt("Enter the new description:");
-            if (newTitle && newDescription) {
-                movie.title = newTitle;
-                movie.description = newDescription;
-                displayMovies(latestMovies);
-            }
-        });
-        movieList.appendChild(movieItem);
-    });
-};
-displayMovies(latestMovies);
+var movieList = document.querySelector(".movie-list");
+var movieTitle = document.querySelector(".movie-title");
+var movieDescription = document.querySelector(".movie-description");
+function displayMovieInfo(title, description) {
+    try {
+        movieTitle.textContent = title;
+        movieDescription.textContent = description;
+    }
+    catch (error) {
+        console.error("An error occurred while updating movie info:", error);
+    }
+}
+function addMovieToList(title, description) {
+    try {
+        var movieEl = movieList.querySelector(".movie-item") || document.createElement("div");
+        movieEl.classList.add("movie-item");
+        movieEl.innerHTML = "\n      <h3>" + title + "</h3>\n      <p>" + description + "</p>\n    ";
+        movieList.appendChild(movieEl);
+    }
+    catch (error) {
+        console.error("An error occurred while adding movie to list:", error);
+    }
+}
+function toggleDarkLightMode() {
+    try {
+        document.body.classList.toggle("dark-theme");
+    }
+    catch (error) {
+        console.error("An error occurred while toggling theme:", error);
+    }
+}
+function loadMovieData() {
+    try {
+        // TODO: Load movie data from a source (e.g. API, local storage, etc.)
+    }
+    catch (error) {
+        console.error("An error occurred while loading movie data:", error);
+    }
+}
+// TODO: Add event listeners for buttons and other elements
--- 02-TypeScript/18-personalProject/Raanan/index.ts
+++ 02-TypeScript/18-personalProject/Raanan/index.ts
@@ -1,26 +1,25 @@
 const displayMovies = (movies: Movie[]) => {
-    const movieList: Element | null = document.querySelector("#movieList");
-    movieList.innerHTML = "";
-    movies.forEach((movie) => {
-      const movieItem: Element | null = document.createElement("li");
-      movieItem.innerHTML = `
-        <h3>${movie.title}</h3>
-        <p>${movie.description}</p>
-        <a href="#">Read Review</a>
-        <button class="editMovie">Edit</button>
-      `;
-      movieItem.querySelector(".editMovie").addEventListener("click", () => {
-        const newTitle = prompt("Enter the new title:");
-        const newDescription = prompt("Enter the new description:");
-        if (newTitle && newDescription) {
-          movie.title = newTitle;
-          movie.description = newDescription;
-          displayMovies(latestMovies);
-        }
-      });
-      movieList.appendChild(movieItem);
+  const movieList: Element | null = document.querySelector("#movieList");
+  movieList.innerHTML = "";
+  movies.forEach((movie) => {
+    const movieItem: Element | null = document.createElement("li");
+    movieItem.innerHTML = `
+      <h3>${movie.title}</h3>
+      <p>${movie.description}</p>
+      <a href="#">Read Review</a>
+      <button class="editMovie">Edit</button>
+    `;
+    movieItem.querySelector(".editMovie").addEventListener("click", () => {
+      const newTitle = prompt("Enter the new title:");
+      const newDescription = prompt("Enter the new description:");
+      if (newTitle && newDescription) {
+        movie.title = newTitle;
+        movie.description = newDescription;
+        displayMovies(latestMovies);
+      }
     });
-  };
-  
-  displayMovies(latestMovies);
-  
+    movieList.appendChild(movieItem);
+  });
+};
+
+displayMovies(latestMovies);
