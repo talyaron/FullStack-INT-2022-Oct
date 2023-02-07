c27eb4cbea8c3520e7be195d8dec756c1d046ceb
Tal Yaron <tal.yaron@gmail.com>
2023-02-07 10:19:20 +0200

    cont

--- 02-TypeScript/10-arrays/OOP-arrays/example/dist/index.js
+++ 02-TypeScript/10-arrays/OOP-arrays/example/dist/index.js
@@ -27,5 +27,20 @@ var buyers = [];
 var lastDay = new Date().getTime() - 24 * 60 * 60 * 1000;
 var lastDayCarts = carts.filter(function (cart) { return cart.created.getTime() > lastDay; });
 //give me the cart of user with spesific ID
-var id = '3423534';
+var id = "3423534";
 var buyerCart = carts.find(function (cart) { return cart.buyer.id === id; });
+//localstorage
+var tal = new Buyer("1234", "Tal");
+buyers.push(tal);
+carts.push(new Cart(tal));
+//save to local storage;
+localStorage.setItem("buyers", JSON.stringify(buyers));
+localStorage.setItem("carts", JSON.stringify(carts));
+//add buyer
+buyers.push(new Buyer("2334", "nikita"));
+localStorage.setItem("buyers", JSON.stringify(buyers));
+var _buyers = localStorage.getItem("buyers");
+if (_buyers) {
+    _buyers = JSON.parse(_buyers);
+    console.log(_buyers);
+}
---
+++ 02-TypeScript/20-TeamProject/example/dist/model.js
@@ -0,0 +1,10 @@
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+var Song = /** @class */ (function () {
+    function Song() {
+    }
+    return Song;
+}());
