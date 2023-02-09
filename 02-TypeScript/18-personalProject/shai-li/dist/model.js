3fdc2de95ff76540a3e6633e69054401f5d7b71a
Shai-li Harniv <shaili6298@gmail.com>
2023-02-09 08:04:17 +0200

    k

---
+++ 02-TypeScript/18-personalProject/shai-li/dist/model.js
@@ -0,0 +1,9 @@
+var Task = /** @class */ (function () {
+    function Task(title, description, dueDate, status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+    return Task;
+}());
