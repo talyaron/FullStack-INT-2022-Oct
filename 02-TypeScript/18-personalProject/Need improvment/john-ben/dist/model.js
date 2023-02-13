b30b3da717e390b49c6e8ed79fd95fba02a1a30e
jon <jonathanbenn@outlook.fr>
2023-02-13 12:34:54 +0200

    final

---
+++ 02-TypeScript/18-personalProject/Need improvment/john-ben/dist/model.js
@@ -0,0 +1,11 @@
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
+var users = [];
+// items.push(
+//   new Item("addias z45", "blue", 120, "shoes", 44, "fgdfgsdg543534t345")
+// );
+console.log(users);
