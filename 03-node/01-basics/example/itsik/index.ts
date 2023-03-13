57aff7e75e98b0acd9e99cba41504adfe2d86b4a
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-03-13 19:55:52 +0200

    itsik

--- 03-node/01-basics/example/itsik/index.ts
+++ 03-node/01-basics/example/itsik/index.ts
@@ -1,5 +1,15 @@
 import express from "express";
+const app = express()
+const PORT = 3000;
 
 const numbers = [1, 2, 3, 4, 5] 
+app.get('/', (req, res) => {
+    res.send('working')
+  })
+
+  app.listen(PORT, ()=>{
+    console.log(`port listen at: ${PORT}`)
+})  
+
 
 console.log(numbers)
