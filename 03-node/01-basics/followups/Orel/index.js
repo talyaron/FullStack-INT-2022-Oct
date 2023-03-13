bf78b7b3072f45a07916bc462dba0b55cd8fa931
Orekarako <orekarako@gmail.com>
2023-03-13 20:29:47 +0200

    asdasd

---
+++ 03-node/01-basics/followups/Orel/index.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+
+// fs.writeFile('text.txt' , "OrelKarakoSADsadqwd" , (err) => {
+//     if(err) throw err ;
+//     console.log("saved!");
+// });
+
+
+fs.appendFile('text.txt', ' asdasdasdasdasd new One.', function (err) {
+    if (err) throw err;
+    console.log('Updated!');
+  });
+
+  fs.open('mynewfile2.ts', 'w', function (err, file) {
+    if (err) throw err;
+    console.log('Saved!');
+    console.log(file);
+  });
