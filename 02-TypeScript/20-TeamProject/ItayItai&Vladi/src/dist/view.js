615cf538b459b94f247b7ce6eef8b5cccd2a844f
itayz1e <itayz1e@gmail.com>
2023-03-15 13:35:27 +0200

    fix bugs

---
+++ 02-TypeScript/20-TeamProject/ItayItai&Vladi/src/dist/view.js
@@ -0,0 +1,51 @@
+// Entry Page elements
+var signInForm = document.querySelector("#signInForm");
+var signUpForm = document.querySelector("#signUpForm");
+var entryPageMainContainer = document.querySelector(".mainContainer");
+var signUpPanelBtn = document.querySelector("#signUpPanel");
+var signInPanelBtn = document.querySelector("#signInPanel");
+var signInBtn = document.querySelector("#signInBtn");
+var userNameInput = document.querySelector("#userNameInput");
+var passwordInput = document.querySelector("#passwordInput");
+//------------------ Recovery page elements -----------------//
+var recoveryForm = document.querySelector(".recoveryForm");
+var passwordDisplayDiv = document.querySelector(".passwordDisplay");
+var recoveredPassword = document.querySelector("#recoveredPassword");
+//------------------ Main page elements -----------------//
+var profileWindow = document.querySelector(".profileWindow");
+var notifictionWindow = document.querySelector(".notifictionWindow");
+var profileDiv = document.querySelector(".profile");
+var notificationsDiv = document.querySelector(".notifications");
+var newBoardWindow = document.querySelector(".newBoardWindow");
+var boardArea = document.querySelector(".mainpageContainer__main__boards__boardArea");
+var backgroundImageSelectionDiv = document.querySelector(".backgroundSelectionDiv");
+var imageDisplayedInCreate = document.querySelector("#imageCreateWindow");
+// Buttons
+var createBoardWindowBtn = document.querySelector(".createBoardWindowBtn");
+var createBoardBtn = document.querySelector(".createBoardBtn");
+var cancelCreateBoardBtn = document.querySelector(".cancelCreateBoardBtn");
+var boardImageBtn = document.querySelector("#boardImageBtn");
+// Input
+var newBoardName = document.querySelector("#boardName");
+var newnBoardColor = document.querySelector("#boardColor");
+var searchBar = document.querySelector(".topNav__search");
+//------------------Board page-----------------------//
+var boardContainer = document.querySelector(".boardContainer__main");
+var boardTitle = document.querySelector(".topNav__boardName");
+var cards = document.querySelectorAll(".boardContainer__main__list__card");
+var editBoardWindow = document.querySelector(".editBoardWindow");
+var imageDisplayedInEdit = document.querySelector("#imageEditWindow");
+var trashCan = document.querySelector("#trash");
+var trashCanDiv = document.querySelector(".trashDiv");
+var noteBox = document.getElementById("notificationBox");
+var editInfoBtns = document.querySelectorAll(".editInfo");
+//Buttons
+var addListBtn = document.querySelector("#addListBtn");
+var editBoardBtn = document.querySelector(".editBoardBtn");
+var updatedBoardImageBtn = document.querySelector("#updatedBoardImageBtn");
+// const editProfileInfo = document.querySelector(".editprofileBtn") as HTMLButtonElement;
+// Input
+var newListInput = document.querySelector("#newListInput");
+var nameInputEle = document.querySelector("#updatedBoardName");
+var colorInputEle = document.querySelector("#updatedBoardColor");
+var addNewCardInputs = document.querySelectorAll(".newCardTextArea");
