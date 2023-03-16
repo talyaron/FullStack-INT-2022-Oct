dee7fd6b11959c913d647afe113ed263a6d6bb0c
Orekarako <orekarako@gmail.com>
2023-03-16 17:50:14 +0200

    asd

--- 03-node/01-basics/followups/Orel/index.js
+++ 03-node/01-basics/followups/Orel/index.js
@@ -1,18 +1,30 @@
-let fs = require('fs');
+let fs = require('fs')
 
-// fs.writeFile('text.txt' , "OrelKarakoSADsadqwd" , (err) => {
-//     if(err) throw err ;
-//     console.log("saved!");
-// });
+   const newHtmlBody = `<!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>New WebDie</title>
+   </head>
+   <body>
+       <H1>Hello World</H1>
+   </body>
+   </html>`
 
 
-fs.appendFile('text.txt', ' asdasdasdasdasd new One.', function (err) {
-    if (err) throw err;
-    console.log('Updated!');
-  });
+fs.writeFile("text.html" , "" , err=>{
+   if(err) throw err;
+   console.log("saved");
+})
 
-  fs.open('mynewfile2.ts', 'w', function (err, file) {
-    if (err) throw err;
-    console.log('Saved!');
-    console.log(file);
-  });
+
+fs.appendFile("text.html" , newHtmlBody , err=>{
+   if(err) throw err;
+   console.log("add to the end");
+})
+fs.appendFile("text.html" , "herrk" , err=>{
+   if(err) throw err;
+   console.log("add to the end");
+})
