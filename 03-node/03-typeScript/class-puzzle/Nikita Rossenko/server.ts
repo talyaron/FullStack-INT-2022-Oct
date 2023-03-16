c15d72db6be9cf5f69b34b44ef3f45e960b5e7dd
Nikita Rossenko <darkcaty1999@live.ru>
2023-03-16 20:32:13 +0200

    1

--- 03-node/03-typeScript/class-puzzle/Nikita Rossenko/server.ts
+++ 03-node/03-typeScript/class-puzzle/Nikita Rossenko/server.ts
@@ -1,7 +1,11 @@
 import express from 'express'
+import fs from 'fs'
 const app = express()
 
 app.get('/', function (req, res) {
+
+    const webPage = fs.readFileSync('./input2.txt',
+    {encoding:'utf8', flag:'r'})
     res.send('<h1 class="test_class" style="background-color:green">Test</h1> <ul><li style="color:green">1</li><li style="color:red">2</li><li style="color:blue">3</li><li style="color:aqua">4</li></ul>')
   })
 
