8ad95e518888200a2be05f89affbbd9d85c3769c
Kseniya Timoshchuk <kseni12121@gmail.com>
2023-03-16 20:29:48 +0200

    building server

--- 03-node/04-express/class-puzzle/room-6/server.ts
+++ 03-node/04-express/class-puzzle/room-6/server.ts
@@ -11,6 +11,15 @@ app.get("/", function (req, res) {
   res.send(indexFile);
 });
 
+app.get("/stam", function (req, res) {
+    //read file
+    const stamFile = fs.readFileSync("./stam .html", {
+      encoding: "utf8",
+      flag: "r",
+    });
+    res.send(stamFile);
+  });
+
 app.listen(3000, () => {
     console.log("server listen on port 3000");
   });
