e209b8ede9469e7beac2471cabc450d237030c1a
matandevl <117112024+matandevl@users.noreply.github.com>
2023-03-19 00:54:24 +0200

    updt
    added about page content:
    
    greet text
    skillsbar progression

--- 03-node/04-express/home-puzzles/matanel harush/public/js/main.js
+++ 03-node/04-express/home-puzzles/matanel harush/public/js/main.js
@@ -1,19 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const body = document.querySelector('body');
-    body.classList.add('fadein');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const body = document.querySelector('body');
+//     body.classList.add('fadein');
 
-    const aboutLink = document.getElementById('about');
-    aboutLink.addEventListener('click', function(e) {
-      e.preventDefault();
-      body.classList.add('fadeout');
+//     const aboutLink = document.getElementById('about');
+//     aboutLink.addEventListener('click', function(e) {
+//       e.preventDefault();
+//       body.classList.add('fadeout');
   
-      setTimeout(function() {
-        window.location.href = e.target.href;
-      }, 500);
-    });
+//       setTimeout(function() {
+//         window.location.href = e.target.href;
+//       }, 500);
+//     });
   
-    body.addEventListener('animationend', function() {
-      body.classList.remove('fadeout');
-    });
-  });
+//     body.addEventListener('animationend', function() {
+//       body.classList.remove('fadeout');
+//     });
+//   });
   
+
+
+
+const transitionOverlay = document.getElementById('transition-overlay');
+
+document.getElementById('about').addEventListener('click', (event) => {
+  event.preventDefault();
+
+  transitionOverlay.style.pointerEvents = 'auto';
+
+  setTimeout(() => {
+    window.location.href = event.target.href;
+  }, 1000);
+});
