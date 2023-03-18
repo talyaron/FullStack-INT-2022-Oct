a60b24e4ed0d16926b37188ec6cf8cfa1d5f81f8
matandevl <117112024+matandevl@users.noreply.github.com>
2023-03-18 19:30:57 +0200

    updt

--- 03-node/04-express/home-puzzles/matanel harush/public/js/main.js
+++ 03-node/04-express/home-puzzles/matanel harush/public/js/main.js
@@ -1,24 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const body = document.querySelector('body');
-  
-    // Set initial state
     body.classList.add('fadein');
-  
-    // Add event listener for link clicks
+
     const aboutLink = document.getElementById('about');
     aboutLink.addEventListener('click', function(e) {
       e.preventDefault();
-  
-      // Fade out effect
       body.classList.add('fadeout');
   
-      // Change page after fade out is complete
       setTimeout(function() {
         window.location.href = e.target.href;
-      }, 1000); // Change to match the duration of the animation
+      }, 500);
     });
   
-    // Remove fadeout class after animation is complete
     body.addEventListener('animationend', function() {
       body.classList.remove('fadeout');
     });
