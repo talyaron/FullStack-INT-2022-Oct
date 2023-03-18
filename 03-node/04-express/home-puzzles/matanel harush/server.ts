5417b643a599245a934f7316a7368dcfffe51da5
matandevl <117112024+matandevl@users.noreply.github.com>
2023-03-18 19:02:07 +0200

    updt
    fixed switching between pages.
    
    all pages linked!

--- 03-node/04-express/home-puzzles/matanel harush/server.ts
+++ 03-node/04-express/home-puzzles/matanel harush/server.ts
@@ -1,23 +1,28 @@
 import express from 'express';
 import fs from 'fs';
+import path from 'path';
 
 const app = express();
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+app.use('/particles', express.static(path.join(__dirname, 'particles')));
 
 app.get('/', function (req, res) {
-  const webPage = fs.readFileSync('./index.html', { encoding: 'utf8', flag: 'r' });
+  const webPage = fs.readFileSync(path.join(publicPath, 'index.html'), { encoding: 'utf8', flag: 'r' });
   res.send(webPage);
 });
 
 app.get('/about', function (req, res) {
-  const webPage = fs.readFileSync('./about.html', { encoding: 'utf8', flag: 'r' });
+  const webPage = fs.readFileSync(path.join(publicPath, 'about.html'), { encoding: 'utf8', flag: 'r' });
   res.send(webPage);
 });
 
 app.get('/contact', function (req, res) {
-  const webPage = fs.readFileSync('./contact.html', { encoding: 'utf8', flag: 'r' });
+  const webPage = fs.readFileSync(path.join(publicPath, 'contact.html'), { encoding: 'utf8', flag: 'r' });
   res.send(webPage);
 });
 
-app.listen(5000, function () {
-  console.log('Server is listening on port 5000');
+app.listen(3000, function () {
+  console.log('Server is listening on port 3000');
 });
