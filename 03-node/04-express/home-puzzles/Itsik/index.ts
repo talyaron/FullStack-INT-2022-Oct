6a262a52ed9a93b3b4a8b86fa68bf96fc15bf044
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-03-20 17:14:04 +0200

    i

--- 03-node/04-express/home-puzzles/Itsik/index.ts
+++ 03-node/04-express/home-puzzles/Itsik/index.ts
@@ -1,6 +1,8 @@
-window.onload = loadPage
+// window.onload = loadPage
 
-function loadPage(){
-    let audio = new Audio('./sound/titan.mp3');
-    audio.play();
-}
+// function loadPage(){
+//     let audio = new Audio('./sound/titan.mp3');
+//     audio.play();
+// }
+
+const navbar = document.querySelectorAll(".nav_ul_li")
