ecb80ae722c908b537ee724d3498120d71ff9a37
itayz1e <itayz1e@gmail.com>
2023-03-22 23:25:37 +0200

    HW-FIX STYLE

--- 03-node/05-AJAX-Async-Await/home-puzzle/itayAmosi/task-2/server2.ts
+++ 03-node/05-AJAX-Async-Await/home-puzzle/itayAmosi/task-2/server2.ts
@@ -3,9 +3,6 @@ const app = express();
 
 app.use(express.static(`./public`));
 
-app.get("https://dog.ceo/api/breeds/image/random" , (req, res)=>{
-})
-
 app.listen(4000, () => {
     console.log("server listen on port 4000");
   });
