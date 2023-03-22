56e4c5c5d384cde76086f2bf96e59fa7244a7676
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-03-22 21:41:26 +0200

    hw

--- 03-node/05-AJAX-Async-Await/home-puzzle/shlomi.samai/client/index.ts
+++ 03-node/05-AJAX-Async-Await/home-puzzle/shlomi.samai/client/index.ts
@@ -4,13 +4,15 @@ if (button){
 button.addEventListener("click",()=>{
 try {
     console.log('test')
-    const result:HTMLElement|null= document.querySelector("#result")
+    const result:HTMLElement|null= document.querySelector('#result')
     if (!result) throw new Error("couldnt find result");
     
-
    fetch("/randomPicture")
   .then((response) => response.json())
-  .then((data) => result.innerHTML=data)
+  .then((data) =>{
+    console.log(data)
+    result.innerHTML=`<img src=${data} alt="">`
+})
 
 } catch (error) {
     console.error(Error)
