354abeec8bac9d8985c74f4fd7f29bcd7dc57ff0
stmauda <120678111+StMauda@users.noreply.github.com>
2023-03-23 15:24:21 +0200

    push dog image - end

--- 03-node/05-AJAX-Async-Await/home-puzzle/Esti/server.ts
+++ 03-node/05-AJAX-Async-Await/home-puzzle/Esti/server.ts
@@ -1,17 +1,18 @@
 
 import express from "express";
-import fs from "fs";
+import path from "path";
+import dog from "dog"
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("./public"));
 
-const text = fs.readFileSync("./public/textfile.txt", "utf8")
-app.get("/esti", (req: any, res: any) => {
-    res.send(text)
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "index.html"));
+  });
 
+  
 app.listen(PORT, () => {
     console.log(`Server lister on port: ${PORT}`);
 })
