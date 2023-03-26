e7b58cc3a9e34c48aa21ccba9dae83f0f9ef7c04
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-03-26 11:41:12 +0300

    i

---
+++ 03-node/05-AJAX-Async-Await/Home-Puzzles2/itsik-euroleague/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+
+app.get("/", (req,res) =>{
+    res.send("hello")
+    
+    })
+
+    
+    app.use(express.static('./public'))
+    
+
+    
+    app.listen(PORT,()=>{
+        console.log(`server listen at: ${PORT}`)
+    })
