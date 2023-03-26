9c133fa7141bd5d46ecd6de007dc177f3e0a48de
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-03-26 12:12:50 +0300

    i

--- 03-node/05-AJAX-Async-Await/Home-Puzzles2/itsik-euroleague/dist/server.js
+++ 03-node/05-AJAX-Async-Await/Home-Puzzles2/itsik-euroleague/dist/server.js
@@ -2,16 +2,11 @@
 exports.__esModule = true;
 var express_1 = require("express");
 var app = express_1["default"]();
-<<<<<<< HEAD
 var PORT = 3000;
 app.get("/", function (req, res) {
     res.send("hello");
 });
 app.use(express_1["default"].static('./public'));
-app.listen(PORT, function () {
-    console.log("server listen at: " + PORT);
-=======
-var PORT = 5000;
 var artic = [
     {
         name: "",
@@ -21,19 +16,14 @@ var artic = [
     {
         name: "Micic",
         src: "https://media-cdn.incrowdsports.com/aa5e7932-e91f-4385-9c07-f4304331a665.jpg?crop=1920%3A1080%3Anowe%3A0%3A0&width=1920&resizeType=fill&format=webp",
-        article: "   "
+        article: " Vasilije Micic of Anadolu Efes Istanbul steps into the spotlight for the first episode of the Euroleague Basketball and Adidas collaboration in which Turkish Airlines EuroLeague stars discuss their personal journeys.   The two-time EuroLeague champion and Final Four MVP was interviewed about the key moments of his career that propelled his path to stardom and the lessons he learned along the way,  The two-time EuroLeague champion and Final Four MVP was interviewed about the key moments of his career that propelled his path to stardom and the lessons he learned along the way. Micic emphasized that some of his biggest decisions didn't seem  logical to others: leaving the EuroLeague to sign for Tofas Bursa at age 22 in a quest to fulfil his potential; moving to Zalgiris Kaunas the next season with a backup role under coach Sarunas Jasikevicius; and, most successfully, joining an Efes team that had ranked dead last in the EuroLeague before he signed in the summer of 2018. "
     },
     {
-        name: "",
-        src: "",
-        article: ""
+        name: "lorenzo brown",
+        src: "https://i.ytimg.com/vi/Gw0lwgepuf4/hq720.jpg",
+        article: " Lorenzo Brown is in the midst of his best Turkish Airlines EuroLeague season to date. The combo guard seems to have found a home with Maccabi Playtika Tel Aviv, which recently signed him to a three-year extension. Now at home on the Mediterranean coast in Israel, Brown spoke about his upbringing and the challenges along the way that led to him becoming a EuroLeague star."
     },
 ];
-app.get("/", function (req, res) {
-    res.send("test");
-});
-app.use(express_1["default"].static("./public"));
 app.listen(PORT, function () {
     console.log("server run at:" + PORT);
->>>>>>> 8216187d183022878479cd2eb7a3927b167fdfe3
 });
--- 03-node/05-AJAX-Async-Await/Home-Puzzles2/itsik-euroleague/server.ts
+++ 03-node/05-AJAX-Async-Await/Home-Puzzles2/itsik-euroleague/server.ts
@@ -1,6 +1,5 @@
 import express from "express";
 
-<<<<<<< HEAD
 const app = express();
 const PORT = 3000;
 
@@ -10,20 +9,8 @@ app.get("/", (req,res) =>{
     
     })
 
+app.use(express.static('./public'))
     
-    app.use(express.static('./public'))
-    
-
-    
-    app.listen(PORT,()=>{
-        console.log(`server listen at: ${PORT}`)
-    })
-=======
-const app = express()
-const PORT = 5000;
-
-
-
 interface news {
     name:string;
     src:string;
@@ -39,29 +26,21 @@ const artic:news[]=[
     {
         name:"Micic",
         src:"https://media-cdn.incrowdsports.com/aa5e7932-e91f-4385-9c07-f4304331a665.jpg?crop=1920%3A1080%3Anowe%3A0%3A0&width=1920&resizeType=fill&format=webp",
-        article:"   ",
+
+        article:" Vasilije Micic of Anadolu Efes Istanbul steps into the spotlight for the first episode of the Euroleague Basketball and Adidas collaboration in which Turkish Airlines EuroLeague stars discuss their personal journeys.   The two-time EuroLeague champion and Final Four MVP was interviewed about the key moments of his career that propelled his path to stardom and the lessons he learned along the way,  The two-time EuroLeague champion and Final Four MVP was interviewed about the key moments of his career that propelled his path to stardom and the lessons he learned along the way. Micic emphasized that some of his biggest decisions didn't seem  logical to others: leaving the EuroLeague to sign for Tofas Bursa at age 22 in a quest to fulfil his potential; moving to Zalgiris Kaunas the next season with a backup role under coach Sarunas Jasikevicius; and, most successfully, joining an Efes team that had ranked dead last in the EuroLeague before he signed in the summer of 2018. "
+
+        
+
+        
     },
     {
-        name:"",
-        src:"",
-        article:"",
+        name:"lorenzo brown",
+        src:"https://i.ytimg.com/vi/Gw0lwgepuf4/hq720.jpg",
+        article:" Lorenzo Brown is in the midst of his best Turkish Airlines EuroLeague season to date. The combo guard seems to have found a home with Maccabi Playtika Tel Aviv, which recently signed him to a three-year extension. Now at home on the Mediterranean coast in Israel, Brown spoke about his upbringing and the challenges along the way that led to him becoming a EuroLeague star.",
     },
 
 ]
 
-
-
-
-
-
-
-app.get("/", (req, res)=>{
-    res.send("test")
-})
-
-app.use(express.static("./public"));
-
 app.listen(PORT, ()=>{
     console.log("server run at:" + PORT)
 })
->>>>>>> 8216187d183022878479cd2eb7a3927b167fdfe3
