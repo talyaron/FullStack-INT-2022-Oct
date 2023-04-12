92c4927ae5220d585000bbdf8cb433a72daa6c3c
tomer cohen <tomer66331@gmail.com>
2023-04-12 18:10:23 +0300

    wipHW

--- 03-node/08-mongoose-design/HomePuzzle/tomer-cohen/API/student/dist/studentRoute.js
+++ 03-node/08-mongoose-design/HomePuzzle/tomer-cohen/API/student/dist/studentRoute.js
@@ -2,3 +2,9 @@
 exports.__esModule = true;
 var express_1 = require("express");
 var router = express_1["default"].Router();
+var studentControle_1 = require("./studentControle");
+router
+    .post("/add-mock-student", studentControle_1.addMockStudent)
+    .get("/get-students", studentControle_1.getStudents)["delete"]("/delete-student", studentControle_1.deleteStudent)
+    .patch("/update-student-name", studentControle_1.updateStudentName);
+exports["default"] = router;
--- 03-node/08-mongoose-design/HomePuzzle/tomer-cohen/API/teachers/teachersControle.ts
+++ 03-node/08-mongoose-design/HomePuzzle/tomer-cohen/API/teachers/teachersControle.ts
@@ -2,3 +2,55 @@ import { CourseSchema } from "../courses/coursesModel";
 import TeacherModel from "./teachersModel";
 import { v4 as uuidv4 } from "uuid";
 
+export const addMockTeacher = async (req: any, res: any) => {
+  try {
+    const newTeacher = await TeacherModel.create({
+      uid: uuidv4(),
+      name: " teacher_" + uuidv4().slice(0, 7),
+      lastName: uuidv4().slice(0, 7),
+    });
+    console.log(newTeacher);
+    res.status(200).send({ ok: true, newTeacher });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+};
+
+export const getTeacher = async (req: any, res: any) => {
+  try {
+    const teachers = await TeacherModel.find({});
+    res.send({ teachers });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+};
+
+export const deleteTeacher = async (req: any, res: any) => {
+  try {
+    const _id = req.query._id;
+    if (!_id) throw new Error("now id find");
+    await TeacherModel.deleteOne({ _id });
+    console.log(_id);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+};
+
+export const updateTeacherName = async (req: any, res: any) => {
+    try {
+      const { name, _id } = req.query;
+      console.log(name, _id);
+      if (!name) throw new Error("No name in data");
+      if (!_id) throw new Error("No uid in data");
+      const teacher = await TeacherModel.findOneAndUpdate({ _id }, { name });
+      if (!teacher) throw new Error("No teacher in array");
+      res.send({ ok: true });
+    } catch (error: any) {
+      console.error(error);
+      res.status(500).send({ error: error.message });
+    }
+  };
