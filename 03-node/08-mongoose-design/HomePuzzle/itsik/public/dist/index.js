b2604a8f12b75fb85dacba3c8855f0ca98e2f028
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-04-17 16:42:08 +0300

    i

--- 03-node/08-mongoose-design/HomePuzzle/itsik/public/dist/index.js
+++ 03-node/08-mongoose-design/HomePuzzle/itsik/public/dist/index.js
@@ -1,1 +1,18 @@
-var tb = document.querySelector(".");
+var tbsu = document.querySelector(".teacherbtn");
+var sbsu = document.querySelector(".studentbtn");
+tbsu.addEventListener("click", renderTeacher);
+sbsu.addEventListener("click", renderStudent);
+function renderTeacher() {
+    var choose = document.querySelector(".chooseOne");
+    choose.style.display = "none";
+    var divteacher = document.querySelector(".divteacher");
+    divteacher.style.display = "flex";
+}
+function renderStudent() {
+    var choose = document.querySelector(".chooseOne");
+    choose.style.display = "none";
+    var divstudent = document.querySelector(".divstudent");
+    divstudent.style.display = "flex";
+    var divteacher = document.querySelector(".divteacher");
+    divteacher.style.display = "none";
+}
--- 03-node/08-mongoose-design/HomePuzzle/itsik/public/index.ts
+++ 03-node/08-mongoose-design/HomePuzzle/itsik/public/index.ts
@@ -1,1 +1,22 @@
-const tb = document.querySelector(".")
+const tbsu = document.querySelector(".teacherbtn")as HTMLButtonElement;
+const sbsu = document.querySelector(".studentbtn")as HTMLButtonElement;
+
+tbsu.addEventListener("click", renderTeacher)
+sbsu.addEventListener("click", renderStudent)
+
+function renderTeacher(){
+ const choose = document.querySelector(".chooseOne")as HTMLDivElement;
+ choose.style.display = "none";
+ const divteacher = document.querySelector(".divteacher")as HTMLDivElement;
+ divteacher.style.display = "flex"
+  }
+
+function renderStudent(){
+    const choose = document.querySelector(".chooseOne")as HTMLDivElement;
+    choose.style.display = "none";
+    const divstudent = document.querySelector(".divstudent")as HTMLDivElement;
+    divstudent.style.display = "flex";
+    const divteacher = document.querySelector(".divteacher")as HTMLDivElement;
+    divteacher.style.display = "none"
+
+}
