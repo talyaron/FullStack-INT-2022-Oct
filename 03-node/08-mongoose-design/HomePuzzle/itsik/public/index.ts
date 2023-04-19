ce5a698bb2314dbd10486865020a501317cdb760
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-04-19 08:24:25 +0300

    i

--- 03-node/08-mongoose-design/HomePuzzle/itsik/public/index.ts
+++ 03-node/08-mongoose-design/HomePuzzle/itsik/public/index.ts
@@ -15,16 +15,15 @@ function renderTeacher(){
 
  const html = `
  <button class="goBackBtn" onclick="hendelGoback()">go back</button>
- <form class="signup__formTeacher">
-    <label for="tname" class="signup__formTeacher__label">Teacher Name</label>
-    <input type="text" name="tname" placeholder="enter name"  class="signup__formTeacher__inputt">
+    <form class="signup__formTeacher">
+    <label for="tname" class="signup__formTeacher__label">Student Name</label>
+    <input id="nameInput" type="text" name="tname" placeholder="enter name" required class="signup__formTeacher__inputt">
     <br>
-    <label for="temail" class="signup__formTeacher__label">Teacher Email</label>
-    <input type="email" name="temail"placeholder="enter email" class="signup__formTeacher__inputt">
+    <label for="temail" class="signup__formTeacher__label">Student Email</label>
+    <input id="nameEmail" type="email" name="temail" placeholder="enter email" required class="signup__formTeacher__inputt">
     <br>
-    <label for="tpassword" class="signup__formTeacher__label">Teacher Password</label>
-    <input type="password" name="tpassword" placeholder="enter password" class="signup__formTeacher__inputt">
-    <br>
+    <label for="tpassword" class="signup__formTeacher__label">Student Password</label>
+    <input id="namePassword" type="password" name="tpassword" placeholder="enter password" required class="signup__formTeacher__inputt">
     <button class="signUpBtn" onclick="signUpBtn()">Sign Up</button>
  </form>`
 
@@ -46,10 +45,10 @@ function renderStudent(){
     <input id="nameInput" type="text" name="tname" placeholder="enter name" required class="signup__formTeacher__inputt">
     <br>
     <label for="temail" class="signup__formTeacher__label">Student Email</label>
-    <input type="email" name="temail" placeholder="enter email" required class="signup__formTeacher__inputt">
+    <input id="nameEmail" type="email" name="temail" placeholder="enter email" required class="signup__formTeacher__inputt">
     <br>
     <label for="tpassword" class="signup__formTeacher__label">Student Password</label>
-    <input type="password" name="tpassword" placeholder="enter password" required class="signup__formTeacher__inputt">
+    <input id="namePassword" type="password" name="tpassword" placeholder="enter password" required class="signup__formTeacher__inputt">
     <button class="signUpBtn" onclick="signUpBtn()">Sign Up</button>
  </form>
  `
@@ -69,14 +68,14 @@ function hendelGoback() {
 }
 
 async function signUpBtn(){
-   let nameInput = document.getElementById("nameInput")!.value
+   let nameInput = document.getElementById("nameInput")as HTMLInputElement
    const options = {
    method: 'POST',
    headers: {
        'Content-Type': 'application/json' ,
        'Set-Cookie': 'token = 33'
    },
-   body: JSON.stringify({ data : nameInput, })
+   body: JSON.stringify({ data : nameInput.value, })
 }
 try {
    let result = await fetch(`localhost:3000/one`, options);
