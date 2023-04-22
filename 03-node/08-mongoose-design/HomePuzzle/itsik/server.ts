b64b847649f87d6e036d72ee3a58eae87a7cef89
itsik-ba <itsikbamani@gmail.com>
2023-04-22 20:43:07 +0300

    i

---
+++ 03-node/08-mongoose-design/HomePuzzle/itsik/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import express  from "express";
+import * as dotenv from 'dotenv';
+dotenv.config()
+const app = express();
+const PORT = 3000;
+const uri:string | undefined = process.env.MONGODB_URI
+
+mongoose.connect(uri).then(()=>{
+    console.log("DB connected");
+})
+
+app.use(express.json())
+app.use(express.static('public'))
+
+
+
+
+app.listen(PORT ,()=>{
+    console.log(`the server run on PORT:${PORT}`);
+})
