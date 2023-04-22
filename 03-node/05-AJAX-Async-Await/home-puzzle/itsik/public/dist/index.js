83049d81ff1ee4d2ad7b133840ab501f7297e362
itsik-ba <itsikbamani@gmail.com>
2023-04-22 20:12:59 +0300

    i

--- 03-node/05-AJAX-Async-Await/home-puzzle/itsik/public/dist/index.js
+++ 03-node/05-AJAX-Async-Await/home-puzzle/itsik/public/dist/index.js
@@ -1,19 +1,18 @@
 var dog = document.querySelector(".contDog");
 var cat = document.querySelector(".contCat");
-var dogBtn = document.querySelector("#dogResult");
-var catBtn = document.querySelector("#catResult");
+var dogBtn = document.querySelector(".dogResult");
+var catBtn = document.querySelector(".catResult");
 dogBtn.addEventListener("click", getDogImage);
-// catBtn.addEventListener("click", getCatImage)
+catBtn.addEventListener("click", getCatImage);
 function getDogImage() {
-    var dog = document.querySelector(".contDog");
     try {
         fetch('https://dog.ceo/api/breeds/image/random')
             .then(function (res) { return res.json(); })
             .then(function (data) {
+            console.log(data);
             if (dog) {
-                console.log(data);
-                var show = "<img src=\"" + data.Message + "\">";
-                dog.innerHTML = show;
+                var ap = data.message;
+                var dog_1 = document.querySelector(".contDog").innerHTML = "<img src=\"" + ap + "\" width=\"397\" height=\"397\"></img>";
             }
         });
     }
@@ -21,5 +20,15 @@ function getDogImage() {
         console.error(error);
     }
 }
-// function getCatImage(){
-// }
+function getCatImage() {
+    fetch(' https://api.thecatapi.com/v1/images/search')
+        .then(function (res) { return res.json(); })
+        .then(function (data) {
+        console.log(data);
+        if (cat) {
+            var cp = data.url;
+            //    cat.innerHTML = `<img src="${cp}" width="397" height="397"></img>`;
+            var cat_1 = document.querySelector(".contCat").innerHTML = "<img src=\"" + cp + "\" width=\"397\" height=\"397\"></img>";
+        }
+    });
+}
--- 03-node/05-AJAX-Async-Await/home-puzzle/itsik/public/index.ts
+++ 03-node/05-AJAX-Async-Await/home-puzzle/itsik/public/index.ts
@@ -1,32 +1,41 @@
 const dog = document.querySelector(".contDog")as HTMLDivElement;
 const cat = document.querySelector(".contCat")as HTMLDivElement;
-const dogBtn = document.querySelector("#dogResult")as HTMLButtonElement;
-const catBtn = document.querySelector("#catResult")as HTMLButtonElement;
+const dogBtn = document.querySelector(".dogResult")as HTMLButtonElement;
+const catBtn = document.querySelector(".catResult")as HTMLButtonElement;
 
 dogBtn.addEventListener("click", getDogImage)
-// catBtn.addEventListener("click", getCatImage)
+catBtn.addEventListener("click", getCatImage)
 
 
 function getDogImage(){
-    const dog = document.querySelector(".contDog")as HTMLDivElement;
+   
     try {
-        fetch('https://dog.ceo/api/breeds/image/random')
+     fetch('https://dog.ceo/api/breeds/image/random')
     .then(res => res.json())
     .then(data =>{
-         if(dog){
-            console.log(data)
-            const show = `<img src="${data.Message}">`
-           dog.innerHTML = show
-         }  
-    })
+       console.log(data)
+       if(dog){
+        const ap = data.message
+        const dog = document.querySelector(".contDog")!.innerHTML = `<img src="${ap}" width="397" height="397"></img>`
+       }
+
+})
     } catch (error) {
         console.error(error)
     }
     
-
-
 }
 
-// function getCatImage(){
+function getCatImage(){
+fetch(' https://api.thecatapi.com/v1/images/search')
+.then(res => res.json())
+    .then(data =>{
+       console.log(data)
+      if(cat){
+       const cp = data.url
+    //    cat.innerHTML = `<img src="${cp}" width="397" height="397"></img>`;
+       const cat = document.querySelector(".contCat")!.innerHTML = `<img src="${cp}" width="397" height="397"></img>`
+       }
+})
 
-// }
+}
