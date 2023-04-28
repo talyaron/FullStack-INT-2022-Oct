b458497a8fd8ee70630aed0ab27802faacf91c15
amitzalts <amitzalts@gmail.com>
2023-04-28 19:42:28 +0300

    wip

--- 03-node/mongoose-joins/home-puzzle/Amit/API/courses/dist/coursesModel.js
+++ 03-node/mongoose-joins/home-puzzle/Amit/API/courses/dist/coursesModel.js
@@ -1,8 +1,7 @@
 "use strict";
 exports.__esModule = true;
-exports.CourseStudentModel = exports.GradeModel = exports.CourseStudentSchema = exports.CourseSchema = void 0;
+exports.CourseStudentModel = exports.CourseStudentSchema = exports.CourseSchema = void 0;
 var mongoose_1 = require("mongoose");
-var gradesModel_1 = require("../grades/gradesModel");
 var studentsModel_1 = require("../students/studentsModel");
 exports.CourseSchema = new mongoose_1.Schema({
     name: String
@@ -11,7 +10,6 @@ exports.CourseStudentSchema = new mongoose_1.Schema({
     student: studentsModel_1.StudentSchema,
     course: exports.CourseSchema
 });
-exports.GradeModel = mongoose_1["default"].model('grades', gradesModel_1.GradeSchema);
 exports.CourseStudentModel = mongoose_1["default"].model("course-students", exports.CourseStudentSchema);
 var CourseModel = mongoose_1["default"].model("courses", exports.CourseSchema);
 exports["default"] = CourseModel;
