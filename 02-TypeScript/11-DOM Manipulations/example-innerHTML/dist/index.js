899e2e1b3d55e393148053c3135e6b1b28d98dc6
Asael-Matari <mtri01234@gmail.com>
2023-05-10 11:21:07 +0300

    hw DOM
    HW DOM

--- 02-TypeScript/11-DOM Manipulations/example-innerHTML/dist/index.js
+++ 02-TypeScript/11-DOM Manipulations/example-innerHTML/dist/index.js
@@ -1,6 +1,6 @@
 var title = document.querySelector("#title");
 console.dir(title);
-var userName = prompt("What is you name");
+var userName = prompt("What is your name");
 var imageUrl = prompt('Enter image url');
 if (userName && title && imageUrl) {
     title.innerHTML = "<div class=\"card\">\n        <h1>This is " + userName + "'s site</h1>\n        <img src='" + imageUrl + "'/>\n        <p class=\"green\">This is the best site ever</p>\n    </div>";
