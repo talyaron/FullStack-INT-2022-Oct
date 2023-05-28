e53cf0ec3cc276bcf5b60d49fa2fcdc4078f9e23
Asael-Matari <mtri01234@gmail.com>
2023-05-28 12:43:06 +0300

    hw
    hw

--- 02-TypeScript/15-store/Home-puzzles/asael.matari/control.ts
+++ 02-TypeScript/15-store/Home-puzzles/asael.matari/control.ts
@@ -1,54 +1,11 @@
-function handleDetails(ev) {
-  try {
-    ev.preventDefault();
+const form=document.getElementsByClassName("form")
 
-    console.dir(ev);
-    const name:string = ev.target.elements.name;
-    const address:string = ev.target.elements.address.value;
-    const email:string = ev.target.elements.email.value;
-    const phone:number = ev.target.elements.phone.value;
-    const password:string = ev.target.elements.password.value;
-    const color:string = ev.target.elements.color.value;
-    const img:string = ev.target.elements.img;
-    const id:number = arrayOfUsers.length===undefined?0:arrayOfUsers.length;
-    if(id && name && address && email && phone &&password && color && img)
-    arrayOfUsers.push(
-      new UserDetails(id, name, address, email, phone, password, color, img));
-      console.log(arrayOfUsers);
-      
-      renderDetails()
-  } catch (error) {
-    console.error(error);
-    return false
-  }}
 
-function renderDetails() {
-  try {
-    // ev.preventDefault();
-    const currentIndexArray=arrayOfUsers.length - 1
-    const card = `<div class="userDetailsCard" id="userInput${
-        arrayOfUsers[currentIndexArray].id}">
-        <p>name: ${arrayOfUsers[currentIndexArray].name}</p><br>
-        <p>address: ${arrayOfUsers[currentIndexArray].address}</p><br>
-        <p>email: ${arrayOfUsers[currentIndexArray].email}</p> <br>
-        <p>phone: ${arrayOfUsers[currentIndexArray].phone}</p><br>
-        <p>password: ${arrayOfUsers[currentIndexArray].password}</p><br>
-        <p>color: ${arrayOfUsers[currentIndexArray].color}</p><br>
-        <img src="${arrayOfUsers[currentIndexArray].img}"</div>`;
-    if (containerOfDetails) {
-      containerOfDetails.innerHTML = card;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-function handleChangerColor(ev) {
-  try {
-    console.log(ev);
-    const color = ev.target.value;
-    document.body.style.backgroundColor = color;
-  } catch (error) {
-    console.error(error);
-    return false
-  }
-}
+
+
+
+
+
+
+
+
--- 02-TypeScript/15-store/Home-puzzles/asael.matari/view.ts
+++ 02-TypeScript/15-store/Home-puzzles/asael.matari/view.ts
@@ -1,1 +1,6 @@
-const containerOfDetails=document.querySelector(".containerOfDetails")
+const img=document.getElementById("img")
+const input=document.getElementById("input")
+img?.addEventListener("click",()=>{
+    if(input)
+    input.type= input.type==="password"?"text":"password"
+})
