517997409d3121ea40f0521c53b26b1a735c3537
amitzalts <amitzalts@gmail.com>
2023-05-31 18:23:46 +0300

    added nodejs team project + home puzzle start

---
+++ 04-React/01-basic/Home-puzzles/amit/src/components/box/box.tsx
@@ -0,0 +1,19 @@
+import { FC, useState } from "react";
+import "./box.scss";
+interface BoxProps {
+  text: string;
+}
+
+const Box: FC<BoxProps> = ({ text }) => {
+  const [counter, setCounter] = useState(0);
+  function handleClick() {
+    setCounter((counter) => counter + 1);
+  }
+  return (
+    <div className="box" onClick={handleClick}>
+      {text}: {counter}
+    </div>
+  );
+};
+
+export default Box;
