179ae419973ae1fd3f25d8589624b4cf3a94ca8e
Tal Yaron <tal.yaron@gmail.com>
2023-06-01 20:26:52 +0300

    HP

--- 04-React/02-useState/follwups/room3/room3/src/App.tsx
+++ 04-React/02-useState/follwups/room3/room3/src/App.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
-import "./App.css"
-import PopUp from './popUp/PopUp'
+import React, { useState } from "react";
+import "./App.css";
+import PopUp from "./popUp/PopUp";
 
 function App() {
   // const [count, setCount] = useState(0);
   const [showPopup, setShowPopup] = useState<boolean>(false);
-  const [colorIndex, setColorIndex] = useState(0);
-
-  
+  const [colorIndex, setColorIndex] = useState<number>(0);
+  const colors = ["red", "yellow", "pink", "green", "gold"];
 
   function handleShowPopup() {
     setShowPopup((showPopup) => !showPopup);
@@ -15,33 +14,25 @@ function App() {
   }
 
   function handleChangeColor() {
-    const colors= ["red", "yellow", "pink"]
-    
     setColorIndex((colorIndex) => {
-
-       if(colorIndex > colors.length){
-        setColorIndex(0)
-       }else{
-        setColorIndex(colorIndex => colorIndex+1)
-       }
-
+      let _colorIndex = colorIndex;
+      if (_colorIndex >= colors.length) {
+        _colorIndex = 0;
+      } else {
+        _colorIndex++;
+      }
+      return _colorIndex;
     });
-
-   
   }
-
+  console.log(colorIndex);
   return (
-    <div>
+    <div style={{ backgroundColor: colors[colorIndex] }}>
       <h1>sadasdasdsa</h1>
       <button onClick={handleChangeColor}>change-color</button>
       <button onClick={handleShowPopup}>{showPopup ? "Close" : "Open"}</button>
       {showPopup ? <PopUp /> : null}
-     
     </div>
   );
 }
 
 export default App;
-
-
-
