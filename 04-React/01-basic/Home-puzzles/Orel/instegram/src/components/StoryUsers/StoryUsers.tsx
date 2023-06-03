f24e5895582d76e2294153ea82213f2717c469ee
Orekarako <orekarako@gmail.com>
2023-06-04 01:13:50 +0300

    1

--- 04-React/01-basic/Home-puzzles/Orel/instegram/src/components/StoryUsers/StoryUsers.tsx
+++ 04-React/01-basic/Home-puzzles/Orel/instegram/src/components/StoryUsers/StoryUsers.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+
 import './StoryUsers.scss'
 
 export interface StoryUsersPrompts {
@@ -6,14 +6,14 @@ export interface StoryUsersPrompts {
     authorName: string;
 }
 
-const StoryUsers:FC<StoryUsersPrompts> = ({image , authorName}) =>{
+const StoryUsers: React.FC<StoryUsersPrompts> = ({ image, authorName }) => {
     return (
-        <div className="container">
-        <div className="circleUserStory-container">
+        <div className="storyUser-container">
+            <div className="circleUserStory-container">
                 <img className="circleUserStory-container__image" src={image} />
+                 </div>
+            <h6 className="circleUserStory-container__user-name" >{authorName}</h6>
         </div>
-                <h6 className="circleUserStory-container__user-name" >{authorName}</h6>
-                </div>
     )
 }
 
