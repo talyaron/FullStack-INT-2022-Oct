4f1f3c70ab2255e291d65ee33dcec306b1ad9a03
Tal Yaron <tal.yaron@gmail.com>
2023-06-05 19:22:54 +0300

    followup

--- 04-React/04-react-with-server/example/client/src/App.tsx
+++ 04-React/04-react-with-server/example/client/src/App.tsx
@@ -1,23 +1,27 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import './App.css'
-
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0);
-  useEffect(()=>{
-    (async()=>{
-      const {data} = await axios.get("/api/get-word");
-      console.log(data)
-    })()
-  },[])
+  const [isTrue,setIsTrue] = useState(false)
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("/api/get-word");
+     
+      const {ok} = data;
+      console.log(ok)
+      if(ok){
+        setIsTrue(ok)
+      }
+    })();
+  }, []);
 
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello: {isTrue?"Hi all":"good?"}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
