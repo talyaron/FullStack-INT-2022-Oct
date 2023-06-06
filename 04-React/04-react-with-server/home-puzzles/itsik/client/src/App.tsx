9e5aef60bb268e9c251ac0b20a7898d07d093a4d
itsik-ba <112100680+itsik-ba@users.noreply.github.com>
2023-06-06 18:43:54 +0300

    i

--- 04-React/04-react-with-server/home-puzzles/itsik/client/src/App.tsx
+++ 04-React/04-react-with-server/home-puzzles/itsik/client/src/App.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect } from "react"
 import './App.css'
 
 
@@ -12,6 +13,42 @@ export const ballons =
 ]
 
 function App() {
+
+   const ballons =
+  [{src:"https://shorturl.at/bpFL5"},
+  {src:"https://shorturl.at/dnOS0"}, 
+  {src:"https://shorturl.at/oLSX5"}, 
+  {src:"https://shorturl.at/ghsO3"},
+  {src:"https://shorturl.at/aoEJ5"},
+  {src:"https://shorturl.at/prwEI"},
+  ]
+    
+  const newBarray:any = {ballons}
+  if (!newBarray) throw new Error("No ballons array");
+
+  useEffect(()=>{
+   
+    (async ()=>{
+      const {data} = await axios.get('/start-game');
+
+      const {message} = data;
+
+      setImg(message)
+
+    })()
+
+  },[])
+
+  useEffect(()=>{
+   
+
+    console.log('update')
+  },[count])
+
+
+
+
+
   return (
     <div>
       
