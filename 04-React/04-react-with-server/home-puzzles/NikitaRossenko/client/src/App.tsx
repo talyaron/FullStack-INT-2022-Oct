ad333cca980a2072d98995392107e5eab3fc3103
Nikita Rossenko <darkcaty1999@live.ru>
2023-06-07 22:17:20 +0300

    1

--- 04-React/04-react-with-server/home-puzzles/NikitaRossenko/client/src/App.tsx
+++ 04-React/04-react-with-server/home-puzzles/NikitaRossenko/client/src/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [balloons, setBalloons] = useState(0)
+
+  function getBalloons(){
+    console.log("getting Balloons")
+
+  }
+
+  useEffect(() => {
+    async () => {
+
+      const balloonsDB = await axios.get("/get-balloons")
+      console.log(balloonsDB)
+    }
+  },[])
 
   return (
     <>
-      <button className='getBallonsBtn'>Get Ballons</button>
+      <div className='balloonsCntainer'>
+
+      </div>
+      <button onClick={getBalloons} className='getBallonsBtn'>Get Ballons</button>
     </>
   )
 }
