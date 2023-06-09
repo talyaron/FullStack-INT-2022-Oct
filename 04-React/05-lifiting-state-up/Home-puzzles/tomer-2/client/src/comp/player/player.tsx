80f7f139a6750d7d82c513142f4079b1774f973c
tomer cohen <tomer66331@gmail.com>
2023-06-09 21:48:45 +0300

    add pop up in the screen

--- 04-React/05-lifiting-state-up/Home-puzzles/tomer-2/client/src/comp/player/player.tsx
+++ 04-React/05-lifiting-state-up/Home-puzzles/tomer-2/client/src/comp/player/player.tsx
@@ -1,4 +1,4 @@
-import { FC,SetStateAction } from "react";
+import { FC,SetStateAction, useState } from "react";
 import { Player } from "../../App";
 
 interface PlayerProps{
@@ -8,29 +8,34 @@ interface PlayerProps{
 };
 
 const Player: FC<PlayerProps> = ({ setPlayers, players, player }) => {
+    const [ouchMessage, setOuchMessage] = useState('');
     function handleClickPlayer(playerId: string) {
-      try {
-        const playerIndex = players.findIndex((player) => player._id === playerId);
-        if (playerIndex === -1) throw new Error("Player not found");
-        
-        const newPlayers = [...players];
-        newPlayers[playerIndex].image1 = newPlayers[playerIndex].image2; // Update image1 with image2
-        
-        setPlayers(newPlayers);
-      } catch (error) {
-        console.error(error);
+        try {
+          const playerIndex = players.findIndex((player) => player._id === playerId);
+          if (playerIndex === -1) throw new Error("Player not found");
+          
+          const newPlayers = [...players];
+          newPlayers[playerIndex].image1 = newPlayers[playerIndex].image2; // Update image1 with image2
+          
+          setPlayers(newPlayers);
+          setOuchMessage('Ouch!'); // Set the message to display
+        } catch (error) {
+          console.error(error);
+        }
       }
-    }
   
     return (
-      <div>
+      <div className="player-container">
         <img
           onClick={() => handleClickPlayer(player._id)}
-          className="player"
+          className="player-img"
           key={player._id}
           src={player.image1}
           alt="player"
         />
+         {ouchMessage && (
+        <div className="message">{ouchMessage}</div>
+      )}
       </div>
     );
   };
