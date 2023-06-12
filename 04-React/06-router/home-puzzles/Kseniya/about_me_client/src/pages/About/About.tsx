0a346384e30e71cc87bec123ba0fc82758d3c3b6
Kseniya Timoshchuk <111976842+KseniyaTim@users.noreply.github.com>
2023-06-13 00:50:27 +0300

    setting up routes

---
+++ 04-React/06-router/home-puzzles/Kseniya/about_me_client/src/pages/About/About.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react';
+
+const About:FC = () => {
+    return (
+        <div>
+            I'm about component
+        </div>
+    );
+};
+
+export default About;
---
+++ 04-React/06-router/home-puzzles/Kseniya/about_me_client/src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const Home:FC = () => {
+    return (
+        <div>
+            I'm Home component
+        </div>
+    );
+};
+
+export default Home;
