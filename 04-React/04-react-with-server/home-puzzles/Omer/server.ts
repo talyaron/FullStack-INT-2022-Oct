da141362bc061dcab2d8646e7fe0e7ea6641e22f
omervavd3 <omervavd3@gmail.com>
2023-06-13 17:41:47 +0300

    l

--- 04-React/04-react-with-server/home-puzzles/Omer/server.ts
+++ 04-React/04-react-with-server/home-puzzles/Omer/server.ts
@@ -5,9 +5,22 @@ const app = express();
 //static file
 app.use(express.static('./client'));
 
-app.get("/api/get-users", (req,res) => {
+interface Balloon {
+    width:number,
+    height:number,
+    color:string
+}
+
+const balloon1:Balloon = {width: 10, height:10, color:"blue"}
+const balloon2:Balloon = {width: 6, height:10, color:"red"}
+const balloon3:Balloon = {width: 8, height:10, color:"green"}
+const balloon4:Balloon = {width: 12, height:10, color:"yellow"}
+
+const balloons = [balloon1,balloon2,balloon3,balloon4]
+
+app.get("/api/get-balloons", (req,res) => {
     try {
-        res.status(200).send({ok:true})
+        res.status(200).send({balloons})
     } catch (error) {
         res.status(500).send({error:error})
     }
