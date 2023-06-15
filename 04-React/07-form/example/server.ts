cb971dd4a6f48584bb9586bc327acc538d5d5a71
Tal Yaron <tal.yaron@gmail.com>
2023-06-15 19:56:54 +0300

    Forms

---
+++ 04-React/07-form/example/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from 'cors';
+const app = express();
+
+
+// app.use(cors({
+//   credentials:true,
+//   origin:"http://localhost:5173"
+// }));
+
+//static file
+app.use(express.static('./client'));
+
+interface Flower{
+    img:string;
+    name:string;
+}
+
+const flowers:Flower[] = []
+
+app.get("/api/get-word",(req, res)=>{
+    res.send({ok:true})
+})
+
+app.get("/api/get-flowers",(req, res)=>{
+    res.send({ok:true})
+})
+
+app.listen(3000, () => {
+  console.log("server listen on port 3000");
+});
