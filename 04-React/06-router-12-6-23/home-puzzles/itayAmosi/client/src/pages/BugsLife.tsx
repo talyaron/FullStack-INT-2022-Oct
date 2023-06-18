629b20db54b53f10ccc50f12c8088bca0ab197eb
itayz1e <itayz1e@gmail.com>
2023-06-18 23:25:28 +0300

    end api and routes ffor projects

--- 04-React/06-router-12-6-23/home-puzzles/itayAmosi/client/src/pages/BugsLife.tsx
+++ 04-React/06-router-12-6-23/home-puzzles/itayAmosi/client/src/pages/BugsLife.tsx
@@ -1,55 +1,52 @@
-import { useEffect, useState } from "react";
-import NavBar from "../components/NavBar";
-import Comment from "../components/Comment";
-import axios from "axios";
-import "../style/bugsLife.scss"
+// import { useEffect, useState } from "react";
+// import NavBar from "../components/NavBar";
+// import Comment from "../components/Comment";
+// import axios from "axios";
+// import "../style/bugsLife.scss";
+// import { useParams } from "react-router-dom";
+// import ProjectData from "../components/ProjectData";
 
+// interface Project {
+//   _id: string;
+//   name: string;
+//   src1: string;
+//   src2: string;
+//   src3: string;
+//   src4: string;
+// }
 
-interface Project {
-  _id: string;
-  name: string;
-  src1: string;
-  src2: string;
-  src3: string;
-  src4: string;
-}
+// const BugsLife = () => {
+//   const [project, setProject] = useState<Project>();
+//   const { _id } = useParams();
+//   useEffect(() => {
+//     const getProjectDetails = async () => {
+//       try {
+//         console.log(_id);
+//         const response = await axios.get(
+//           `/api/project/get-project-by-id?projectId=${_id}`
+//         );
+//         const { data } = response;
+//         console.log(data);
+//         setProject(data);
+//       } catch (error: any) {
+//         console.error(error);
+//       }
+//     };
 
-const BugsLife = () => {
+//     getProjectDetails();
+//   }, [_id]);
 
-  const [projects, setProject] = useState<Project[]>([]);
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get("/api/project/get-projects");
-      const { Project } = data;
-      console.log(Project);
-      setProject(Project);
-    })();
-  }, []);
+//   if (!project) {
+//     return null;
+//   }
 
-  
-  return (
-    <>
-    <NavBar />
-    {projects.map((project, i) => (          
-      <section key={i}>
-        <div className="box box-1">
-          <img src={project.src1} alt="" />
-        </div>
-        <div className="box box-2">
-        <img src={project.src2} alt="" />
-        </div>
-        <div className="box box-3">
-        <img src={project.src3} alt="" />
-        </div>
-        <div className="box box-4">
-        <img src={project.src4} alt="" />
-        </div>
-      </section>
-      ))}
-      <Comment />
+//   return (
+//     <>
+//       <NavBar />
+//       <ProjectData />
+//       <Comment />
+//     </>
+//   );
+// };
 
-    </>
-  );
-};
-
-export default BugsLife;
+// export default BugsLife;
