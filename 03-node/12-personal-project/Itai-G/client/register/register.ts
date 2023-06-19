56dd4d7fcf309526d846d14f79203dd4c694d108
itai432 <gelberg4322@gmail.com>
2023-06-19 19:26:18 +0300

    fix hw

--- 03-node/12-personal-project/Itai-G/client/register/register.ts
+++ 03-node/12-personal-project/Itai-G/client/register/register.ts
@@ -1,33 +1,42 @@
-import { json } from "body-parser";
-
 interface User {
     name: string;
     email:string;
 }
 
-function handelAddUser(ev: any){
+function handleAddUser(ev) {
+    ev.preventDefault(); 
+  
     try {
-        const name = ev.target.elements.name.value;
-        const password = ev.target.elements.password.value;
-        const email = ev.target.elements.email.value;
-        if(!name) throw new Error("no name!")
-        if(!password) throw new Error("no password!!!")
-        if(!email) throw new Error("no email!")
-
-        const newUser: any = {name,password,email};
-
-        fetch("/api/users/add-user",{
-            method:"POST",
-            headers:{
-                Accept: "application/json",
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(newUser),
-        })
+      const name = ev.target.elements.name.value;
+      const password = ev.target.elements.password.value;
+      const email = ev.target.elements.email.value;
+  
+      if (!name) throw new Error("No name!");
+      if (!password) throw new Error("No password!");
+      if (!email) throw new Error("No email!");
+  
+      const newUser = { name, password, email };
+  
+      fetch("/api/users/add-user", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      })
         .then((res) => {
-            // console.log(data);
+          if (res.ok) {
+            console.log("User registered successfully!");
+            window.location.href = "http://localhost:3000/login/login.html";
+          } else {
+            throw new Error("Failed to register user");
+          }
         })
-    } catch (error: any) {
-        console.error("coudlds complete the add user")
+        .catch((error) => {
+          console.error("Could not complete the registration:", error);
+        });
+    } catch (error) {
+      console.error("Could not complete the add user:", error);
     }
-}
+  }
