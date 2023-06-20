b6192d071fd6e035e3bb706f101c41411374c0e4
Kseniya Timoshchuk <111976842+KseniyaTim@users.noreply.github.com>
2023-06-20 17:32:03 +0300

    uploading again client and server of node project

--- 03-node/12-personal-project/Kseniya/server/modules/Recipe/RecipeRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { addRecipe, getRecipes } from "./RecipeControl";
-
-router.get("/all", getRecipes).post("/add", addRecipe);
-
-export default router
